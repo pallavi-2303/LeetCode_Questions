5ef302976ed46a296deec2e419659bb52f8b38a1
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-01-07 00:40:05 +0530

    Time: 5 ms (56.02%), Space: 44.8 MB (67.3%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -2,14 +2,25 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
       int n=nums.length;
       //brute force ->generating all the subarray and finding out the sum
-      int[] ans={-1};
+     /* int[] ans={-1};
       for(int i=0;i<n;i++){
         for(int j=i+1;j<n;j++){
        if(nums[i]+nums[j]==target) {
         ans=new int[]{i,j};
        }    
         }
-      }
+      }*/
+      //optimal aproach->using a hashmap to store previous element
+     HashMap<Integer,Integer> mp=new HashMap<>();
+     int[] ans={-1};
+    for(int i=0;i<n;i++) {
+    int rem=target-nums[i];
+    if(mp.containsKey(rem)){
+    //means arr[i]+rem==target
+    ans=new int[]{mp.get(rem),i};
+    }
+    mp.put(nums[i],i);
+    }
 return ans;
     }
 }
