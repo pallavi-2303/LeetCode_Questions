5e8f6a9d4e10a4cc5210ab7a312c252936dcade6
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-01-06 21:30:40 +0530

    Time: 0 ms (100%), Space: 57.6 MB (18.77%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,6 +1,16 @@
 class Solution {
+    public void rotate2(int[] nums,int i,int j){
+        while(i<j){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
+        }
+    }
     public void rotate(int[] nums, int k) {
-      //rotate by using an external array
+      /*rotate by using an external array
+      //TC=0(2N) SC=0(N)
       int n=nums.length;
       int[] ans=new int[n]; 
       k=k%n;
@@ -17,6 +27,16 @@ ans[idx++]=nums[i];
       //puuting all ellements in nums
       for(int i=0;i<n;i++){
         nums[i]=ans[i];
-      }
+      }*/
+      //second aproach of O(1) SC
+      int n=nums.length;
+      k=k%n;
+      if(n==k) return;
+    //STEP-1 ROTATE FROM 0 TO LESS THAN N-K
+   rotate2(nums,0,n-k-1);
+    //STEP-2 ROTATE FROM N-K TO N-1
+    rotate2(nums,n-k,n-1);
+    //STEP-3 ROTATE FROM 0 TO N-1
+    rotate2(nums,0,n-1);
     }
 }
