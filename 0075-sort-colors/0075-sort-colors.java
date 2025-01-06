a82ec08da88854d9443755a6bee3e287a325cf60
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-01-07 01:41:26 +0530

    Time: 0 ms (100%), Space: 42 MB (38.37%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,45 +1,48 @@
 class Solution {
-   public void swap(int[] arr,int i,int j){
- int temp=arr[i];
-    arr[i]=arr[j];
-    arr[j]=temp;}
+    public void swap(int[] arr,int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
     public void sortColors(int[] nums) {
- //Arrays.sort(nums);
-/*int n=nums.length;
-int count0=0;
+   int n=nums.length;
+   //brute force ->couting number of zeroes ,ones and twos and then putting in array
+//Arrays.sort(nums) ;  //TC->0(2N)
+/*int count0=0;
 int count1=0;
 int count2=0;
 for(int i=0;i<n;i++){
-if(nums[i]==0) count0+=1;
-else if(nums[i]==1) count1+=1;
-else count2+=1;}
-//putting all in nums
+    if(nums[i]==0) count0++;
+    else if(nums[i]==1) count1++;
+    else count2++;
+}
+//putting all ellements in 0,1,2 in array
 for(int i=0;i<count0;i++){
-nums[i]=0;}
+    nums[i]=0;
+}
 for(int i=count0;i<count0+count1;i++){
-nums[i]=1;}
+    nums[i]=1;
+}
 for(int i=count0+count1;i<count0+count1+count2;i++){
-nums[i]=2;}*/
-int n=nums.length;
+    nums[i]=2;
+}*/
+//optimal aproach of using a binary sort
 int low=0;
 int high=n-1;
 int mid=0;
 while(mid<=high){
-if(nums[mid]==0){
-swap(nums,low,mid);
- low++;
-mid++;}
-else if(nums[mid]==1){
-    mid++;}
-else {//nums mid==2
- swap(nums,mid,high);
-    high--;}}
-   
-
-
-
-
-
-        
+    if(nums[mid]==0){
+        swap(nums,low,mid);
+        low++;
+        mid++;
+    }
+    else if(nums[mid]==1){
+        mid++;
+    }
+    else {
+        swap(nums,mid,high);
+        high--;
+    }
+}
     }
 }
