150d4e31e673a9242674666375ee9e81221c5fbe
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-01-06 23:36:04 +0530

    Time: 2 ms (85.19%), Space: 46.1 MB (25.31%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,18 +1,35 @@
 class Solution {
+  public void swap(int[] arr,int i,int j){
+  int temp=arr[i];
+    arr[i]=arr[j];
+    arr[j]=temp;}
     public void moveZeroes(int[] nums) {
-int n=nums.length;
-//optimal aproach 
-ArrayList<Integer> temp=new ArrayList<>();
- //storing all non zero element
- for(int i=0;i<n;i++) {
-if(nums[i]!=0)  temp.add(nums[i]);
-         }
+ int n=nums.length;
+/*List<Integer> temp=new ArrayList<>();
+for(int i=0;i<n;i++){
+if(nums[i]!=0){
+temp.add(nums[i]);}}
 int idx=0;
- for(int it:temp) {
-  nums[idx++]=it;}
-for(int i=idx;i<n;i++) {
-   nums[i]=0;}
-         
-        
+for(int it:temp){
+nums[idx++]=it;}
+for(int i=idx;i<n;i++){
+ nums[i]=0;}*/
+//storing first zeroth element
+int j=-1;
+for(int i=0;i<n;i++){
+if(nums[i]==0){
+  j=i;
+break;}}
+ if(j==-1) return;//no zeroth ellement
+for(int i=j+1;i<n;i++){
+ if(nums[i]!=0){
+   swap(nums,i,j);
+     j++;}}
+            
+
+
+
+
+
     }
 }
