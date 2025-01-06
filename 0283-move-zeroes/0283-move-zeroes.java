c50c21674f6f76ce426d44df71c19324265e4cb2
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-01-06 23:36:18 +0530

    Time: 2 ms (85.19%), Space: 46.2 MB (14.92%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,35 +1,47 @@
 class Solution {
-  public void swap(int[] arr,int i,int j){
-  int temp=arr[i];
-    arr[i]=arr[j];
-    arr[j]=temp;}
+    public void swap(int[] arr,int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
     public void moveZeroes(int[] nums) {
- int n=nums.length;
-/*List<Integer> temp=new ArrayList<>();
+   /*  int n=nums.length;
+     //TC->0(2N) SC->0(N)
+     //brute force->counting number of zerores ans then adding them to end
+     List<Integer> arr=new ArrayList<>();
+ //index to track number of elements that are added and not equal to zero
 for(int i=0;i<n;i++){
-if(nums[i]!=0){
-temp.add(nums[i]);}}
+    if(nums[i]!=0){
+        arr.add(nums[i]);
+    }
+}
+//putting all the ellements in array which are not zero
 int idx=0;
-for(int it:temp){
-nums[idx++]=it;}
-for(int i=idx;i<n;i++){
- nums[i]=0;}*/
-//storing first zeroth element
+for(int it:arr){
+ nums[idx++]=it;  
+}
+//adding all zero ellements
+while(idx<n){
+ nums[idx++]=0;
+}*/
+//optimal aproach of using two pointers
+
+int n=nums.length;
 int j=-1;
+//finding the first index that has zero
 for(int i=0;i<n;i++){
 if(nums[i]==0){
   j=i;
-break;}}
- if(j==-1) return;//no zeroth ellement
+  break;  
+}
+}
+if(j==-1) return;//no zero element present then return
+//traversing the array and swapping the zeroes to end
 for(int i=j+1;i<n;i++){
- if(nums[i]!=0){
-   swap(nums,i,j);
-     j++;}}
-            
-
-
-
-
+if(nums[i]!=0){
+    swap(nums,i,j);//it will swap zero index number to non zero index number
+j++;// j is increamented susch that next non zero element comes at this position
+}}
 
     }
 }
