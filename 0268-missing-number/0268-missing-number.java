5da0a7c01cac96046f8b93e2c40c71b50528beba
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-01-06 23:41:11 +0530

    Time: 0 ms (100%), Space: 45.1 MB (86.12%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+     //finding out sum of all numbers from 0 to n and subtracting it with sum of elements in array
+     int n=nums.length;
+     int totalSum=(n*(n+1))/2;
+     int sum=0;
+     for(int i=0;i<n;i++)  {
+        sum+=nums[i];
+     } 
+     return totalSum-sum;
+    }
+}
