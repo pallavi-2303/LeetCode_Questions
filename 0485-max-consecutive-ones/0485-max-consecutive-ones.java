cf683109a3d41533d8cf2d7dd9b9404d493b9ea4
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-01-13 17:05:57 +0530

    Time: 2 ms (94.3%), Space: 50 MB (83.55%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -1,30 +1,25 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
-   //brute force generating all subarray to find max consecutive ones
-  /* int n=nums.length;
-   int maxlen=0;
-   for(int i=0;i<n;i++) {
-    int j=i;
-    while(j<n && nums[j]!=0){
-        j++;
-    }
-    //if nums[j] becomes zero find length
-    maxlen=Math.max(maxlen,j-i);
-   } 
-   return maxlen;*/// TC->0(N2)  
-   //optimal apraoch of using 0(N) 
-   int n=nums.length;
-   int count=0;
-   int maxlen=0;
-   for(int i=0;i<n;i++){
-    count+=nums[i];//for counting total number of ones
-    if(nums[i]==0){
-      maxlen=Math.max(maxlen,count);//updating if zero comes
-      count=0;  
-    }
-   }
-   //if there are all ones in array 
-   maxlen=Math.max(maxlen,count);
-   return maxlen;
+     int n=nums.length;
+     int maxcount=0;
+     //generating all pairs 
+//      for(int i=0;i<n;i++)  {
+// int j=i;
+// while(j<n && nums[j]!=0){
+//     j++;
+// }
+// maxcount=Math.max(maxcount,j-i);
+//      } 
+//      return maxcount;
+//optimal aproch
+int sum=0;
+for(int i=0;i<n;i++){
+sum+=nums[i];
+if(sum>maxcount){
+maxcount=sum;
+}
+if(nums[i]==0) sum=0;
+}
+return maxcount;
     }
 }
