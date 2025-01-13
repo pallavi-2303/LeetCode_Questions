c60f8e452079a41521dd48b3d0f05ebf0bdb3659
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-01-13 17:37:08 +0530

    Time: 5 ms (56.22%), Space: 44.5 MB (95.08%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,26 +1,24 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-      int n=nums.length;
-      //brute force ->generating all the subarray and finding out the sum
-     /* int[] ans={-1};
-      for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-       if(nums[i]+nums[j]==target) {
-        ans=new int[]{i,j};
-       }    
-        }
-      }*/
-      //optimal aproach->using a hashmap to store previous element
-     HashMap<Integer,Integer> mp=new HashMap<>();
+     int n=nums.length;
      int[] ans={-1};
-    for(int i=0;i<n;i++) {
-    int rem=target-nums[i];
-    if(mp.containsKey(rem)){
-    //means arr[i]+rem==target
-    ans=new int[]{mp.get(rem),i};
+     //brute force->Generating all subarray
+    //  for(int i=0;i<n;i++) {
+    // for(int j=i+1;j<n;j++){
+    // if(nums[i]+nums[j]==target){
+    // ans=new int[]{i,j};
+    // }
+    // }
+    //  }  
+    //  return ans;
+    HashMap<Integer,Integer> mp=new HashMap<>();
+    for(int i=0;i<n;i++){
+    int remele=target-nums[i];
+    if(mp.containsKey(remele)){
+    ans=new int[]{mp.get(remele),i};
     }
     mp.put(nums[i],i);
     }
-return ans;
+    return ans;
     }
 }
