6bb5eed59989b1482b7d76aa58f5988e734bf6ba
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-01-16 15:58:37 +0530

    Time: 10 ms (82.73%), Space: 48.5 MB (56.33%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,18 +1,22 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-int n=g.length;
-int m=s.length;
-  Arrays.sort(g);
-  Arrays.sort(s);
- int child=0;
- int cookie=0;
- while(child<n && cookie<m) {
- if(g[child]<=s[cookie]) {
-  child++;}
-   //if the cookie satisfy the children or not we need to move the cookie pointer
-    cookie++ ;}
-        return child;
-              
-        
+       int n=g.length;
+       int m=s.length;
+       //since s is children and g is greed factor we will first sort both of thsse array
+       Arrays.sort(s) ;
+       Arrays.sort(g);
+       int cookie=0;
+       int child=0;
+       while(child<n && cookie<m){
+        if(s[cookie]>=g[child]){
+        //we can assign this to current child
+        child++;
+        cookie++;
+        }
+        else {
+            cookie++;
+        }
+       }
+       return child;
     }
 }
