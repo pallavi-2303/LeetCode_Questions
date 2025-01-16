a9637aee02049c2068376298dc559b92c29369cd
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-01-16 20:17:47 +0530

    Time: 1 ms (44.24%), Space: 45.5 MB (8.89%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,4 +1,33 @@
 class Solution {
+    public class pair{
+        int row;
+        int col;
+        public pair(int row,int col){
+            this.row=row;
+            this.col=col;
+        }
+    }
+    public void bfs(int i,int j,int[][] image,int[][] vis,int color,int inicolor,int[] delrow,int[] delcol){
+          int n=image.length;
+    int m=image[0].length;
+    Queue<pair> q=new LinkedList<>();
+    q.add(new pair(i,j));
+    vis[i][j]=color;
+    while(!q.isEmpty()){
+    pair it=q.poll();
+    int row=it.row;
+    int col=it.col;
+    //traversing the 4 neighbour
+    for(int idx=0;idx<4;idx++) {
+    int nrow=row+delrow[idx];
+    int ncol=col+delcol[idx];
+    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]!=color && image[nrow][ncol]==inicolor){
+    vis[nrow][ncol]=color;
+    q.add(new pair(nrow,ncol));
+    }
+    }  
+    }
+    }
     public void dfs(int row,int col,int[][] image,int[][] vis,int color,int inicolor,int[] delrow,int[] delcol){
         int n=image.length;
     int m=image[0].length;
@@ -22,7 +51,9 @@ if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]!=color && image[nro
     int inicolor=image[sr][sc];
     int[] delrow={-1,1,0,0} ;
     int[] delcol={0,0,-1,1};
-    dfs(sr,sc,image,vis,color,inicolor,delrow,delcol);
+   // dfs(sr,sc,image,vis,color,inicolor,delrow,delcol);
+   //now the bfs approach
+   bfs(sr,sc,image,vis,color,inicolor,delrow,delcol);
     return vis;
     }
 }
