d2ed6cb9151cbf27731798b49a1db32c97e6a0c6
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-01-17 15:44:24 +0530

    Time: 2 ms (84.84%), Space: 45.4 MB (69.14%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -3,7 +3,7 @@ class Solution {
          int n=board.length;
      int m=board[0].length;
      vis[row][col]=1;
-     for(int i=0;i<n;i++) {
+     for(int i=0;i<4;i++) {
     int nrow=row+delrow[i];
     int ncol=col+delcol[i];
 if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && vis[nrow][ncol]==0 && board[nrow][ncol]=='O'){
@@ -21,7 +21,7 @@ if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && vis[nrow][ncol]==0 && board[nrow][n
      //pereforming the region that are on edge
      for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-        if(i==0 || j==0 ||i==n-1 || j==m-1){
+        if(i==0 || j==0 || i==n-1 || j==m-1){
         if(vis[i][j]==0 && board[i][j]=='O'){
         dfs(i,j,board,vis,delrow,delcol);
         }
