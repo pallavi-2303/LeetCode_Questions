928215c534ea71fc9f2a9084021bf312012ef15f
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-01-17 15:44:49 +0530

    Time: 3 ms (34.35%), Space: 46 MB (11.07%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,4 +1,13 @@
 class Solution {
+    public class pair{
+        int row;
+        int col;
+        public pair(int row,int col){
+            this.row=row;
+            this.col=col;
+        }
+
+    }
     public void dfs(int row,int col,char[][] board,int[][] vis,int[] delrow,int[] delcol){
          int n=board.length;
      int m=board[0].length;
@@ -10,6 +19,25 @@ if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && vis[nrow][ncol]==0 && board[nrow][n
  dfs(nrow,ncol,board,vis,delrow,delcol);
     }
      }
+    }
+    public void bfs(int i,int j,char[][] board,int[][] vis,int[] delrow,int[] delcol){
+     int n=board.length;
+     int m=board[0].length;
+Queue<pair> q=new LinkedList<>();
+q.add(new pair(i,j));
+vis[i][j]=1;
+while(!q.isEmpty()){
+    pair it=q.poll();
+    int row=it.row;
+    int col=it.col;
+for(int idx=0;idx<4;idx++){
+int nrow=row+delrow[idx];
+int ncol=col+delcol[idx];
+if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && vis[nrow][ncol]==0 && board[nrow][ncol]=='O'){
+vis[nrow][ncol]=1;
+q.add(new pair(nrow,ncol));
+}
+}}
     }
     public void solve(char[][] board) {
      //since we have to connect the region we fill perform dfs and bfs to mark the region that are on edge 
@@ -23,7 +51,8 @@ if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && vis[nrow][ncol]==0 && board[nrow][n
         for(int j=0;j<m;j++){
         if(i==0 || j==0 || i==n-1 || j==m-1){
         if(vis[i][j]==0 && board[i][j]=='O'){
-        dfs(i,j,board,vis,delrow,delcol);
+       // dfs(i,j,board,vis,delrow,delcol);
+        bfs(i,j,board,vis,delrow,delcol);
         }
         }
         }
