afa03d1ae94eeb211797bfd588606e045b6da324
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-01-22 16:03:53 +0530

    Time: 3 ms (92.53%), Space: 43.5 MB (72.72%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,28 +1,22 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-    int n1=nums1.length;
-    int n2=nums2.length; 
-    //first we will find next greater element of nums2
-    int[] nge=new int[n2];
-    Stack<Integer> st=new Stack<>();
-    for(int i=n2-1;i>=0;i--){
-        while(!st.isEmpty() && st.peek()<=nums2[i]){
-            st.pop();
-        }
-        if(st.isEmpty()) nge[i]=-1;
-        else {
-        nge[i]=st.peek();}
-        st.push(nums2[i]);
-    }
-    HashMap<Integer,Integer> mp=new HashMap<>();
-    for(int i=0;i<n2;i++){
-    mp.put(nums2[i],nge[i]);
-    }
-    int[] ans=new int[n1];
-    //putting all next greater of nums1 
-    for(int i=0;i<n1;i++){
-ans[i]=mp.get(nums1[i]);
-    }
-    return ans;
+ int n=nums1.length;
+ int m=nums2.length;
+ HashMap<Integer,Integer> mp=new HashMap<>();
+Stack<Integer> st=new Stack<>();
+mp.put(nums2[m-1],-1);
+st.push(nums2[m-1]);
+ for(int i=m-2;i>=0;i--) {
+  while(st.size()>0 && st.peek()<=nums2[i]) {
+   st.pop();}
+ if(st.size()==0) mp.put(nums2[i],-1);
+else {
+mp.put(nums2[i],st.peek());}
+  st.push(nums2[i]);}
+  int[] ans=new int[n];
+  for(int i=0;i<n;i++) {
+    ans[i]=mp.get(nums1[i]);}
+        return ans;
+        
     }
 }
