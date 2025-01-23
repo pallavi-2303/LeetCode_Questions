152fb3941fdfaaadb34e5d481c68db45f713e1fa
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-01-23 17:08:18 +0530

    Time: 117 ms (13.69%), Space: 57.6 MB (41.68%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,40 +1,38 @@
 class Solution {
+    public void NSE(int[] arr,int[] nse){
+    Stack<Integer> st=new Stack<>() ;
+    int n=arr.length;
+    for(int i=n-1;i>=0;i--) {
+    while(!st.isEmpty() && arr[st.peek()]>=arr[i]){
+        st.pop();
+    }
+    nse[i]=st.isEmpty() ? n:st.peek();
+    st.push(i);
+    }  
+    }
+      public void PSE(int[] arr,int[] pse){
+    Stack<Integer> st=new Stack<>() ;
+    int n=arr.length;
+    for(int i=0;i<n;i++) {
+    while(!st.isEmpty() && arr[st.peek()]>=arr[i]){
+        st.pop();
+    }
+    pse[i]=st.isEmpty() ? -1:st.peek();
+    st.push(i);
+    }  
+    }
     public int largestRectangleArea(int[] heights) {
-int n=heights.length;
-int[] pse=new int[n];
-int[] nse=new int[n];
-Stack<Integer> st=new Stack<>();
-//calculate pse element array
-pse[0]=-1;
-st.push(0);
-for(int i=1;i<n;i++){
- while(st.size()>0 && heights[st.peek()]>=heights[i]) {
- st.pop();}
- if(st.size()==0) pse[i]=-1;
- else {
-pse[i]=st.peek();}
-st.push(i);}
-//emptying the stack
- while(st.size()!=0) {
-  st.pop();}
- //calculate nse element array
- nse[n-1]=n;
- st.push(n-1);
- for(int i=n-2;i>=0;i--) {
- while(st.size()>0 && heights[st.peek()]>=heights[i]) {
- st.pop();}
- if(st.size()==0) nse[i]=n;
- else{
-nse[i]=st.peek();}
-st.push(i);}
-//calculating the area;
-
-int max=Integer.MIN_VALUE;
-for(int i=0;i<n;i++){
-int  area=((nse[i]-pse[i]-1)*heights[i]);
- max=Math.max(area,max);
-}
-return max;
-        
+      int n=heights.length;
+       int[] nse=new int[n];
+       int[] pse=new int[n];
+        NSE(heights,nse);
+        PSE(heights,pse);
+     int maxArea=0;
+     //calculating  area of ech rectange
+     for(int i=0;i<n;i++) {
+    int area=heights[i]*(nse[i]-pse[i]-1);
+    maxArea=Math.max(area,maxArea);
+     }
+     return maxArea;  
     }
 }
