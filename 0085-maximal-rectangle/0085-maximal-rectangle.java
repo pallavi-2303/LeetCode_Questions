32301ce023b0134bc3de813b75effb856aabe9b0
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-01-23 17:31:56 +0530

    Time: 26 ms (21.03%), Space: 45.9 MB (77.3%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -1,46 +1,56 @@
 class Solution {
-public int maxArea(int[] arr){
-int n=arr.length;
-int[] pse=new int[n];
-int[] nse=new int[n];
- Stack<Integer> st=new Stack<>();
-//calculating pse;
-for(int i=0;i<n;i++){
- while(st.size()>0 && arr[st.peek()]>=arr[i]){
-   st.pop();}
-if(st.size()==0) {
-  pse[i]=-1;}
-else{
-    pse[i]=st.peek();}
- st.push(i);}
-//emptying the stack
-  while(st.size()!=0) st.pop();
- //calculating nse
- for(int i=n-1;i>=0;i--){
- while(st.size()>0 && arr[st.peek()]>=arr[i]){
-   st.pop();}
- if(st.size()==0) nse[i]=n;
- else nse[i]=st.peek();
-  st.push(i);}
- //calculating max Area
-  int maxi=Integer.MIN_VALUE;
-for(int i=0;i<n;i++){
-int area=arr[i]*(nse[i]-pse[i]-1);
- maxi=Math.max(maxi,area);}
-        return maxi;}
+      public void NSE(int[] arr,int[] nse){
+    Stack<Integer> st=new Stack<>() ;
+    int n=arr.length;
+    for(int i=n-1;i>=0;i--) {
+    while(!st.isEmpty() && arr[st.peek()]>=arr[i]){
+        st.pop();
+    }
+    nse[i]=st.isEmpty() ? n:st.peek();
+    st.push(i);
+    }  
+    }
+      public void PSE(int[] arr,int[] pse){
+    Stack<Integer> st=new Stack<>() ;
+    int n=arr.length;
+    for(int i=0;i<n;i++) {
+    while(!st.isEmpty() && arr[st.peek()]>=arr[i]){
+        st.pop();
+    }
+    pse[i]=st.isEmpty() ? -1:st.peek();
+    st.push(i);
+    }  
+    }
+    public int largestRectangle(int[] heights){
+      int n=heights.length;
+       int[] nse=new int[n];
+       int[] pse=new int[n];
+        NSE(heights,nse);
+        PSE(heights,pse);
+     int maxArea=0;
+     //calculating  area of ech rectange
+     for(int i=0;i<n;i++) {
+    int area=heights[i]*(nse[i]-pse[i]-1);
+    maxArea=Math.max(area,maxArea);
+     }
+     return maxArea;  
+    }
     public int maximalRectangle(char[][] matrix) {
-  int n=matrix.length;
-  int m=matrix[0].length;
-  int[] heights=new int[m];
-   Arrays.fill(heights,0);
-  int maxi=Integer.MIN_VALUE;
-   for(int i=0;i<n;i++){
-  for(int j=0;j<m;j++){
- //heights is storing height of each matrix and we are treating it as 1 array
-if(matrix[i][j]=='1'){
-  heights[j]+=1;}
- else heights[j]=0;}
-maxi=Math.max(maxi,maxArea(heights));}
-            return maxi;
+     int n=matrix.length;
+     int m=matrix[0].length;
+     int maxi=0;
+     int[] heights=new int[m];
+     for(int i=0;i<n;i++) {
+    for(int j=0;j<m;j++){
+    if(matrix[i][j]=='1'){
+    heights[j]+=1;
+    }
+    else {
+    heights[j]=0;
+    }
+    }
+    maxi=Math.max(maxi,largestRectangle(heights));
+     }
+     return maxi;
     }
 }
