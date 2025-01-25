cf0dd3b282befa6e0a8eaf571fc704093e064a28
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-01-25 14:52:54 +0530

    Time: 0 ms (100%), Space: 41.6 MB (86.07%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -9,24 +9,41 @@
  * }
  */
 class Solution {
-    public int findSize(ListNode head){
-        int count=0;
-while(head!=null){
-    count++;
-    head=head.next;
+  public int CountNodes(ListNode head){
+ int count=0;
+ while(head!=null) {
+ count++;
+ head=head.next;
+ }
+ return count;
 }
-return count;
-    }
     public ListNode removeNthFromEnd(ListNode head, int n) {
-     //nth node from end means size-n+1 node from the start
-     if(head==null || (head.next==null && n==1)) return null;
-     int size=findSize(head);
-     int m=size-n+1;
-     ListNode temp=head;
-     for(int i=1;i<m-1;i++) {
-        temp=temp.next;
-     }  
-     temp.next=temp.next.next;
+  //nth Node from last means mth node from start
+/*int size=CountNodes(head);
+int m=size-n+1;
+if(m==1) return head.next;
+ListNode temp=head;
+for(int i=1;i<m-1;i++){
+  temp=temp.next;
+}
+temp.next=temp.next.next;
+            return head;*/
+ //intution -> first we move fast by n places and then fast reaches last node means by moving slow and fast at same distance means slow is at just before L-n node 
+ ListNode fast=head;
+ListNode slow=head;
+//first move fast by n places
+for(int i=1;i<=n;i++){
+ fast=fast.next;}
+if(fast==null) // n=size of node
+ return head.next;
+while(fast.next!=null){
+slow=slow.next;
+fast=fast.next;}
+
+slow.next=slow.next.next;
      return head;
+        
+            
+        
     }
 }
