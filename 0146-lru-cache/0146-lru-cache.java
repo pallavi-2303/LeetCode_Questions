637a6f50fdd50fa93d6c7cabae1b8aaf602ebbc3
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-01-26 18:12:04 +0530

    Time: 48 ms (48.64%), Space: 114.5 MB (56.54%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,53 +1,71 @@
 class LRUCache {
 Node head=new Node(0,0);
- Node tail=new Node(0,0);
- HashMap<Integer,Node> mp=new HashMap<>();
-   int capacity;
-    public LRUCache(int _capacity) {
-  head.next=tail;
-   tail.prev=head;
-     capacity=_capacity;
-        
+Node tail=new Node(0,0);
+HashMap<Integer,Node> mp=new HashMap<>();
+int capacity;
+    public LRUCache(int capacity) {
+      this.capacity=capacity;
+      tail.prev=head;
+      head.next=tail;  
     }
-    
+    public void deleteTheNode(Node node){
+    Node nextNode=node.next;
+    Node prevNode=node.prev;
+    //we have to delte the articular node 
+    nextNode.prev=prevNode;
+    prevNode.next=nextNode;
+    }
+public void insertAfterHead(Node node){
+   Node afterHead=head.next;
+   head.next=node;
+   node.prev=head;
+   node.next=afterHead;
+   afterHead.prev=node; 
+    }
     public int get(int key) {
-   if(mp.containsKey(key)) {
-   Node node=mp.get(key);
-     remove(node);
-      insert(node);
-     return node.value; }
-      else {
-        return -1;}
+//if key is not present in the map
+if(!mp.containsKey(key)) {
+return -1;
+}  
+//if key is present in map
+ Node node=mp.get(key); //it will give its value
+ deleteTheNode(node);
+ insertAfterHead(node);
+return node.value;
     }
     
     public void put(int key, int value) {
-   if(mp.containsKey(key)) {
-  remove(mp.get(key));}
-   if(mp.size()==capacity) {
-    remove(tail.prev);}
-  insert(new Node(key,value)); 
+    //first case if the key is already present then just update its value
+   if(mp.containsKey(key)){
+    Node node=mp.get(key);
+    node.value=value;
+    mp.put(key,node);
+deleteTheNode(node);
+ insertAfterHead(node);
+   }
+   else {
+if(mp.size()==capacity){
+Node node=tail.prev;
+//we have to remove this node
+deleteTheNode(node);
+mp.remove(node.key);
+}
+//Now ew have to pput this
+Node node=new Node(key,value);
+mp.put(key,node);
+insertAfterHead(node);//it is the recnt node
+   }
     }
-  public void remove(Node node) {
-  mp.remove(node.key);
-  node.prev.next=node.next;
-   node.next.prev=node.prev;}
-  public void insert(Node node) {
-  mp.put(node.key,node);
- Node headNext=head.next;
-  head.next=node;
-   headNext.prev=node;
-    node.next=headNext;
-    node.prev=head;}
-  
- public class Node {
-  Node next;
-  Node prev;
-  int key;
- int value;
-     Node(int _key,int _value) {
-    key=_key;
-     value=_value;
-         }}
+//let just first create the class of Node
+public class Node{
+    Node prev;
+    Node next;
+int key,value;
+public Node(int key,int value){
+this.key=key;
+this.value=value;
+}
+}
 }
 
 /**
