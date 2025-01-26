68963b642f7fa79cf693fefcdf8df3af8d244bb9
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-01-26 18:42:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -2,21 +2,18 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
       //we have to found the first position where element owould be present if it is not present first index it will be there
     int n=nums.length;
-    int floor=n;
+    int ans=n;
     int low=0;
     int high=n-1;
     while(low<=high) {
-    int mid=low+(high-low)/2;
-    if(nums[mid]==target) return mid;
-    else if(nums[mid]<target){
-   
-    low=mid+1;
+    int mid=low+(high-mid);
+    if(nums[mid]>=target){
+        ans=mid;
+        high=mid-1;
     }
     else {
-floor=mid;
- high=mid-1;
-    }
-    } 
-    return floor;
+        low=mid+1;
+    }}
+    return ans;
     }
 }
