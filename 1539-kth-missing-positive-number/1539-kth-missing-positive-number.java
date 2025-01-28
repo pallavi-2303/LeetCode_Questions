97d5ad94d27a7775a0a3e2672ce5693e38aa8d61
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-01-28 20:39:38 +0530

    Time: 0 ms (100%), Space: 42.7 MB (71.41%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) {
     int n=arr.length;
-    for(int i=0;i<n;i++)  {
+   /* for(int i=0;i<n;i++)  {
         if(arr[i]<=k){
             k++;
         }
@@ -9,6 +9,28 @@ class Solution {
             break;
         }
     }  
-    return k;
+    return k;*/
+    //optimal aproach of binary search as array is sorted
+//We are able to find the nearby index between which a ekement can oocur we can get the answr
+//number if missing is equal to arr[idx]-idx+1;
+//high will point to lower index 
+//answer->arr[high]+more missing->
+//arr[high]-(high+1) more->k-missing
+//k-(arr[high]-(high+1))
+//arr[high]+k-arr[high]+(high+1)
+//high+1+k
+int low=0;
+int high=n-1;
+while(low<=high) {
+int mid=low+(high-low)/2;
+int missing=arr[mid]-(mid+1);
+if(missing<k) {
+ low=mid+1;
+}
+else {
+high=mid-1;
+}
+}
+return high+1+k;//low+k;
     }
 }
