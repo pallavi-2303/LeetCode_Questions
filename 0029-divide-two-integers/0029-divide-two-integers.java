5ae7086a6f10c7e6fdde1e4f57ab0be1544426b4
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-01-29 21:45:17 +0530

    Time: 1 ms (76.25%), Space: 40.5 MB (97.38%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -1,28 +1,31 @@
 class Solution {
     public int divide(int dividend, int divisor) {
- if(dividend==divisor) return 1;
-boolean sign=true;
-if(dividend>0 && divisor<0 ) 
+if(dividend==divisor) return 1;
+    boolean sign=true;
+if(dividend>=0 && divisor<0) 
     sign=false;
-  if(dividend<0 && divisor>0) 
-      sign=false;
- long n=Math.abs((long)dividend);
- long d=Math.abs((long)divisor);
-  long ans=0;
- while(n>=d) {
+if(divisor>=0 && dividend<0)
+    sign=false;
+    long ans=0;
+  //22/3-> 3*7->7=(2^2+2^1+2^0)
+  //22-3*2^2=10
+long n=Math.abs((long)(dividend));
+long d=Math.abs((long)(divisor));
+  while(n>=d) {
  int count=0;
- while(n>=(d<<(count+1))){
+//d<<count+1->d*2raise to power of count+1;
+ while(n>=(d<<(count+1))) {
    count++;}
-   ans+=1<<count;
-    n=n-(d<<count);}
-   if(ans==(1<<31) && sign)
-       return Integer.MAX_VALUE;
-    if(ans==(1<<31) && !sign)
-        return Integer.MIN_VALUE;
- 
- 
- return sign ? (int)ans:(int)(ans*-1);
- 
+//1<<count->2 raise to power of count 
+ans+=1<<count;
+ n=n-(d<<count); }
+ if(ans==(1<<31) && sign) {
+   return Integer.MAX_VALUE;}
+ if(ans==(1<<31) && !sign) {
+   return Integer.MIN_VALUE;}
+     
+      return sign ? (int)(ans) : (int)(-1*ans);
+
  
         
     }
