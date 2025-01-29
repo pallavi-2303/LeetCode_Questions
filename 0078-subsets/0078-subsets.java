1ebd7fda58c88a0880363658a7bad16150d78cf4
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-01-29 16:22:29 +0530

    Time: 0 ms (100%), Space: 42.7 MB (81.85%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,16 +1,39 @@
 class Solution {
+// public void findSubsets(int idx,int[] nums,List<Integer> temp,List<List<Integer>> res,int size){
+//     //base case
+//     if(idx==size){
+//         res.add(new ArrayList<>(temp));}
+//         return;
+//     }
+//     //let us pick the current element index
+//    temp.add(nums[idx]);
+//     //function call for next elelement
+//     findSubsets(idx+1,nums,temp,res,size);
+//     //after the idx reches n and it priints the first subsequence remove the lst elelemt 
+//     temp.remove(temp.size()-1);
+//     //not pick case for the current index
+//     findSubsets(idx+1,nums,temp,res,size);
+// }
     public List<List<Integer>> subsets(int[] nums) {
- int n=nums.length;
- List<List<Integer>> ans=new ArrayList<>();
- int subsets=1<<n;
-for(int num=0;num<subsets;num++) {
- List<Integer> subset=new ArrayList<>();
-  for(int i=0;i<n;i++) {
-   //check whether ith bit is set in given number or not 
-  if((num & (1<<i))>0) {
-   subset.add(nums[i]);}
-      }
- ans.add(subset);}
-   return ans;
+    List<List<Integer>> ans=new ArrayList<>() ;
+   // List<Integer> temp=new ArrayList<>();
+   /* int n=nums.length;
+    findSubsets(0,nums,temp,ans,n);  
+    return ans;*/
+    //power set
+    int n=nums.length;
+  int subsets=1<<n;
+  //finding out for for the numbers from 0 to 7  wheater the ith bit is set or not
+  for(int num=0;num<subsets;num++) {
+List<Integer> temp=new ArrayList<>();
+for(int i=0;i<n;i++){
+//check whewgetr the ith bit is set for givrn number or not
+if((num&(1<<i))>0){
+temp.add(nums[i]);
+}
+}
+ans.add(temp);
+  }
+  return ans;
     }
 }
