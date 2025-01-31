c8249ff420d2410d3fca4e6eac74554d30590d8e
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-02-01 01:44:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -9,15 +9,24 @@ class Solution {
     }
     public int tabu(List<Integer> arr){
     int n=arr.size();
-    int[] dp=new int[n];
-    dp[0]=arr.get(0);
+    //int[] dp=new int[n];
+   // dp[0]=arr.get(0);
+   int prev=arr.get(0);
+   int prev2=0;
     for(int idx=1;idx<n;idx++){
     int take=arr.get(idx);
-    if(idx>1) take+=dp[idx-2];
-    int nottake=dp[idx-1];
-    dp[idx]=Math.max(take,nottake);
+    if(idx>1) 
+    //take+=dp[idx-2];
+    take+=prev2;
+   // int nottake=dp[idx-1];
+   nottake=prev;
+   // dp[idx]=Math.max(take,nottake);
+   int curr=Math.max(take,nottake);
+   prev2=prev;
+   prev=curr;
     }
-    return dp[n-1];
+  //  return dp[n-1];
+  return prev;
     }
     public int rob(int[] nums) {
     //since we cannot take both the houses at the same time we can take two arrays ind in one we can take 0th element and in second we can take n-1 element 
