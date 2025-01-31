f13a8c9a9e248885e3ccb453dac4ad3ab82fb61b
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-02-01 00:55:12 +0530

    Time: 0 ms (100%), Space: 41.2 MB (46.07%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,33 +1,19 @@
 class Solution {
-public int Memo(int[] arr,int idx,int[] dp) {
- if(idx<0) return 0;
- if(idx==0) return arr[0];
- if(dp[idx]!=-1) return dp[idx];
-int take=arr[idx]+Memo(arr,idx-2,dp);
-int nottake=0+Memo(arr,idx-1,dp);
-return dp[idx]=Math.max(take,nottake);}
+    public int memo(int idx,int[] nums,int[] dp){
+        //base case if we reacch the 0 index we can take all the money
+        if(idx==0) return nums[0];
+        if(dp[idx]!=-1) return dp[idx];
+    int take=nums[idx];
+    if(idx-2>=0){
+    take+=memo(idx-2,nums,dp);}
+    int nottake=memo(idx-1,nums,dp);
+    return dp[idx]=Math.max(take,nottake);
+    }
     public int rob(int[] nums) {
- int n=nums.length;
-  int[] dp=new int[n];
- //Arrays.fill(dp,-1);
-     //return Memo(nums,n-1,dp);
-// dp[0]=nums[0];
-int prev=nums[0];
-int prev2=0;
- for(int i=1;i<n;i++) {
-/* int take=nums[i];
-if(i>1) {
-   take+=dp[i-2];}
-int nottake=0+dp[i-1];
-dp[i]=Math.max(take,nottake);*/
-int take=nums[i]+prev2;
-int nottake=0+prev;
-int curr=Math.max(take,nottake);
-prev2=prev;
- prev=curr;
-}
-            //return dp[n-1];
-    return prev ;
-  
+    ///we would have two cases that either we can take particular house or not take it
+  int n=nums.length;
+   int[] dp=new int[n] ; 
+   Arrays.fill(dp,-1); 
+   return memo(n-1,nums,dp) ;
     }
 }
