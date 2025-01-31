dbe133c5bc0c56e29153e0c4bdcfe06de0025830
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-02-01 01:43:44 +0530

    Time: 0 ms (100%), Space: 41.3 MB (28.41%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -7,6 +7,18 @@ class Solution {
     int nottake=memo(idx-1,arr,dp);
     return dp[idx]=Math.max(take,nottake);
     }
+    public int tabu(List<Integer> arr){
+    int n=arr.size();
+    int[] dp=new int[n];
+    dp[0]=arr.get(0);
+    for(int idx=1;idx<n;idx++){
+    int take=arr.get(idx);
+    if(idx>1) take+=dp[idx-2];
+    int nottake=dp[idx-1];
+    dp[idx]=Math.max(take,nottake);
+    }
+    return dp[n-1];
+    }
     public int rob(int[] nums) {
     //since we cannot take both the houses at the same time we can take two arrays ind in one we can take 0th element and in second we can take n-1 element 
     int n=nums.length;
@@ -17,11 +29,15 @@ class Solution {
     if(i!=n-1) temp1.add(nums[i]);
     if(i!=0) temp2.add(nums[i]);
     }
-    int[] dp=new int[n-1];
-    Arrays.fill(dp,-1);
-    int ans1=memo(n-2,temp1,dp);
-    Arrays.fill(dp,-1);
-    int ans2=memo(n-2,temp2,dp);
-    return Math.max(ans1,ans2);
+    // int[] dp=new int[n-1];
+    // Arrays.fill(dp,-1);
+    // int ans1=memo(n-2,temp1,dp);
+    // Arrays.fill(dp,-1);
+    // int ans2=memo(n-2,temp2,dp);
+    // return Math.max(ans1,ans2);
+    //tabulation
+    int ans=tabu(temp1);
+    int ans2=tabu(temp2);
+    return Math.max(ans,ans2);
     }
 }
