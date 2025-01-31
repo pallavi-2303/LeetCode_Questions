0dda567de7e01e61af42bb90f541a02eddbda27a
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-01-31 23:15:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -8,10 +8,16 @@ return dp[n]=memo(n-1,dp)+memo(n-2,dp);}
       int[] dp=new int[n+1];
 //Arrays.fill(dp,-1);
 //return memo(n,dp);
-dp[0]=1;
-dp[1]=1;
+// dp[0]=1;
+// dp[1]=1;
+int prev2=1;
+int prev=1;
 for(int i=2;i<=n;i++){
-dp[i]=dp[i-1]+dp[i-2];}
-return dp[n];  
+//dp[i]=dp[i-1]+dp[i-2];}
+curr=prev+prev2;
+prev2=prev;
+prev=curr;}
+//return dp[n]; 
+return prev; 
     }
 }
