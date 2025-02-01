cd7df16073ab60c581793bacddff8da5a4437a7b
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-02-02 01:29:05 +0530

    Time: 5 ms (90.71%), Space: 44.4 MB (33.84%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,4 +1,35 @@
 class Solution {
+    public int findSumTabu(int[] arr,int target) {
+ int n=arr.length;
+ //int[][] dp=new int[n][target+1];
+ int[] prev=new int[target+1];
+ int[] curr=new int[target+1];
+ if(arr[0]==0)
+ //dp[0][0]=2;
+ prev[0]=2;
+ else {
+ //dp[0][0]=1;
+ prev[0]=1;}
+ if(arr[0]!=0 && arr[0]<=target)
+ //dp[0][arr[0]]=1;
+ prev[arr[0]]=1;
+for(int i=1;i<n;i++) {
+ curr[0]=1;
+for(int tar=0;tar<=target;tar++){
+int pick=0;
+if(arr[i]<=tar)
+//pick=dp[i-1][tar-arr[i]];
+pick=prev[tar-arr[i]];
+//int notpick=dp[i-1][tar];
+int notpick=prev[tar];
+//dp[i][tar]=(pick+notpick);
+curr[tar]=(pick+notpick);
+}
+prev=curr.clone();
+}
+//return dp[n-1][target];
+return prev[target];
+}
     public int findSum(int idx,int[] arr,int target,int[][] dp) {
 if(idx==0) {
 if(arr[0]==0 && target==0)
@@ -27,6 +58,6 @@ int target2=(total-target)/2;
 
 int[][] dp=new int[n][target2+1];
 for(int[] a:dp) Arrays.fill(a,-1);
-return findSum(n-1,nums,target2,dp);
+return findSumTabu(nums,target2);
     }
 }
