da1bc63c97edb89bcb384619f36385d41e422ce2
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-02-02 01:08:55 +0530

    Time: 18 ms (44.49%), Space: 44.8 MB (46.73%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,24 +1,50 @@
 class Solution {
-    public int memo(int idx,int[] arr,int target,int[][] dp) {
-//we will go till zero index
-if(idx==0) {
-if(target%arr[0]==0) 
-return target/arr[0];//number of coins to form the answer
-else return (int)(1e9);//if not possible to form the target
-}
-if(dp[idx][target]!=-1) return dp[idx][target];
-//if we take a particular index for
-int pick=(int)1e9;
-if(arr[idx]<=target)
-pick=1+memo(idx,arr,target-arr[idx],dp);
-int notpick=0+memo(idx-1,arr,target,dp);
-return dp[idx][target]=Math.min(pick,notpick);
-}
-    public int coinChange(int[] arr, int amount) {
-       int n=arr.length;
+ public int tabuSum(int[] arr,int sum) {
+int n=arr.length;
+//int[][] dp=new int[n][sum+1];
+int[] prev=new int[sum+1];
+int[] curr=new int[sum+1];
+//base case
+ //idx==0;
+for(int tar=0;tar<=sum;tar++) {
+ if(tar%arr[0]==0) 
+ // dp[0][tar]=tar/arr[0];
+  prev[tar]=tar/arr[0];
+ //else dp[0][tar]=(int)(1e9);}
+  else prev[tar]=(int)(1e9);}
+for(int i=1;i<n;i++) {
+for(int target=0;target<=sum;target++) {
+ //int nottake=0+dp[i-1][target];
+int nottake=0+prev[target];
+ int take=(int)(1e9);
+ if(arr[i]<=target)
+ //take=1+dp[i][target-arr[i]];
+ take=1+curr[target-arr[i]];
+//dp[i][target]=Math.min(take,nottake);
+curr[target]=Math.min(take,nottake);}
+prev=curr;}
+return prev[sum];}
+   // return dp[n-1][sum];     }
+ public int memoSum(int idx,int[] arr,int target,int[][] dp) {
+  if(idx==0) {
+ if(target%arr[0]==0) 
+  return target/arr[0];
+else return (int)(1e9);}
+ if(dp[idx][target]!=-1)
+         return dp[idx][target];
+ int nottake=0+memoSum(idx-1,arr,target,dp);
+int take=(int)(1e9);
+if(arr[idx]<=target) 
+ take=1+memoSum(idx,arr,target-arr[idx],dp);
+  return dp[idx][target]=Math.min(take,nottake);}
+    public int coinChange(int[] coins, int amount) {
+int n=coins.length;
 int[][] dp=new int[n][amount+1];
-for(int[] a:dp) Arrays.fill(a,-1);
-int ans= memo(n-1,arr,amount,dp);
-return ans==(int)1e9 ? -1 :ans;
+for(int[] a:dp) {
+ Arrays.fill(a,-1);}
+//int ans=memoSum(n-1,coins,amount,dp);
+ int ans=tabuSum(coins,amount);
+ return ans==(int)(1e9)?-1:ans;           
+        
     }
 }
