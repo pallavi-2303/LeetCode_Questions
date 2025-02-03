3c6db3c6d9013d468fbf1e04a52f36f7265fe4fb
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-02-03 22:53:13 +0530

    Time: 22 ms (24.32%), Space: 51.2 MB (53.3%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,43 +1,47 @@
 class Solution {
-public int disSubs(int i,int j,String s,String t,int[][] dp){
-//if(j<0) return 1;
-//if(i<0) return 0;
-if(j==0) return 1;
-if(i==0) return 0;
- if(dp[i][j]!=-1) 
-   return dp[i][j];
-//if character matches
- if(s.charAt(i-1)==t.charAt(j-1))
- return dp[i][j]=disSubs(i-1,j-1,s,t,dp)+disSubs(i-1,j,s,t,dp);
-else return dp[i][j]=disSubs(i-1,j,s,t,dp);}
-    public int numDistinct(String s, String t) {
-int n1=s.length();
-int n2=t.length();
+public int findCount(int idx1,int idx2,String s1,String s2,int[][] dp) {
+//base case
+if(idx2==0) return 1;//one subsequnce if j exhaust
+if(idx1==0) return 0;
+
+if(dp[idx1][idx2]!=-1) return dp[idx1][idx2];
+if(s1.charAt(idx1-1)==s2.charAt(idx2-1)){
+//if the String matches we can match it with i or we cannot match it with this characters and give sum of both 
+return findCount(idx1-1,idx2-1,s1,s2,dp) +findCount(idx1-1,idx2,s1,s2,dp);
+}
+else
+return findCount(idx1-1,idx2,s1,s2,dp);
+}
+    public int numDistinct(String s1, String s2) {
+        int n1=s1.length();
+int n2=s2.length();
 int[][] dp=new int[n1+1][n2+1];
- //for(int[] a:dp) Arrays.fill(a,-1);
-//Shifting the indexes
-//return disSubs(n1-1,n2-1,s,t,dp);
-//return disSubs(n1,n2,s,t,dp);
- //base case
+// for(int[] a:dp)
+// Arrays.fill(a,-1);
+// return findCount(n1,n2,s1,s2,dp);
+//base case for dp
 int[] prev=new int[n2+1];
 int[] curr=new int[n2+1];
+
 for(int j=0;j<=n2;j++)
-// dp[0][j]=0;
- prev[j]=0;
+dp[0][j]=0;
+//prev[0]=0;
 for(int i=0;i<=n1;i++)
-//dp[i][0]=1;
- prev[0]=1;
-for(int i=1;i<=n1;i++) {
- curr[0]=1;
-for(int j=1;j<=n2;j++) {
-if(s.charAt(i-1)==t.charAt(j-1))
- //match with another character of given j
- curr[j]=prev[j-1]+prev[j];
-//dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
-//else dp[i][j]=dp[i-1][j];}}
-else curr[j]=prev[j];}
- prev=curr.clone();}
- //return dp[n1][n2];
-   return prev[n2];
+dp[i][0]=1;
+//prev[j]=1;                                                                                   
+for(int i=1;i<=n1;i++){
+for(int j=1;j<=n2;j++){
+if(s1.charAt(i-1)==s2.charAt(j-1)){
+dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+curr[j]=prev[j-1]+prev[j];
+}
+else
+dp[i][j]=dp[i-1][j];
+//curr[j]=prev[j];
+}
+//prev=curr.clone();
+}
+return dp[n1][n2];
+//return prev[n2];
     }
 }
