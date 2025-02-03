b0d6c01f69e56bba534afc6037aa9f27a632a689
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-02-03 15:03:03 +0530

    Time: 55 ms (43.27%), Space: 45 MB (96.29%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,22 +1,40 @@
 class Solution {
-    public int findLCS(int idx1,int idx2,String s,String t,int[][] dp){
-    //base case out of bound case
-    if(idx1==0 || idx2==0) return  0;
-    if(dp[idx1][idx2]!=-1) return dp[idx1][idx2];
-    //we the chracter mathces
-    if(s.charAt(idx1-1)==t.charAt(idx2-1))
-    return 1+findLCS(idx1-1,idx2-1,s,t,dp);
-    //not matchh case
-    else return Math.max(findLCS(idx1-1,idx2,s,t,dp),findLCS(idx1,idx2-1,s,t,dp));
-    }
+ public int lcs(int i,int j,String s,String t,int[][] dp){
+if(i==0 || j==0)
+  return 0;
+ if(dp[i][j]!=-1)
+    return dp[i][j];
+if(s.charAt(i-1)==t.charAt(j-1))
+ return dp[i][j]=1+lcs(i-1,j-1,s,t,dp);
+else
+ return dp[i][j]=Math.max(lcs(i-1,j,s,t,dp),lcs(i,j-1,s,t,dp));}
     public int longestPalindromeSubseq(String s) {
-      //since we have to find longest pallaindromic subesequence we need to reverse the string the string and then we can find pallindrome;
-String t=new StringBuilder(s).reverse().toString();
-
-
+StringBuilder st=new StringBuilder(s);
+st.reverse();
+String t=st+"";
 int n=s.length();
-int[][] dp=new int[n+1][n+1];
-for(int[] a:dp) Arrays.fill(a,-1);
-return findLCS(n,n,s,t,dp);
+//int[][] dp=new int[n+1][n+1];
+int[] prev=new int[n+1];
+int[] curr=new int[n+1];
+/*for(int[] a:dp)
+  Arrays.fill(a,-1);
+  return lcs(n,n,s,t,dp);*/
+for(int i=0;i<=n;i++) //dp[i][0]=0;
+ prev[0]=0;
+for(int j=0;j<=n;j++)
+   //dp[0][j]=0;
+  prev[j]=0;
+for(int i=1;i<=n;i++) {
+for(int j=1;j<=n;j++) {
+if(s.charAt(i-1)==t.charAt(j-1))
+ //dp[i][j]=1+dp[i-1][j-1];
+  curr[j]=1+prev[j-1];
+else
+//dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);}}
+ curr[j]=Math.max(prev[j],curr[j-1]);}
+  prev=curr.clone();}
+       // return dp[n][n];
+     return prev[n];
+        
     }
 }
