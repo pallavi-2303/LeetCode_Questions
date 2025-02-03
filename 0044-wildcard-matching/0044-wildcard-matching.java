9adbf5c20613e4f0bfabb777db4ac32481e41087
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-02-03 21:06:47 +0530

    Time: 14 ms (78.01%), Space: 49.1 MB (9.28%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,81 +1,34 @@
 class Solution {
-public boolean isAllStar(String s,int i) {
- for(int idx=1;idx<=i;idx++){
-  if(s.charAt(idx-1)!='*'){
-     return false;} }
-    return true;} 
-public boolean TabuMatch(String s,String t) {
-int n1=s.length();
-int n2=t.length();
-boolean[][] dp=new boolean[n1+1][n2+1];
+public int findMatch(int i,int j,String s1,String s2,int[][] dp) {
 //base case
-dp[0][0]=true;
-for(int j=1;j<=n2;j++)
- dp[0][j]=false;
-for(int i=1;i<=n1;i++) {
- dp[i][0]=isAllStar(s,i);}
- 
- 
-    
-
- 
- for(int i=1;i<=n1;i++) {
-  for(int j=1;j<=n2;j++) {
- if(s.charAt(i-1)==t.charAt(j-1) || s.charAt(i-1)=='?')
- dp[i][j]=dp[i-1][j-1];
- else {
-  if(s.charAt(i-1)=='*')
-  dp[i][j]=dp[i-1][j]|| dp[i][j-1];
-else dp[i][j]=false;}} }
-  return dp[n1][n2];}
-public int Match2(int i,int j,String s,String t,int[][] dp){
- if(i==0 && j==0)
-    return 1;
-if(i==0 && j>0)
-  return 0;
-  if(j==0 && i>0) {
+if(i==0 && j==0)
+return 1;
+if(i==0 && j>0) 
+return 0;
+if(i>0 && j==0) {
 for(int idx=1;idx<=i;idx++){
-  if(s.charAt(idx-1)!='*')
-    return 0;}
-   return 1;}
- if(dp[i][j]!=-1) 
-         return dp[i][j];
- if(s.charAt(i-1)==t.charAt(j-1) || s.charAt(i-1)=='?')
- return dp[i][j]=Match2(i-1,j-1,s,t,dp);
-if(s.charAt(i-1)=='*')
- return dp[i][j]=Match2(i-1,j,s,t,dp)==1 || Match2(i,j-1,s,t,dp)==1 ? 1 : 0;
-   return dp[i][j]=0;}
-public int Match(int i,int j,String s,String t,int[][] dp){
- //base case
- //if both string is exhausted
-   if(i<0 && j<0)
-      return 1;
- //if i is exhusted and j remain 
- if(i<0 && j>=0)
-  return 0;
-// if j<0 and i!=0;
- if(i>=0 && j<0){
-for(int idx=0;idx<=i;idx++) {
- if(s.charAt(idx)!='*')
-     return 0;}
- return 1;}
+if(s1.charAt(idx-1)!='*') return 0;
+}
+return 1;}
 if(dp[i][j]!=-1) return dp[i][j];
-//if charcter mateched or ? is present at i it can match with 1 charcter
- if(s.charAt(i)==t.charAt(j) || s.charAt(i)=='?')
- return dp[i][j]=Match(i-1,j-1,s,t,dp);
-//if character at i is * one time we treat it as empty and another time as remover charcter
-if(s.charAt(i)=='*')
- return dp[i][j]=(Match(i-1,j,s,t,dp)==1) || (Match(i,j-1,s,t,dp)==1) ? 1 :0;
- //no one matched return false
- return 0;}
-    public boolean isMatch(String s, String p) {
-int n1=p.length();
-int n2=s.length();
-int[][] dp=new int[n1+1][n2+1];
-// for(int[] a:dp)
-  //Arrays.fill(a,-1);
- //return Match(n1-1,n2-1,p,s,dp)==1 ? true : false;
-  //return Match2(n1,n2,p,s,dp)==1 ? true : false;   
-  return TabuMatch(p,s);
+//if the character matches or if there is a question mark in //here the wild card will only be prsent in String s1
+if(s1.charAt(i-1)==s2.charAt(j-1) || s1.charAt(i-1)=='?'){
+  //reduce both string
+ return dp[i][j]=findMatch(i-1,j-1,s1,s2,dp);
+}
+//if there is a wildcard thatit *
+else if(s1.charAt(i-1)=='*') {
+//we can take it as empty string or match it with one character
+return dp[i][j]= (findMatch(i-1,j,s1,s2,dp)==1 || findMatch(i,j-1,s1,s2,dp)==1) ? 1 : 0;
+}
+else return dp[i][j]=0;//at end if no match found 
+}
+    public boolean isMatch(String s1, String s2) {
+       int n1=s1.length();
+int n2=s2.length();
+int[][] dp=new int[n2+1][n1+1];
+for(int[] a:dp)
+Arrays.fill(a,-1);
+ return findMatch(n2,n1,s2,s1,dp)==1 ? true :false;
     }
 }
