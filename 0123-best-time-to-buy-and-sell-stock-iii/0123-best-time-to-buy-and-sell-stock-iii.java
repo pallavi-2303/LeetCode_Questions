646c5ddd254c58e1fe95f184cc1b8b72074ff6a4
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-02-04 16:15:11 +0530

    Time: 68 ms (30.89%), Space: 63.9 MB (34.8%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -16,10 +16,33 @@ return dp[idx][buy][cap]=profit;
     public int maxProfit(int[] prices) {
      int n=prices.length;
 int[][][] dp=new int[n+1][2][3];
+// for(int i=0;i<=n;i++){
+// for(int j=0;j<2;j++){
+// for(int k=0;k<3;k++){
+// dp[i][j][k]=-1;}}}
+// return maxprofit(0,1,2,prices,dp); 
+//for any index and buy if cap is 0
 for(int i=0;i<=n;i++){
-for(int j=0;j<2;j++){
-for(int k=0;k<3;k++){
-dp[i][j][k]=-1;}}}
-return maxprofit(0,1,2,prices,dp);   
+for(int buy=0;buy<=1;buy++){
+dp[i][buy][0]=0;}}
+//for index n
+for(int buy=0;buy<=1;buy++){
+for(int cap=0;cap<=2;cap++){
+dp[n][buy][cap]=0;}}
+for(int i=n-1;i>=0;i--){
+for(int buy=0;buy<=1;buy++){
+for(int cap=1;cap<=2;cap++){
+int profit=0;
+if(buy==1) {
+profit=Math.max(-prices[i]+dp[i+1][0][cap],0+dp[i+1][1][cap]);
+}
+else {
+profit=Math.max(prices[i]+dp[i+1][1][cap-1],0+dp[i+1][0][cap]);
+}
+dp[i][buy][cap]=profit;
+}
+}
+}
+return dp[0][1][2];  
     }
 }
