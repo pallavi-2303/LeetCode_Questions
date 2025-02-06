506cd9ed1a8aecbb6f878d65d387f3b529bd0981
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-02-06 16:23:36 +0530

    Time: 7 ms (55.21%), Space: 42.1 MB (78.36%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -18,8 +18,22 @@ return dp[i]=maxi;
     public int maxSumAfterPartitioning(int[] arr, int k) {
       //this problem is a type of front partioning
       int n=arr.length;
-      int[] dp=new int[n];
-      Arrays.fill(dp,-1) ;
-      return findMax(0,arr,k,dp) ;
+      int[] dp=new int[n+1];
+    //   Arrays.fill(dp,-1) ;
+    //   return findMax(0,arr,k,dp) ;
+    for(int i=n-1;i>=0;i--){
+    int maxi=-(int)1e9;
+int sum=0;
+int len=0;
+int max=-1;
+for(int j=i;j<Math.min(i+k,n);j++){
+len++;
+max=Math.max(max,arr[j]);
+sum=len*max+dp[j+1];
+maxi=Math.max(maxi,sum);
+}
+dp[i]=maxi;
+    }
+    return dp[0];
     }
 }
