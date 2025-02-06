5adf28dc029248f0548c07f87751109d65e937e1
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-02-07 04:38:34 +0530

    Time: 0 ms (100%), Space: 41.4 MB (95.94%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -13,8 +13,24 @@ return dp[i][j]=up+left;
     int n=mat.length;
     int m=mat[0].length;
      int[][] dp=new int[n][m];
-for(int[] a:dp)
-Arrays.fill(a,-1);
-return findPath(n-1,m-1,mat,dp);   
+// for(int[] a:dp)
+// Arrays.fill(a,-1);
+// return findPath(n-1,m-1,mat,dp);   
+for(int i=0;i<n;i++){
+for(int j=0;j<m;j++){
+if(mat[i][j]==1) dp[i][j]=0;
+else if(i==0 && j==0){
+dp[i][j]=1;
+}
+else {
+ int up=0;
+ int left=0;
+ if(i>0) up=dp[i-1][j];
+ if(j>0) left=dp[i][j-1];
+ dp[i][j]=up+left;
+}
+}
+}
+return dp[n-1][m-1];
     }
 }
