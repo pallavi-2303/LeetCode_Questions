b4c0d0bd47ed2a25dbcfaf16c960e39358fa21f8
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-02-07 04:43:16 +0530

    Time: 6 ms (14.52%), Space: 47.4 MB (88.93%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -13,8 +13,22 @@ class Solution {
       int n=mat.length;
       int m=mat[0].length;
       int[][] dp=new int[n][m];
-for(int[] a:dp)
-Arrays.fill(a,-1);
-return findMinSum(n-1,m-1,mat,dp);  
+// for(int[] a:dp)
+// Arrays.fill(a,-1);
+// return findMinSum(n-1,m-1,mat,dp);  
+for(int i=0;i<n;i++){
+for(int j=0;j<m;j++){
+if(i==0 && j==0) 
+dp[i][j]=mat[0][0];
+else {
+int up=(int)(1e9);
+int left=(int)(1e9);
+if(i>0) up=mat[i][j]+dp[i-1][j];
+if(j>0) left=mat[i][j]+dp[i][j-1];
+dp[i][j]=Math.min(up,left);
+}
+}
+}
+return dp[n-1][m-1];
     }
 }
