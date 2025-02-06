8312fc3f63cd5a9c65d4592fe035380eb93bc5f1
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-02-07 04:34:01 +0530

    Time: 0 ms (100%), Space: 40.8 MB (25.95%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -10,8 +10,22 @@ return dp[i][j]=up+left;
 }
     public int uniquePaths(int n, int m) {
         int[][] dp=new int[n][m];
-for(int[] a:dp)
-Arrays.fill(a,-1);
-return findPath(n-1,m-1,dp);
+//for(int[] a:dp)
+// Arrays.fill(a,-1);
+// return findPath(n-1,m-1,dp);
+for(int i=0;i<n;i++){
+for(int j=0;j<m;j++){
+if(i==0 && j==0)
+dp[i][j]=1;
+else {
+int up=0;
+int left=0;
+if(i>0) up=dp[i-1][j];
+if(j>0) left=dp[i][j-1];
+dp[i][j]=up+left;
+}
+}
+}
+return dp[n-1][m-1];
     }
 }
