d0f64d82f5a93a8a1dc97ac266c43273dc6d5ce2
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-02-07 17:56:59 +0530

    Time: 9 ms (21.74%), Space: 45.4 MB (63.84%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,68 +1,78 @@
 class Solution {
- public int memoPath(int i,int j,int[][] matrix,int[][] dp) {
-int m=matrix[0].length;
- //out of bound condition
- if(j<0 || j>=m)  {
-    return (int)(1e9); }
- //base case
- if(i==0) {
-  return matrix[0][j];}
- if(dp[i][j]!=-1) 
-     return dp[i][j];
-int up=matrix[i][j]+memoPath(i-1,j,matrix,dp);
-int left=matrix[i][j]+memoPath(i-1,j-1,matrix,dp);
-int right=matrix[i][j]+memoPath(i-1,j+1,matrix,dp);
-return dp[i][j]=Math.min(up,Math.min(left,right));}
-    public int minFallingPathSum(int[][] matrix) {
-int n=matrix.length;
-int m=matrix[0].length;
-int[][] dp=new int[n][m];
-/*for(int[] a:dp) {
-   Arrays.fill(a,-1);}
-//we have to perform the memoization for every column as it has variable ending and starting point
- int mini=(int)(1e9);
-for(int j=0;j<m;j++) {
-mini=Math.min(mini,memoPath(n-1,j,matrix,dp));}
-     return mini;*/
- //Tabulation
-  /*for(int j=0;j<m;j++) {
- dp[0][j]=matrix[0][j];}
-for(int i=1;i<n;i++) {
-for(int j=0;j<m;j++) {
-int up=(int)(1e9);
-int left=(int)(1e9);
-int right=(int)(1e9);
-up=matrix[i][j]+dp[i-1][j];
-if(j>0) left=matrix[i][j]+dp[i-1][j-1];
-if(j<m-1) right=matrix[i][j]+dp[i-1][j+1];
-dp[i][j]=Math.min(up,Math.min(left,right));}}
-//it has variable ending point therefore we take min of all path
-int mini=(int)(1e9);
- for(int j=0;j<m;j++) {
- mini=Math.min(mini,dp[n-1][j]);}
-            return mini;*/
-//space optimization
-int[] prev=new int[m];
-//base case
-for(int j=0;j<m;j++) {
-prev[j]=matrix[0][j];}
-for(int i=1;i<n;i++) {
-int[] curr=new int[m];
-for(int j=0;j<m;j++) {
-int up=(int)(1e9);
-int left=(int)(1e9);
-int right=(int)(1e9);
-up=matrix[i][j]+prev[j];
-if(j>0) left=matrix[i][j]+prev[j-1];
-if(j<m-1) right=matrix[i][j]+prev[j+1];
-curr[j]=Math.min(up,Math.min(left,right));}
-       prev=curr;}
-  int mini2=(int)(1e9);
-  for(int j=0;j<m;j++) {
-  mini2=Math.min(mini2,prev[j]);}
-      return mini2;
-
-
- 
+  public int findSum(int i, int j, int[][] mat, int[][] dp) {
+    int n = mat.length;
+    int m = mat[0].length;
+    if (j < 0 || j >= m)
+      return (int) 1e9;
+    if (i == 0) {
+      return mat[i][j];
     }
+    if (dp[i][j] != -1)
+      return dp[i][j];
+    int up = mat[i][j] + findSum(i - 1, j, mat, dp);
+    int left = mat[i][j] + findSum(i - 1, j - 1, mat, dp);
+    int right = mat[i][j] + findSum(i - 1, j + 1, mat, dp);
+    return dp[i][j] = Math.min(up, Math.min(left, right));
+  }
+
+  public int minFallingPathSum(int[][] mat) {
+    // Since it has unfixed starting point and unfixed ending point
+    int n = mat.length;
+    int m = mat[0].length;
+    int[][] dp = new int[n][m];
+    // for (int[] a : dp)
+    //   Arrays.fill(a, -1);
+    // int mini = (int) 1e9;
+    // for (int j = 0; j < m; j++) {
+    //   mini = Math.min(mini, findSum(n - 1, j, mat, dp));
+    // }
+    // return mini;
+    //base case
+for(int j=0;j<m;j++){
+dp[0][j]=mat[0][j];
+}
+for(int i=1;i<n;i++){
+for(int j=0;j<m;j++){
+int up=mat[i][j]+dp[i-1][j];
+int left=(int)1e9;
+if(j-1>=0) 
+left=mat[i][j]+dp[i-1][j-1];
+int right=(int)1e9;
+if(j+1<m) 
+right=mat[i][j]+dp[i-1][j+1];
+dp[i][j]=Math.min(up,Math.min(left,right));
+}
+}
+int mini=(int)1e9;
+for(int j=0;j<m;j++){
+mini=Math.min(dp[n-1][j],mini);
+}
+return mini;
+/*space optimization
+int[] prev=new int[m];
+int[] curr=new int[m];
+//base case
+for(int j=0;j<m;j++){
+//dp[0][j]=mat[0][j];
+prev[j]=mat[0][j];
+}
+for(int i=1;i<n;i++){
+for(int j=0;j<m;j++){
+int up=mat[i][j]+prev[j];
+int left=(int)1e9;
+if(j-1>=0) 
+left=mat[i][j]+prev[j-1];
+int right=(int)1e9;
+if(j+1<m) 
+left=mat[i][j]+prev[j+1];
+curr[j]=Math.min(up,Math.min(left,right));
+}
+prev=curr.clone();
+}
+int mini=(int)1e9;
+for(int j=0;j<m;j++){
+mini=Math.min(prev[j],mini);
+}
+return mini;*/
+  }
 }
