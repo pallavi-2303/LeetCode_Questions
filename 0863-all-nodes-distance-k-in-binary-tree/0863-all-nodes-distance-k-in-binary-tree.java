7d243f96c2625c00ed68bbde426d8170f41be683
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-02-09 20:18:04 +0530

    Time: 13 ms (68.07%), Space: 43 MB (34.65%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -8,59 +8,58 @@
  * }
  */
 class Solution {
-        public void parentTrack(TreeNode root,HashMap<TreeNode,TreeNode> parent){
-                Queue<TreeNode> q=new LinkedList<>();
-                q.add(root);
-                while(!q.isEmpty()){
-                TreeNode curr=q.poll();
-                        if(curr.left!=null){
-                        parent.put(curr.left,curr);
-                        q.offer(curr.left);
-                        }
-                        if(curr.right!=null){
-                                parent.put(curr.right,curr);
-                                q.offer(curr.right);
-                        }
-                }
-        }
+    public void findParent(TreeNode root,HashMap<TreeNode,TreeNode> mp){
+    Queue<TreeNode> q=new LinkedList<>();
+    q.add(root);
+    while(!q.isEmpty()){
+    TreeNode node=q.poll();
+    if(node.left!=null){
+    q.add(node.left);
+    mp.put(node.left,node);
+    }
+    if(node.right!=null){
+        mp.put(node.right,node);
+        q.add(node.right);
+    }
+    }
+    }
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-     //visited node at level k
-        //we will do bfs trversalfor each node level wise 
-            HashMap<TreeNode,TreeNode> parent=new HashMap<>();
-            parentTrack(root,parent);
-           int currlevel=0;
-            Queue<TreeNode> q=new LinkedList<>();
-            //adding the target as we nneed to tracck k ditance  fromthe target
-            q.offer(target);
-            List<Integer> ans=new ArrayList<>();
-            HashMap<TreeNode,Boolean> vis=new HashMap<>();
-            vis.put(target,true);
-            while(!q.isEmpty()){
-             int n=q.size() ;
-        if(k==currlevel) break;
-                 currlevel++;
-        //traversing for given level
-                    for(int i=0;i<n;i++){
-                TreeNode curr=q.poll();
-                if(curr.left!=null && vis.get(curr.left)==null){
-                        q.offer(curr.left);
-                        vis.put(curr.left,true);
-                }
-         if(curr.right!=null && vis.get(curr.right)==null){
-                                  q.offer(curr.right);
-                                    vis.put(curr.right,true);
-                            }
-                        if(parent.get(curr)!=null && vis.get(parent.get(curr))==null) {
-                              vis.put(parent.get(curr),true) ;
-                                q.offer(parent.get(curr));
-                        }   
-                    }
-            }
-            //after level reaches to k ququw has all nodes at k distance
-            while(!q.isEmpty()){
-                TreeNode node=q.poll();
-                    ans.add(node.val);
-            }
-            return ans;
+    //so we are making the parent the parent pointer so that we can also go the parent node as it will also be at the same distance as the left and right node and we will also require the visisted array so that we not go the parnet and same node twice as it will not be at kth distance 
+//step->1 Creating the parent pointer 
+HashMap<TreeNode,TreeNode> parent=new HashMap<>();
+findParent(root,parent);
+//performing level order traversal to find the all the kth node from the target
+Queue<TreeNode> q=new LinkedList<>();
+HashMap<TreeNode,Boolean> vis=new HashMap<>();
+q.add(target);
+vis.put(target,true);
+int level=0;
+while(!q.isEmpty()){
+if(level>=k) break;
+int n=q.size();
+level++;
+for(int i=0;i<n;i++){
+//perfoming the level order traversal to find the node at kth distaNCE 
+TreeNode node=q.poll();
+if(node.left!=null && vis.get(node.left)==null){
+q.add(node.left);
+vis.put(node.left,true);
+}
+if(node.right!=null && vis.get(node.right)==null){
+q.add(node.right);
+vis.put(node.right,true);
+}
+if(parent.get(node)!=null && vis.get(parent.get(node))==null){
+q.add(parent.get(node));
+vis.put(parent.get(node),true);
+}
+}
+}
+List<Integer> ans=new ArrayList<>();
+while(!q.isEmpty()){
+TreeNode node=q.poll();
+ans.add(node.val);
+}
+return ans;
     }
 }
