1fbf2db40ef9f900847d5bd05e17b3b8a1e14197
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-02-11 00:27:18 +0530

    Time: 0 ms (100%), Space: 41.3 MB (96.13%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -14,49 +14,53 @@
  * }
  */
 class Solution {
-        public void preOrder(TreeNode root,List<Integer> res){
-                if(root==null){
-                        return;
-                }
-                //root left right
-                res.add(root.val);
-                preOrder(root.left,res);
-                preOrder(root.right,res);
-        }
+public void preOrder(TreeNode root,List<Integer> res) {
+if(root==null)
+return;
+res.add(root.val);
+preOrder(root.left,res);
+preOrder(root.right,res);
+}
     public List<Integer> preorderTraversal(TreeNode root) {
-List<Integer> res=new ArrayList<>() ;
-            //preOrder(root,res);
- //Iterative solution
-/*Stack<TreeNode> st=new Stack<>();
- if(root==null) return res;
- st.add(root);
-while(!st.isEmpty()){
-TreeNode curr=st.pop();
-res.add(curr.val);
-if(curr.right!=null) 
- st.push(curr.right);
-if(curr.left!=null)
- st.push(curr.left);}*/
+List<Integer> res=new ArrayList<>();
+// preOrder(root,res);
 if(root==null) return res;
- TreeNode curr=root;
-     while(curr!=null){
-   //if curr left is null
-    if(curr.left==null){
-    res.add(curr.val);
-    curr=curr.right;}
-    else {
- TreeNode prev=curr.left;
-  //move to last element on right
- while(prev.right!=null && prev.right!=curr){
- prev=prev.right;}
+// Stack<TreeNode> st=new Stack<>();
+// st.push(root);
+// while(!st.isEmpty()){
+//  root=st.pop();
+//  res.add(root.val);
+//  if(root.right!=null)  st.push(root.right);
+//  if(root.left!=null) st.push(root.left);
+// }
+// return res;
+//morris traversal
+//root left right
+while(root!=null){
+if(root.left==null){
+res.add(root.val);
+root=root.right;
+}
+else {
+//if root left is not null go to right mos node and connect pointer to root so that it can return back
+TreeNode prev=root.left;
+while(prev.right!=null && prev.right!=root){
+prev=prev.right;
+}
 if(prev.right==null){
-res.add(curr.val);
- prev.right=curr;
-  curr=curr.left;}
- else {
-  //if prev right point to curr
-  prev.right=null;
-  curr=curr.right;}}}
-            return res;
+//since here we are standing at the root 
+res.add(root.val);
+prev.right=root;
+root=root.left;
+}
+else {
+//if it is already coonected
+prev.right=null;
+root=root.right;
+}
+}
+}
+return res;
+        
     }
 }
