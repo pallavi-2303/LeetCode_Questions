497d3357f327916eff874fda696f847eca56421c
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-02-11 04:08:24 +0530

    Time: 5 ms (100%), Space: 45.5 MB (16.57%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,16 +9,19 @@
  */
 
 class Solution {
-     public TreeNode LCA(TreeNode root, TreeNode p, TreeNode q) {
-    if(root==null || root==p || root==q) return root;
-    TreeNode Left=LCA(root.left,p,q);
-    TreeNode Right=LCA(root.right,p,q);
-    if(Left==null) return Right;
-    else if(Right==null) return Left;
-    else return root;
-
-     }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-       return LCA(root,p,q); 
+   if(root==null)  return null;
+   //if  we cannot decide wheather to search on left or right then it will be our answer
+   if(root.val>p.val && root.val>q.val){
+    //both lie on left side
+    return lowestCommonAncestor(root.left,p,q);
+   }
+   //both lie on right side
+if(root.val<p.val && root.val<q.val){
+    return lowestCommonAncestor(root.right,p,q);
+}
+//if we cannot decide wheather to move left or right so root is answer
+return root;
+
     }
 }
