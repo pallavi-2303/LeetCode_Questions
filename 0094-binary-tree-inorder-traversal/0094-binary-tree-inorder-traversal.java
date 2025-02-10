5a5e79d34e499842e4f133524ecab39204eecbbb
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-02-11 00:17:35 +0530

    Time: 0 ms (100%), Space: 41.7 MB (43.93%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -14,56 +14,57 @@
  * }
  */
 class Solution {
-   public void inOrder(TreeNode root,List<Integer> res){
-        if(root==null)   {
-                return ;
-        }
-     inOrder(root.left,res);
-         res.add(root.val);
-        inOrder(root.right,res);   
-   }     
+      public void inOrder(TreeNode root,List<Integer> res) {
+if(root==null) return;
+inOrder(root.left,res);
+res.add(root.val);
+inOrder(root.right,res);
+}
     public List<Integer> inorderTraversal(TreeNode root) {
-       List<Integer> res=new ArrayList<>() ;
-        // inOrder(root,res);
- //Iterative solution
-/*if(root==null) {
-return res;}
- Stack<TreeNode> st=new Stack<>();
- while(true){
- if(root!=null) {
-  st.push(root);
-  root=root.left;}
-  else {
-  if(st.isEmpty()) break;
-   TreeNode curr=st.pop();
-    res.add(curr.val);
-   root=curr.right;}}
-      return res;*/
-      List<Integer> ans=new ArrayList<>();
-if(root==null){
-return ans;}
-TreeNode curr=root;
-while(curr!=null){
-//First case if curr->left is null
-if(curr.left==null){
-//THERE is no left part push the root and move to right part
-ans.add(curr.val);
-curr=curr.right;}
+List<Integer> res=new ArrayList<>();
+if(root==null) return res;
+// inOrder(root,res);
+//Interative aproach
+// Stack<TreeNode> st=new Stack<>();
+// while(true){
+// if(root!=null) {
+// st.push(root);
+// root=root.left;
+// }  
+// else {
+// if(st.isEmpty()) break;
+// TreeNode curr=st.pop();
+// res.add(curr.val);
+// root=curr.right;
+// } 
+// }
+//Morris Traversal so there are two case if root->left is null or if it not null 
+while(root!=null){
+//left root and right
+if(root.left==null){
+res.add(root.val);
+//move to right side
+root=root.right;
+}
 else {
-//if curr left is not null find rightmost element of left part
-TreeNode prev=curr.left;
-while(prev.right!=null && prev.right!=curr){
-//If prev is not null and. prev is not pointing to curr
-prev=prev.right;}
+//go to right most guy of left part and coonect the theread  it also has two caes
+TreeNode prev=root.left;
+while(prev.right!=null && prev.right!=root){
+prev=prev.right;
+}
+//case 1 if prev right is null
 if(prev.right==null){
-//Connect to curr or make the thread
-prev.right=curr;
-curr=curr.left;}
-else{
-//Prev is already pointing to curr and left part is done
+prev.right=root;
+root=root.left;
+}
+else {
+//it is the place we have travsered the left aprt we have come to root node via right pointer 
 prev.right=null;
-ans.add(curr.val);
-curr=curr.right;} } }
-return ans;
+res.add(root.val);
+root=root.right;
+}
+}
+}
+return res;
     }
 }
