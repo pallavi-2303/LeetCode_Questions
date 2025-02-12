065fdf7ff65b422438df3b734c5f25baf4bf103b
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-02-13 02:00:41 +0530

    Time: 3 ms (82.95%), Space: 45.6 MB (82.11%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,34 +1,33 @@
 class Solution {
     public int candy(int[] ratings) {
-     //brute force making a left and right array 
-     int n=ratings.length;
-    int[] left=new int[n] ;
-    int[] right=new int[n];
-    //brute force
-    left[0]=1;
-    for(int i=1;i<n;i++) {
-    if(ratings[i]>ratings[i-1]){
-    left[i]=left[i-1]+1;
-    }
-    else {
-        left[i]=1;
-    }
-    }
-right[n-1]=1;
-for(int i=n-2;i>=0;i--){
-if(ratings[i]>ratings[i+1]){
-right[i]=right[i+1]+1;
+ int n=ratings.length;
+int i=1;
+int sum=1;
+while(i<n){
+if(ratings[i]==ratings[i-1]) {
+ sum+=1;
+ i++;
+ continue;
 }
-else {
-right[i]=1;
+//for upward curve
+int peak=1;
+while(i<n && ratings[i]>ratings[i-1]) {
+ peak+=1;
+ sum+=peak;
+ i++;
 }
+//for downward curve
+int down=1;
+while(i<n && ratings[i-1]>ratings[i]) {
+ sum+=down;
+ down+=1;
+ i++;
 }
-int sum=0;
-for(int i=0;i<n;i++){
-sum+=Math.max(left[i],right[i]);
+if(down>peak){
+ sum+=(down-peak);
+}
 }
 return sum;
-
-
+        
     }
 }
