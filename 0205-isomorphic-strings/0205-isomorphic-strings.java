6b757c8ebf3d7afd7fa46cbabd1bb6a66dca4be2
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-02-14 16:19:21 +0530

    Time: 10 ms (75%), Space: 42.7 MB (37.04%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,23 +1,28 @@
 class Solution {
-  public boolean isIsomorph(String s,String t) {
- HashMap<Character,Character> mp=new HashMap<>();
-int n=s.length();
-for(int i=0;i<n;i++) {
- char str=s.charAt(i);
- char ctr=t.charAt(i);
- if(mp.containsKey(str)) {
-  if(mp.get(str)!=ctr) {
-    return false;}}
- else if(mp.containsValue(ctr)) {
-     return false;}
-  else {
-    mp.put(str,ctr);}}
-      return true;}
- 
+    public boolean IsIsomorphic(String s,String t){
+      HashMap<Character,Character> mp=new HashMap<>();
+      for(int i=0;i<s.length();i++) {
+    //so we are maping s to t
+     char sch=s.charAt(i);
+     char tch=t.charAt(i);
+    if(mp.containsKey(sch)){
+    if(mp.get(sch)!=tch){
+     return false;
+    }}
+    else if(mp.containsValue(tch)){
+        return false;
+    }
+    else {
+        mp.put(sch,tch);
+    }
+      }
+      return true;//if all the mapping is done
+    }
     public boolean isIsomorphic(String s, String t) {
-if(s.length()!=t.length()) {
-  return false;}
-    return isIsomorph(s,t);
-        
+       //we will map one chracater of string s to  another character of string character
+       int n1=s.length();
+       int n2=t.length();
+      if(n1!=n2) return false;
+      return IsIsomorphic(s,t);
     }
 }
