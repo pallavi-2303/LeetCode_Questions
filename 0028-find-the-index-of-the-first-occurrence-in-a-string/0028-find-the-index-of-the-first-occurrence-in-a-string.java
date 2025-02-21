40e7ec56a66c690722ff818aa38c0c94947c59c7
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-02-21 21:37:53 +0530

    Time: 0 ms (100%), Space: 41.4 MB (77.62%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,5 +1,26 @@
 class Solution {
-    public int strStr(String haystack, String needle) {
+    public void findLPS(String s,int[] lps) {
+int prefix=0;
+int suffix=1;
+int n=s.length();
+while(suffix<n) {
+if(s.charAt(prefix)==s.charAt(suffix))     {
+ lps[suffix]=prefix+1;
+ prefix++;
+ suffix++;
+}
+else {
+if(prefix==0){
+ lps[suffix]=0;
+ suffix++;
+}
+else {
+ prefix=lps[prefix-1];
+}
+}
+}
+}
+    public int strStr(String s, String t) {
       //brute force apparoch
     //   int n=haystack.length();
     //   int m=needle.length();
@@ -10,6 +31,30 @@ class Solution {
     // }
     //   }
     //   return -1;
-    return haystack.indexOf(needle);
+  //  return haystack.indexOf(needle);
+
+  int n=s.length();
+int m=t.length();
+int[] lps=new int[m];
+int first=0;
+int second=0;
+findLPS(t,lps);
+while(first<n && second<m) {
+if(s.charAt(first)==t.charAt(second)) {
+ first++;
+ second++;
+}
+else {
+if(second==0){
+//still its not matching 
+first++;
+}
+else {
+//look for previous mathched character so that we don't have to travel from start 
+ second=lps[second-1];
+}
+}
+}
+return second==m ? first-second :-1;
     }
 }
