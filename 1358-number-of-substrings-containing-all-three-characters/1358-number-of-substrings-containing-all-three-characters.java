b88bfc293e6c3e1a6a1b11f1dc0cdb492607b587
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-03-11 21:50:37 +0530

    Time: 12 ms (72.97%), Space: 45.1 MB (23.35%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -1,31 +1,28 @@
 class Solution {
     public int numberOfSubstrings(String s) {
-      //brute force generating all the sbstring and cheking whather they exit in given sting or not 
-        int n=s.length();
-        int count=0;
-     /*  for(int i=0;i<n;i++){
-         int[] check=new int[3];
-           Arrays.fill(check,-1);
-           for(int j=i;j<n;j++){
-               char ch=s.charAt(j);
-               check[ch-'a']=j;
-               if(check[0]!=-1 && check[1] !=-1 && check[2]!=-1){
-                   count++;
-               }
-           }
-       }
-        return count;//0(n2);*/
-        //we have to stand at particular index find minium lenght where last is occuring because behind it all the the string will be valid
-  int[] lastseen={-1,-1,-1} ;
-    int r=0;
-        while(r<n){
-        char ch=s.charAt(r);
-    lastseen[ch-'a']=r;
-            if(lastseen[0]!=-1 && lastseen[1]!=-1 && lastseen[2]!=-1){
-             count+=1+Math.min(lastseen[2],Math.min(lastseen[0],lastseen[1])) ;
-            }
-            r++;
-        }
-       return count; 
+     int n=s.length();
+    //brute force generating all the substring and counting all of them tc->0(n2) sc->0(3n)
+    int count=0;
+   /* for(int i=0;i<n;i++){
+ int[] lastseen={-1,-1,-1};
+ for(int j=i;j<n;j++){
+    char ch=s.charAt(j);
+    lastseen[ch-'a']=i;
+    if(lastseen[0]!=-1 && lastseen[1]!=-1 && lastseen[2]!=-1){
+        count++;
+    }
+ }   }*/
+ //optimal aproach
+int[] lastseen={-1,-1,-1};
+for(int i=0;i<n;i++){
+char ch=s.charAt(i);
+lastseen[ch-'a']=i;
+//condition check is all charcter are presnt in given string
+if(lastseen[0]!=-1 && lastseen[1]!=-1 && lastseen[2]!=-1){
+//we are finding out the min of all because before it all of them have all the three characters
+count+=1+Math.min(lastseen[0],Math.min(lastseen[1],lastseen[2]));
+}
+}
+ return count;
     }
 }
