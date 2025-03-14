03752db8478643e69fd2cafef659e8496567a382
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-03-14 19:01:48 +0530

    Time: 1 ms (75.59%), Space: 45.6 MB (65.95%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,43 +1,25 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
- int n=matrix.length;
-int m=matrix[0].length;
-/*int[] row=new int[n];
-int[] col=new int[m];
-for(int i=0;i<n;i++){
-for(int j=0;j<m;j++){
-if(matrix[i][j]==0){
-row[i]=1;
-col[j]=1;}}}
-//changing the matrix
+     //brute force
+     int n=matrix.length;
+     int m=matrix[0].length;
+     int[] row=new int[n] ;
+     int[] col=new int[m] ;
+     for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+    if(matrix[i][j]==0){
+    row[i]=1;
+    col[j]=1;
+    }
+    }
+     }
+//changing the matrix;
 for(int i=0;i<n;i++){
 for(int j=0;j<m;j++){
 if(row[i]==1 || col[j]==1){
-matrix[i][j]=0;}}}*/
-//optimal solution using first row to mark column and first col to mark row and col0 to mark 0th col
-int col0=1;
-for(int i=0;i<n;i++){
-for(int j=0;j<m;j++){
-if(matrix[i][j]==0){
-//marking the row
- matrix[i][0]=0;
-//marking the col
-if(j!=0){
-matrix[0][j]=0;}
-else{
-col0=0;}}}}
-//changing the matrix
-for(int i=1;i<n;i++){
-for(int j=1;j<m;j++){
-if(matrix[i][0]==0 || matrix[0][j]==0){
-matrix[i][j]=0;}}}
-//for first row
- if(matrix[0][0]==0){
-for(int j=0;j<m;j++){
-matrix[0][j]=0;}}
-//for first col
-if(col0==0){
-for(int i=0;i<n;i++){
-matrix[i][0]=0;}}
+matrix[i][j]=0;
+}
+}
+}
     }
 }
