ccb34c3e22ec19eae6eedf409bc891ff432772f7
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-03-14 20:01:36 +0530

    Time: 1 ms (99.49%), Space: 56.7 MB (88.72%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,26 +1,13 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-      int n=nums.length;
-      /*brute force generating all subaaray sum and finding out the maximum of all
-      int maxSum=Integer.MIN_VALUE;
-      for(int i=0;i<n;i++)  {
-        int sum=0;
-        for(int j=i;j<n;j++){
-            sum+=nums[j];
-            maxSum=Math.max(maxSum,sum);
-        }
-      }
-      return maxSum;*/
-      //better apraoch of using the kedanes algorithm if summ becomes less than zero then it is not going to contribute to the maxsum so make it zero
-      int maxSum=Integer.MIN_VALUE;
-      int sum=0;
-      for(int i=0;i<n;i++){
-sum+=nums[i];
-if(sum>maxSum){
-    maxSum=sum;
-}
-if(sum<0) sum=0;
-      }
-return maxSum;
+     int n=nums.length;
+     int sum=0;
+     int maxsum=Integer.MIN_VALUE;
+     for(int num:nums) {
+    sum+=num;
+    maxsum=Math.max(sum,maxsum);
+    if(sum<0) sum=0;
+     }
+     return maxsum;  
     }
 }
