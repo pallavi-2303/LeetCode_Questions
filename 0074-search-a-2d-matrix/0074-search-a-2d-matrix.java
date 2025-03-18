e12c5a1c0f1190af4117d563570e966704722d95
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-03-18 17:47:35 +0530

    Time: 0 ms (100%), Space: 41.4 MB (99.85%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,31 +1,18 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-int n=matrix.length;
-int m=matrix[0].length;
-//brute force search in entire matrix and tc->n2
- //since row and col are sorted so we can use this property
-/*int i=0;
-int j=m-1;
-while(i<n && j>=0){
-if(matrix[i][j]==target){
-  return true;}
- else if(matrix[i][j]<target){
-   i++;}
-else {
-  j--;}}
-  return false;*/
-   int low=0;
-int high=n*m-1;
-while(low<=high){
-int mid=low+(high-low)/2;
-int midEle=matrix[mid/m][mid%m];
-if(midEle==target){
-return true;}
-else if(midEle<target){
-low=mid+1;}
-else {
-high=mid-1;}}
-return false;
-        
+      int n=matrix.length;
+     int m=matrix[0].length;
+     int i=0;
+     int j=m-1;
+     while(i<n && j>=0)  {
+    if(matrix[i][j]==target) return true;
+    else if(matrix[i][j]<target){
+    i++;
+    }
+    else {
+      j--;
+    }
+     }
+     return false;
     }
 }
