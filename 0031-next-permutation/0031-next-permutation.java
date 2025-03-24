d18273447965a2a23b636bf028020c6f8cf8af28
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-03-24 15:28:09 +0530

    Time: 0 ms (100%), Space: 43 MB (74.09%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,38 +1,39 @@
 class Solution {
-    public void swap(int[] arr,int i,int j){
-    int temp=arr[i];
-    arr[i]=arr[j];
-    arr[j]=temp;
-    }
-    public void reverse(int[] arr,int i,int j){
-while(i<j){
-    swap(arr,i,j);
-    i++;
-    j--;
+    public void swap(int[] nums,int i,int j) {
+int temp=nums[i];
+nums[i]=nums[j];
+nums[j]=temp;
 }
-    }
+public void reverse(int[] arr,int i,int j) {
+while(i<j) {
+swap(arr,i,j);
+i++;
+j--;
+}
+}
     public void nextPermutation(int[] nums) {
-   int n=nums.length;
-   //2 1 5 4 3 0 0
-    //step->1 Finding the first dip element;
-    int idx=-1;
-    for(int i=n-2;i>=0;i--) {
-   if(nums[i]<nums[i+1]){
-//means it is the dip element
+     int n=nums.length;
+//step-1 finding out the first dip
+int idx=-1;
+for(int i=n-2;i>=0;i--){
+if(nums[i]<nums[i+1]){
 idx=i;
-break;}}
-//if we are not able to find first dip means it is the last permuation and just reverse the array
+break;
+}
+}
+//if we are not able to find the dip means it is the last permutations and we will come back to previous one
 if(idx==-1){
- reverse(nums,0,n-1);
- return;}
-//from last we have to find the elememnt just greater than idx element  and swap the element
+reverse(nums,0,n-1);
+return;
+}
+//step->2 find the just greater element than the first dip and swapping its value with dip
 for(int i=n-1;i>idx;i--){
-if(nums[i]>nums[idx]){
-    swap(nums,i,idx);
-    break;
+if(nums[i]>nums[idx]) {
+swap(nums,i,idx);
+break;
 }
 }
-//step-3 just reverse the array from idx+1 to end to keep it closer
-reverse(nums,idx+1,n-1);
+//swapping from idx+1 to n-1
+reverse(nums,idx+1,n-1);   
     }
 }
