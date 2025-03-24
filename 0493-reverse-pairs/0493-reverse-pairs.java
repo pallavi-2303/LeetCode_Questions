3503bf837e94aa379e63207f4b57cb0da86df6a9
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-03-25 03:53:13 +0530

    Time: 2782 ms (5.02%), Space: 54.4 MB (77.58%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -1,51 +1,59 @@
 class Solution {
-public void merge(int[] arr,int low,int mid,int high){
-List<Integer> temp=new ArrayList<>();
-int i=low;
-int j=mid+1;
-while(i<=mid && j<=high){
-if(arr[i]<=arr[j]){
-temp.add(arr[i++]);}
-else {
-temp.add(arr[j++]);}}
- while(i<=mid){
- temp.add(arr[i++]);}
-while(j<=high){
-temp.add(arr[j++]);}
-for(int idx=low;idx<=high;idx++){
-arr[idx]=temp.get(idx-low);}
-        }
-public int CountPairs(int[] nums,int low,int mid,int high){
-int n=nums.length;
-int count=0;
-int right=mid+1;
+    public void merge(int[] arr,int low,int mid,int high){
+    List<Integer> temp=new ArrayList<>();
+    int i=low;
+    int j=mid+1;
+    while(i<=mid && j<=high){
+    if(arr[i]<=arr[j]) {
+        temp.add(arr[i++]);
+    }   
+    else {
+        temp.add(arr[j++]);
+    }
+    }
+    while(i<=mid){
+        temp.add(arr[i++]);
+    }
+    while(j<=high){
+       temp.add(arr[j++]);  
+    }
+    //putting in array
+    for(int idx=low;idx<=high;idx++){
+        arr[idx]=temp.get(idx-low);
+    }
+    }
+    public int reversePairs(int[] arr,int low,int mid,int high){
+        int count=0;
 for(int left=low;left<=mid;left++){
-while(right<=high && (long)(nums[left])>2*(long)(nums[right])) {
- right++;}
-count+=(right-(mid+1));}
-return count;}
-
-
-
- 
-public int mergeSort(int[] arr,int low,int high){        
+int right=mid+1;
+while(right<=high && (long)arr[left]> (long)2*arr[right]){
+right++;
+}
+count+=(right-(mid+1));
+}
+return count;
+    }
+   public int MergeSort(int[] arr,int low,int high){
 int count=0;
 if(low>=high) return count;
 int mid=low+(high-low)/2;
-count+=mergeSort(arr,low,mid);
-count+=mergeSort(arr,mid+1,high);
-count+=CountPairs(arr,low,mid,high);
+count+=MergeSort(arr,low,mid);
+count+=MergeSort(arr,mid+1,high);
+//counting the pairs
+count+=reversePairs(arr,low,mid,high);
 merge(arr,low,mid,high);
-  return count;      }
+return count;
+   }
     public int reversePairs(int[] nums) {
-int n=nums.length;
-/*int count=0;
-//brute force generating all pairs
-for(int i=0;i<n;i++){
-for(int j=i+1;j<n;j++){
-if((long)nums[i]>2*(long)nums[j]){
-count++;}}}
- //return count;*/
- return mergeSort(nums,0,n-1);
+      int n=nums.length;
+//       int count=0;
+//       for(int i=0;i<n;i++) {
+//     for(int j=i+1;j<n;j++){
+//     if((long)nums[i]>((long)2*nums[j])){
+// count++;
+//     }
+//       } }
+//       return count;
+return MergeSort(nums,0,n-1);
     }
 }
