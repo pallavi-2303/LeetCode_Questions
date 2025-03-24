bcf90fe6558c6558c72c1933c8fff0081d15ecf7
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-03-24 15:53:33 +0530

    Time: 13 ms (7.36%), Space: 46.7 MB (38.74%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,26 +1,27 @@
 class Solution {
-    public class sortComparator implements Comparator<int[]> {
-public int compare(int[] a,int[] b) {
-//sort in asscending order
-return a[0]-b[0];
-}
-}
+    public class sortComparator implements Comparator<int[]>{
+    public int compare(int[] a,int[] b){
+    return a[0]-b[0];
+    }
+    }
     public int[][] merge(int[][] intervals) {
-        int n=intervals.length;
-Arrays.sort(intervals,new sortComparator());
-List<List<Integer>> res=new ArrayList<>();
-for(int i=0;i<n;i++){
-if(res.size()==0 || res.get(res.size()-1).get(1)<intervals[i][0]) {
-List<Integer> temp=Arrays.asList(intervals[i][0],intervals[i][1]);
-res.add(temp);
-}
-else {
-res.get(res.size()-1).set(1,Math.max(intervals[i][1],res.get(res.size()-1).get(1)));
-}
-}
-int size=res.size();
-int[][] ans=new int[size][2];
-for(int i=0;i<size;i++){
+    //sort the intervals  on  ascending order of zeroth element
+    int n=intervals.length; 
+    Arrays.sort(intervals,new sortComparator());
+    List<List<Integer>> res=new ArrayList<>();
+    for(int i=0;i<n;i++) {
+    if(res.size()==0 || intervals[i][0] >res.get(res.size()-1).get(1)){
+    List<Integer> temp=Arrays.asList(intervals[i][0],intervals[i][1]);
+    res.add(temp);   
+    }
+    //if 0th element is smaller than the 1st element of res then merge
+    else {
+    res.get(res.size()-1).set(1,Math.max(intervals[i][1],res.get(res.size()-1).get(1)));
+    }
+    }
+    int m=res.size();
+int[][] ans=new int[m][2];
+for(int i=0;i<m;i++){
 ans[i][0]=res.get(i).get(0);
 ans[i][1]=res.get(i).get(1);
 }
