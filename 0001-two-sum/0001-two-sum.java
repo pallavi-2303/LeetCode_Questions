e964ddbb10b649d62ad50a8c988ba9f7ec759203
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-03-25 03:20:50 +0530

    Time: 33 ms (48.95%), Space: 44.9 MB (77.53%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,24 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-     int n=nums.length;
-     int[] ans={-1};
-     //brute force->Generating all subarray
-    //  for(int i=0;i<n;i++) {
-    // for(int j=i+1;j<n;j++){
-    // if(nums[i]+nums[j]==target){
-    // ans=new int[]{i,j};
-    // }
-    // }
-    //  }  
-    //  return ans;
-    HashMap<Integer,Integer> mp=new HashMap<>();
-    for(int i=0;i<n;i++){
-    int remele=target-nums[i];
-    if(mp.containsKey(remele)){
-    ans=new int[]{mp.get(remele),i};
-    }
-    mp.put(nums[i],i);
-    }
-    return ans;
+      int[] ans={-1};
+int n=nums.length;
+//Brute force
+for(int i=0;i<n;i++){
+for(int j=i+1;j<n;j++){
+if(nums[i]+nums[j]==target){
+ans=new int[]{i,j};
+break;}}}
+return ans;  
     }
 }
