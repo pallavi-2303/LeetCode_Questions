2ecbd8e4144f9ce8876094e97cbb9781d02bbe8b
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-03-25 18:01:44 +0530

    Time: 37 ms (23.04%), Space: 68.1 MB (15.25%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,44 +1,24 @@
 class Solution {
-    public boolean Search(int[] arr,int target){
-    int n=arr.length;
-    for(int i=0;i<n;i++){
-    if(arr[i]==target) return true;
-    }
-    return false;
-    }
     public int longestConsecutive(int[] nums) {
-    //   int n=nums.length;
-    //   int maxcount=0;
-    //   //brute force 
-    //   for(int i=0;i<n;i++)  {
-    // int curr=nums[i];
-    // int count=1;
-    // while(Search(nums,curr+1)){
-    // count++;
-    // curr=curr+1;
-    // }
-    // maxcount=Math.max(count,maxcount);
-    //   }
-//     HashSet<Integer> st=new HashSet<>();
-//     int count=0;
-//     for(int num:nums){
-//     st.add(num);
-//     }
-//     //iterate through all the elements
-//     for(int i=0;i<n;i++){
-// //if curr is fist occuence 
-// if(!st.contains(nums[i]-1)){
-//  count=1;
-// int num=nums[i];
-// while(st.contains(num+1)){
-// count+=1;
-// num=num+1;
-// }
-// maxcount=Math.max(count,maxcount);
-// }
-//     }
-//       return maxcount;
- int n=nums.length;
+    /* int maxLen=0;
+     int n=nums.length;
+HashSet<Integer> st=new HashSet<>();
+for(int num:nums) st.add(num);
+for(int i=0;i<n;i++){
+if(!st.contains(nums[i]-1)){
+//it is the first coouucrance
+int curr=nums[i];
+int count=1;
+while(st.contains(curr+1)){
+count++;
+curr+=1;
+
+}
+maxLen=Math.max(maxLen,count);}
+
+}
+return maxLen;*/
+int n=nums.length;
   int maxcount=0;
  int count=0;
 HashSet<Integer> st=new HashSet<>();
