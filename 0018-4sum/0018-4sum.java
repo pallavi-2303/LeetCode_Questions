7f65fa362998e8c5b0aec4c9178c1b4f2258bab1
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-03-25 15:55:42 +0530

    Time: 15 ms (88.5%), Space: 43.8 MB (79.09%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,56 +1,61 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-int n=nums.length;
-//brute force generating all subarray
-/*HashSet<List<Integer>> st=new HashSet<>();
+      //since we have to find unique pairs in brute force we will use a hashset to store umber
+       int n=nums.length;
+       List<List<Integer>> ans=new ArrayList<>();
+//       HashSet<List<Integer>> st=new HashSet<>();
+//       for(int i=0;i<n;i++) {
+//         for(int j=i+1;j<n;j++){
+//         for(int k=j+1;k<n;k++){
+//     for(int l=k+1;l<n;l++){
+//     long sum=nums[i];
+//     sum+=nums[j];
+//     sum+=nums[k];
+//     sum+=nums[l];
+//     if((int)sum==target){
+//     List<Integer> temp=Arrays.asList(nums[i],nums[j],nums[k],nums[l]);
+//     temp.sort(null);//sorting to avoid duplicacy
+//     st.add(temp);
+//     }
+//     }
+//         }
+//         }
+//       }
+//       List<List<Integer>> result=new ArrayList<>(st);
+//       return result;
+//optimal aproch of uusing two pointer
+Arrays.sort(nums);
 for(int i=0;i<n;i++){
+if(i!=0 && nums[i]==nums[i-1]) continue;
 for(int j=i+1;j<n;j++){
-for(int k=j+1;k<n;k++){
-for(int l=k+1;l<n;l++){
-long sum=0;
-sum+=nums[i];
+ if(j>i+1 && nums[j]==nums[j-1])  continue;
+ int k=j+1;
+ int l=n-1;
+ while(k<l) {
+long sum=nums[i];
 sum+=nums[j];
 sum+=nums[k];
 sum+=nums[l];
-if(sum==target){
-List<Integer> temp=Arrays.asList(nums[i],nums[j],nums[k],nums[l]);
-temp.sort(null);
-st.add(temp);}}}}}
-List<List<Integer>> ans=new ArrayList<>(st);
-      return ans;*/
-List<List<Integer>> ans=new ArrayList<>();
- Arrays.sort(nums);
- for(int i=0;i<n;i++){
-if(i!=0 && nums[i]==nums[i-1]){
- continue;}
-for(int j=i+1;j<n;j++){
-if(j!=i+1 && nums[j]==nums[j-1]){
-continue;}
-int k=j+1;
-int l=n-1;
-while(k<l){
-long sum=0;
-sum+=nums[i];
-sum+=nums[j];
-sum+=nums[k];
-sum+=nums[l];
-if(sum<target){
-k++;}
-else if(sum>target){
-  l--;}
+if(sum<target) k++;
+else if(sum>target) l--;
 else {
-//sum==target 
+//sum==target
 List<Integer> temp=Arrays.asList(nums[i],nums[j],nums[k],nums[l]);
 ans.add(temp);
 k++;
 l--;
 while(k<l && nums[k]==nums[k-1]){
- k++;}
- while(k<l && nums[l]==nums[l+1]){
-  l--;}
-        }
-   }
-        }}
-   return ans;
-    }
+k++;
 }
+while(k<l && nums[l]==nums[l+1]){
+    l--;
+}
+}
+ }
+}
+}
+return ans;
+
+
+
+     } }
