de2a083acac3f802fee022d61719ea932b52f972
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-03-28 02:19:01 +0530

    Time: 3 ms (84.22%), Space: 45.6 MB (79.41%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,7 +1,7 @@
 class Solution {
     public int candy(int[] nums) {
-      int n=nums.length;
-int[] left=new int[n];
+int n=nums.length;
+/*int[] left=new int[n];
 int[] right=new int[n];
 left[0]=1;
 right[n-1]=1;
@@ -25,6 +25,27 @@ int sum=0;
 for(int i=0;i<n;i++){
 sum+=Math.max(right[i], left[i]);
 }
-return sum;  
+return sum;*/  
+int[] left=new int[n];
+left[0]=1;
+for(int i=1;i<n;i++){
+if(nums[i]>nums[i-1]){
+left[i]=left[i-1]+1;
+}
+else {
+left[i]=1;
+}
+}
+int sum=Math.max(left[n-1],1);
+int right=1;
+for(int i=n-2;i>=0;i--){
+if(nums[i]>nums[i+1]){
+right=right+1;
+}
+else {
+right=1;
+}
+sum+=Math.max(left[i],right);}
+return sum;
     }
 }
