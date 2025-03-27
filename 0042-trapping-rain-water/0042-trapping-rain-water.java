30edbc5f857f3fbcc9a4beebce17232265b3ad05
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-03-27 16:03:12 +0530

    Time: 0 ms (100%), Space: 46.7 MB (20.19%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,54 +1,49 @@
 class Solution {
     public int trap(int[] arr) {
-       int n=arr.length;
- /* int[] pge=new int[n];
-  int[] nge=new int[n];
-  pge[0]=arr[0];
-  for(int i=1;i<n;i++){
-   pge[i]=Math.max(pge[i-1],arr[i]);
-  }
- nge[n-1]=arr[n-1];
- for(int i=n-2;i>=0;i--){
-  nge[i]=Math.max(arr[i],nge[i+1]);
+/*int n=height.length;
+int[] nge=new int[n];
+int[] pge=new int[n];
+//pge
+pge[0]=height[0];
+  for(int i=1;i<n;i++) {
+   pge[i]=Math.max(height[i],pge[i-1]);
  }
- //Now calculating the amount to be stored
-int sum=0;
- for(int i=0;i<n;i++){
-  sum+=(Math.min(pge[i],nge[i])-arr[i]);   
- }
- return sum;*/
- //optimal aproach of using only indexe
- //in this we perform operation on smaller element first
- //in brute force since we are only taking the minimum of pregreater or nextgreater
- //so we will first traverse the smaller one
- int l=0;
- int r=n-1;
- int leftMax=0;
- int rightMax=0;
- int sum=0;
- while(l<r) {
+  nge[n-1]=height[n-1];
+  for(int i=n-2;i>=0;i--) {
+   nge[i]=Math.max(height[i],nge[i+1]);}
+ 
+   
+ //area
+  int total=0;
+ for(int i=0;i<n;i++) {
+  total+=(Math.min(nge[i],pge[i])-height[i]);}
+        return total;*/
+int n=arr.length;
+int l=0;
+int r=n-1;
+int leftmost=0;
+int rightmost=0;
+int total=0;
+ while(l<=r) {
  if(arr[l]<=arr[r]) {
- //we have someone bigger in right so it can store water
-if(arr[l]<=leftMax) {
- sum+=(leftMax-arr[l] );
-}
-else {
- //he himself is greater in left so it will not contribute to water beacuse no one greart than this on left
- leftMax=arr[l];
-}
-l++;
- }
+ if(arr[l]<leftmost) {
+  total+=leftmost-arr[l];}
  else {
-//right one is smaller
-if(arr[r]<=rightMax){
- sum+=(rightMax-arr[r]);
-}
-else {
-rightMax=arr[r];
-}
-r--;
- }
- }
- return sum; 
+   leftmost=arr[l];}
+    l++;}
+  else {
+ if(arr[r]<rightmost) {
+  total+=rightmost-arr[r];}
+   else {
+   rightmost=arr[r];}
+      r--;}
+     }
+        return total;
+
+
+
+    
+
+        
     }
 }
