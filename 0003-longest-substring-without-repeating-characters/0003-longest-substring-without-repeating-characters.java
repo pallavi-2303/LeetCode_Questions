e7caf50d228f7db75e33023b6f2d79dc0fdc678b
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-03-27 17:44:42 +0530

    Time: 5 ms (87.58%), Space: 44.9 MB (31.74%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,32 +1,55 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-     int n=s.length();
-     int maxLen=0;
-//      for(int i=0;i<n;i++) {
-//     HashSet<Character> st=new HashSet<>();
-//     for(int j=i;j<n;j++){
-// if(st.contains(s.charAt(j))){
-// maxLen=Math.max(maxLen,j-i);
-// break;
-// }
-// st.add(s.charAt(j));
+       int n=s.length();
+       //brute force generating all the subarray and fining max of all
+    /*   int maxlen=0;//tc0(n2) scc->0(n)
+       for(int i=0;i<n;i++){
+    HashSet<Character> st=new HashSet<>();
+    for(int j=i;j<n;j++){
+     char ch=s.charAt(j);
+     if(!st.contains(ch)){
+        st.add(ch);
+        maxlen=Math.max(maxlen,j-i+1);
+     }
+     else {
+        break;
+     }
+
+    }
+       }
+       return maxlen;*/
+       //optimal aproach of using a sliding window max
+//        int maxlen=0;
+//        HashMap<Character,Integer> mp=new HashMap<>();
+//        int l=0;
+//        int r=0;
+//        while(r<n){
+//     char ch=s.charAt(r);
+//     while( l<r && mp.containsKey(ch)){
+// char tch=s.charAt(l);
+// mp.remove(tch);
+// l++;
 //     }
-//      }  
-//      return  maxLen;
-HashMap<Character,Integer> mp=new HashMap<>();
-int l=0;
-int  r=0;
-while(r<n){
-char ch=s.charAt(r);
-while(mp.containsKey(ch)){
-char tch=s.charAt(l);
-mp.remove(tch);
-l++;
-}
-maxLen=Math.max(maxLen,r-l+1);
-r++;
-mp.put(ch,r);
-}
-return maxLen;
+//     //the gven hracter will nnot present 
+//     maxlen=Math.max(maxlen,r-l+1);
+//     mp.put(ch,r);
+//     r++;
+//        }
+//        return maxlen;
+  int maxlen=0;
+       HashMap<Character,Integer> mp=new HashMap<>();
+       int l=0;
+       int r=0;
+       while(r<n){
+    char ch=s.charAt(r);
+   if(mp.containsKey(ch)){
+    l=Math.max(l,mp.get(ch)+1);
+    }
+    //the gven hracter will nnot present 
+    maxlen=Math.max(maxlen,r-l+1);
+    mp.put(ch,r);
+    r++;
+       }
+       return maxlen;
     }
 }
