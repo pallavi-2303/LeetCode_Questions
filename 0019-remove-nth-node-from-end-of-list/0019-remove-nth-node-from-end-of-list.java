234c0afe1364ecc67bbfb982ee4068c688cc554d
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-03-28 00:49:41 +0530

    Time: 0 ms (100%), Space: 41.8 MB (44.29%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -10,39 +10,34 @@
  */
 class Solution {
     public int findSize(ListNode head){
-        int count=0;
+int size=0;
 while(head!=null){
-    count++;
-    head=head.next;
-}
-return count;
-    }
+size++;
+head=head.next;}
+return size;}
     public ListNode removeNthFromEnd(ListNode head, int n) {
-     //nth node from end means size-n+1 node from the start
-    /* if(head==null || (head.next==null && n==1)) return null;
-     int size=findSize(head);
-     if(size==n) return head.next;
-     int m=size-n+1;
-     ListNode temp=head;
-     for(int i=1;i<m-1;i++) {
-        temp=temp.next;
-     }  
-     temp.next=temp.next.next;
-     return head;*/
-     ListNode fast=head;
-     ListNode slow=head;
-     //we need l-n disatnce from the starting point
-     for(int i=1;i<=n;i++){
-    fast=fast.next;
-     }
-     if(fast==null){
-        return head.next;//because we have to  delete the nth from 
-     }
-   while(fast.next!=null)  {
-    fast=fast.next;
-    slow=slow.next;
-   }
-   slow.next=slow.next.next;
-   return head;
+        //last NTh Node means starting se size-n+1 th node
+if((head==null || head.next==null) && n==1) return null;
+/*int size=findSize(head);
+if(size==n) return head.next;
+int m=size-n+1;
+ListNode temp=head;
+for(int i=1;i<m-1;i++){
+temp=temp.next;}
+temp.next=temp.next.next;
+return head;*/
+ListNode slow=head;
+ListNode fast=head;
+//since we want to come before the node to be deleted size-n
+for(int i=1;i<=n;i++){
+fast=fast.next;
+}
+if(fast==null) return head.next;
+while(fast.next!=null){
+fast=fast.next;
+slow=slow.next;
+}
+slow.next=slow.next.next;
+return head;
     }
 }
