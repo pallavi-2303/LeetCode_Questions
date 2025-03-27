6e8420a8eae25f59e9431de23020de65cc33ecee
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-03-27 23:47:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,30 +10,28 @@
  */
 class Solution {
     public ListNode reverse(ListNode head){
-    //base case
     if(head.next==null){
-        return head;
+    return head;
     }
-    ListNode newHead=reverse(head.next);
-    //self work
-//links 4-5 we have to do 5-4
-ListNode front=head.next;
-front.next=head;
-head.next=null;
+   ListNode newHead=reverse(head.next);
+  //4-5 5-4
+  ListNode front=head.next;
+  front.next=head;
+  head.next=null;
 return newHead;
+
     }
     public ListNode reverseList(ListNode head) {
-    if(head==null || head.next==null) return head;
-     /* ListNode temp=head;
-      ListNode prev=null;
-      while(temp!=null) {
-        ListNode Next=temp.next;
-        temp.next=prev;
-        prev=temp;
-        temp=Next;
-      } 
-      return prev;*/
-      //recurssive solution
-      return reverse(head);
+   /*    ListNode temp=head;
+ListNode prev=null;
+while(temp!=null){
+ListNode nextNode=temp.next;
+temp.next=prev;
+prev=temp;
+temp=nextNode;}
+return prev; */
+if(head==null || head.next==null) return null;
+return reverse(head);
+
     }
 }
