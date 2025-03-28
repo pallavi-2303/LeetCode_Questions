124cfc290cf16d55522ae61e803cf69f0016b79c
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-03-29 02:12:19 +0530

    Time: 0 ms (100%), Space: 43.9 MB (97.73%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -9,46 +9,48 @@
  * }
  */
 class Solution {
-public ListNode reverseLL(ListNode head){
+    public ListNode reverse(ListNode head){
 ListNode temp=head;
 ListNode prev=null;
-while(temp!=null){
-ListNode front=temp.next;
+while(temp!=null) {
+ListNode nextNode=temp.next;
 temp.next=prev;
- prev=temp;
- temp=front;}
- return prev;}
- public ListNode findKThNode(ListNode temp,int k){
-k=k-1;
-while(temp!=null && k>0){
-temp=temp.next;
- k-=1;}
- return temp;}
+prev=temp;
+temp=nextNode;
+}
+return prev;
+}
+public ListNode findkthNode(ListNode head,int k) {
+k-=1;
+while(head!=null && k>0) {
+head=head.next;
+k--;
+}
+return head;
+}
     public ListNode reverseKGroup(ListNode head, int k) {
- ListNode temp=head;
-ListNode PrevNode=null;
-while(temp!=null){
-ListNode kthNode=findKThNode(temp,k);
-//if KTHNode is null
- if(kthNode==null){
-if(PrevNode!=null){
- //it is in last portion
-  PrevNode.next=temp;
-    break;}}
-ListNode NextNode=kthNode.next;
- kthNode.next=null;
- //reverse K portion of LL
- reverseLL(temp);
- //now kth Node will be head if it is first occursence
- if(temp==head){
-  head=kthNode;}
-  else {
-  //connect prevlast to kth node
-  PrevNode.next=kthNode;}
-//after reversal temp become prev Node
- PrevNode=temp;
- temp=NextNode;}
-   return head;
-        
+      ListNode temp=head;
+ListNode prev=null;
+while(temp!=null) {
+ListNode kthNode=findkthNode(temp,k);
+if(kthNode==null) {
+if(prev!=null) 
+prev.next=temp;
+break;
+}
+//since we have to break the kthnode next to reverse it we have to store it's next Node
+ListNode nextNode=kthNode.next;
+kthNode.next=null;
+reverse(temp);
+//if it is first reversal
+if(head==temp) {
+head=kthNode;
+}
+else {
+prev.next=kthNode;}
+prev=temp;
+temp=nextNode;
+}
+return head;  
     }
 }
