3de0fb1f33ec85f0cc4d89670e74e67d160aaf63
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-03-29 02:13:11 +0530

    Time: 0 ms (100%), Space: 44.1 MB (95.24%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,15 +11,14 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-      if(head==null) return false;
-      ListNode slow=head;
-      ListNode fast=head;
-      while(fast!=null && fast.next!=null) {
-   
-       slow=slow.next;
-       fast=fast.next.next; 
-        if(slow==fast) return true;
-      }
-      return false;
+     ListNode slow=head;
+ListNode fast=head;
+//if there will be a cycle then they tend to meet at a point as fast move by 2 and slow move by 1 distance reduces by 1 there will be time when diff=0;
+while(fast!=null && fast.next!=null) {
+slow=slow.next;
+fast=fast.next.next;
+if(slow==fast) return true;
+}
+return false ;   
     }
 }
