8159ba81a8345ff3bccaacacfdab70ab42ba52ad
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-03-30 22:52:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -10,9 +10,16 @@
  * }
  */
 public class Solution {
+    public ListNode findStart(ListNode slow,ListNode fast){
+    while(slow!=fast){
+    slow=slow.next;
+    fast=fast.next;
+    }
+    return slow;
+    }
     public ListNode detectCycle(ListNode head) {
      //brute force 
-     HashMap<ListNode ,Integer> mp=new HashMap<>();
+    /* HashMap<ListNode ,Integer> mp=new HashMap<>();
      ListNode temp=head;
      while(temp!=null){
     if(mp.containsKey(temp)){
@@ -22,6 +29,20 @@ public class Solution {
     mp.put(temp,1);
     temp=temp.next;
      }  
-     return null; 
+     return null; */
+     if(head==null || head.next==null) return null;
+     ListNode slow=head;
+     ListNode fast=head;
+//detecting the cycle
+while(fast!=null || fast.next!=null){
+slow=slow.next;
+fast=fast.next.next;
+//if cycle found and slow and fast become zero
+if(slow==fast){
+   fast=head;
+   return findStart(slow,fast); 
+}
+}
+return null;
     }
 }
