35ed773b49b434f5a85e46d5283ca9be2784c5d1
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-03-30 16:16:42 +0530

    Time: 13 ms (31.53%), Space: 45 MB (11.87%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,18 +1,17 @@
 class Solution {
-  public HashMap<Character,Integer> MakeHashMap(String s) {
- HashMap<Character,Integer> mp=new HashMap<>();
-for(int i=0;i<s.length();i++) {
- char ch=s.charAt(i);
- mp.put(ch,mp.getOrDefault(ch,0)+1);}
-    return mp;}
- public boolean isAnagram2(String s,String t) {
- HashMap<Character,Integer> mp1=MakeHashMap(s);
-HashMap<Character,Integer> mp2=MakeHashMap(t);
-  return mp1.equals(mp2);}
+    public void makeHashMap(String str,HashMap<Character,Integer> mp) {
+for(char ch:str.toCharArray()) {
+mp.put(ch,mp.getOrDefault(ch,0)+1);
+}
+}
     public boolean isAnagram(String s, String t) {
-if(s.length()!=t.length()) {
- return false;}
-  return isAnagram2(s,t);
-        
+        int n=s.length();
+int m=t.length();
+HashMap<Character,Integer> mp1=new HashMap<>();
+HashMap<Character,Integer> mp2=new HashMap<>();
+if(n!=m) return false;
+makeHashMap(s,mp1);
+makeHashMap(t,mp2);
+return mp1.equals(mp2);
     }
 }
