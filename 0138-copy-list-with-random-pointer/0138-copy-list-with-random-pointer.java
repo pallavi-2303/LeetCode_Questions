51b33873bfd724b949f466a8321567f561a0372c
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-03-31 01:32:10 +0530

    Time: 0 ms (100%), Space: 44.5 MB (52.64%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -14,25 +14,61 @@ class Node {
 */
 
 class Solution {
-    public Node copyRandomList(Node head) {
-    //Creating a copyNode and storing its value in hashmap
-HashMap<Node,Node> mp=new HashMap<>();
-Node temp=head;
+    public void insertCopyNode(Node temp) {
 while(temp!=null) {
+Node copyNode=new Node(temp.val);
+copyNode.next=temp.next;
+temp.next=copyNode;
+temp=temp.next.next;
+}
+}
+public void connectRandomPointer(Node temp) {
+ while(temp!=null) {
+ Node copyNode=temp.next;
+ if(temp.random!=null) {
+ copyNode.random=temp.random.next;
+ }
+ else {
+ copyNode.random=null;
+ }
+ temp=temp.next.next;
+ }
+}
+public Node deepCopyExtract(Node temp) {
+Node dummy=new Node(0);
+Node res=dummy;
+while(temp!=null) {
+res.next=temp.next;
+temp.next=temp.next.next;
+res=res.next;
+temp=temp.next;
+}
+return dummy.next;
+}
+    public Node copyRandomList(Node head) {
+      //brute force
+//creating copyNode and putting in map and then creating the pointers
+/*HashMap<Node,Node> mp=new HashMap<>();
+Node temp=head;
+while(temp!=null){
 Node copyNode=new Node(temp.val);
 mp.put(temp,copyNode);
 temp=temp.next;
 }
-//Connecting the random and next pointer
+//creating the next and random pointers 
 temp=head;
-while(temp!=null) {
+while(temp!=null){
 Node copyNode=mp.get(temp);
-Node nextNode=mp.get(temp.next);
-Node Random=mp.get(temp.random);
-copyNode.next=nextNode;
-copyNode.random=Random;
+copyNode.next=mp.get(temp.next);
+copyNode.random=mp.get(temp.random);
 temp=temp.next;
 }
-return mp.get(head);    
+return mp.get(head);  */
+//first step-> Inserting the copy Node inbetween 
+insertCopyNode(head);
+//step->2 Connecting the Random pointer
+connectRandomPointer(head);
+//step->3 extrating deep copy and connecting the next pointer
+return deepCopyExtract(head);
     }
 }
