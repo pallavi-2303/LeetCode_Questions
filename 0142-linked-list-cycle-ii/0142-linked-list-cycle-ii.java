86b37cdc4db9846e61d92671757b62a1228ba1c5
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-03-30 22:51:26 +0530

    Time: 4 ms (9.7%), Space: 44.9 MB (17.51%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -10,39 +10,18 @@
  * }
  */
 public class Solution {
-    public ListNode startingNode(ListNode slow,ListNode fast,ListNode head){
-      slow=head;
-      while(slow!=fast) {
-    slow=slow.next;
-    fast=fast.next;
-      } 
-      return slow;
-    }
     public ListNode detectCycle(ListNode head) {
-      if(head==null || head.next==null)  return null;//as there will be no cycle
-/*brute force
-HashMap<ListNode,Integer> mp=new HashMap<>() ;
-//if the node is alreaddy present in map means it is the starting point of cycle
-ListNode temp=head;
-while(temp!=null) {
-if(mp.containsKey(temp)){
-//means it is staring point of  cycle
-return temp;
-}
-mp.put(temp,1);
-temp=temp.next;
-}*/
-//optimal aproach  of if there is cycle use of tortoise hair algorithm
-//in this fast move towards slow by 2 step and slow move away from fast 1 step so disance between slow and fast reduces by 1 step which reduces to 0 and they fially collide
-ListNode slow=head;
-ListNode fast=head;
-while(fast!=null && fast.next!=null){
-slow=slow.next;
-fast=fast.next.next;
-if(slow==fast){
-return startingNode(slow,fast,head);
-}
-}
-return  null;//if there is no cycle   
+     //brute force 
+     HashMap<ListNode ,Integer> mp=new HashMap<>();
+     ListNode temp=head;
+     while(temp!=null){
+    if(mp.containsKey(temp)){
+        //it is the first visit to node after one time
+    return temp;
+    }
+    mp.put(temp,1);
+    temp=temp.next;
+     }  
+     return null; 
     }
 }
