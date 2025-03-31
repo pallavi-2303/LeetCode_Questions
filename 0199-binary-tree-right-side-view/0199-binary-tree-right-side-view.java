91778648ffc1c069d3b07f341f341b479d9541d7
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-01 03:22:09 +0530

    Time: 1 ms (70.69%), Space: 42.4 MB (27.28%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -26,7 +26,21 @@ class Solution {
     public List<Integer> rightSideView(TreeNode root) {
      List<Integer> res=new ArrayList<>();
      if(root==null)   return res;
-     RightSideView(root,0,res);
+    //  RightSideView(root,0,res);
+  //Right Side view means  last nodeof every level 
+ Queue<TreeNode> q=new LinkedList<>();
+ q.add(root);
+ while(!q.isEmpty())  {
+int size=q.size();
+List<Integer> temp=new ArrayList<>();
+for(int i=1;i<=size;i++){
+TreeNode node=q.poll();
+temp.add(node.val);
+if(node.right!=null) q.add(node.right);
+if(node.left!=null) q.add(node.left);
+}
+res.add(temp.get(0));
+ }
      return res;
     }
 }
