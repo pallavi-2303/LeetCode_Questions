ab10d35806cf6408cefec9fbe57711f9553c33e7
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-03-31 17:13:50 +0530

    Time: 18 ms (42.72%), Space: 45.5 MB (14.95%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -2,7 +2,6 @@ class Solution {
     public int memo(int idx,int[] nums,
 int[][] dp,int target) {
 if(target==0) return 0;
-if(idx<0 && target>0) return (int)1e9;
 if(idx==0) {
 if(target%nums[0]==0) {
 return target/nums[0];
@@ -21,11 +20,38 @@ target);
 return dp[idx][target]=
 Math.min(take,nottake);
 }
+public int tabulation(int[] nums,int target){
+int n=nums.length;
+int[][] dp=new int[n][target+1];
+for(int tar=0;tar<=target;tar++){
+if(tar%nums[0]==0){
+dp[0][tar]=tar/nums[0];
+}
+else {
+dp[0][tar]=(int)1e9;
+}
+}
+for(int i=0;i<n;i++){
+dp[i][0]=0;
+}
+for(int i=1;i<n;i++){
+for(int tar=0;tar<=target;tar++){
+int take=(int)1e9;
+if(nums[i]<=tar){
+take=1+dp[i][tar-nums[i]];
+}
+int nottake=0+dp[i-1][tar];
+dp[i][tar]=Math.min(take,nottake);
+}
+}
+return dp[n-1][target];
+}
     public int coinChange(int[] coins, int amount) {
        int n=coins.length;
        int[][] dp=new int[n][amount+1];
        for(int[] a:dp) Arrays.fill(a,-1);
-int ans= memo(n-1,coins,dp,amount);
+//int ans= memo(n-1,coins,dp,amount);
+int ans=tabulation(coins,amount);
 return ans==(int)1e9 ? -1 :ans;
 
     }
