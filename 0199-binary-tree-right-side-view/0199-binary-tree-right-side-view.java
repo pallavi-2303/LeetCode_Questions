7642a6f79f69c02941cb5afd6d049884c2e2cc7e
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-01 03:25:36 +0530

    Time: 1 ms (70.69%), Space: 42.5 MB (8.7%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -14,33 +14,23 @@
  * }
  */
 class Solution {
-    public void RightSideView(TreeNode root,int level,List<Integer> res) {
- if(root==null) 
- return ;
- if(level==res.size()) {
- res.add(root.val);
- }
- RightSideView(root.right,level+1,res);
- RightSideView(root.left,level+1,res);
-}
     public List<Integer> rightSideView(TreeNode root) {
-     List<Integer> res=new ArrayList<>();
-     if(root==null)   return res;
-    //  RightSideView(root,0,res);
-  //Right Side view means  last nodeof every level 
- Queue<TreeNode> q=new LinkedList<>();
- q.add(root);
- while(!q.isEmpty())  {
-int size=q.size();
+     List<Integer> ans=new ArrayList<>();
+Queue<TreeNode> q=new LinkedList<>();
+if(root==null) return new ArrayList<>();
+//right side view means first node  of every level
+q.add(root);
+while(!q.isEmpty()){
+int  size=q.size();
 List<Integer> temp=new ArrayList<>();
 for(int i=1;i<=size;i++){
-TreeNode node=q.poll();
-temp.add(node.val);
-if(node.right!=null) q.add(node.right);
-if(node.left!=null) q.add(node.left);
+TreeNode curr=q.poll();
+temp.add(curr.val);
+if(curr.right!=null) q.offer(curr.right);
+if(curr.left!=null) q.offer(curr.left);
 }
-res.add(temp.get(0));
- }
-     return res;
+ans.add(temp.get(0));
+}
+return ans;
     }
 }
