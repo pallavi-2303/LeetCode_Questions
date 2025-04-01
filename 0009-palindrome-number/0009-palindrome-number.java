5d05078143dbd315a24edf385eb8b866099ea942
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-01 20:58:58 +0530

    Time: 5 ms (72.5%), Space: 44.1 MB (47.26%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,17 +1,16 @@
 class Solution {
-    public int findReverse(int num){
-    long reverse=0;
-    while(num>0){
-    int rem=num%10;
+    public int reverse(int x){
+    int reverse=0;
+    while(x>0){
     reverse*=10;
+    int rem=x%10;
     reverse+=rem;
-    num/=10;
+    x/=10;
     }
-    return (int)reverse;
+    return reverse;
     }
     public boolean isPalindrome(int x) {
-      if(x<0)  return false;
-      int num=Math.abs(x);
-      return x==findReverse(num);
+     if(x<0)  return  false;
+     return x==reverse(x); 
     }
 }
