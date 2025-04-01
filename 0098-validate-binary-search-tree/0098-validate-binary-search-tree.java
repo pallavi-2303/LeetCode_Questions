af28c35cf34ef37d37fd10d0c34f7d0d61cbec5a
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-01 14:55:02 +0530

    Time: 0 ms (100%), Space: 44 MB (32.51%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -14,21 +14,13 @@
  * }
  */
 class Solution {
-    public void inorder(TreeNode root,List<Integer> temp) {
-if(root==null) return;
-inorder(root.left,temp);
-temp.add(root.val);
-inorder(root.right,temp);
-}
+    public boolean isValid(TreeNode root,long minValue,long maxValue){
+    if(root==null) return true;
+    if(root.val<=minValue || root.val>=maxValue) return false;
+    return isValid(root.left,minValue,root.val) && isValid(root.right,root.val ,maxValue);
+    }
     public boolean isValidBST(TreeNode root) {
-      //we know that inorder of binary search tree is always sort
-if(root==null) return true;
-List<Integer> temp=new ArrayList<>();
-inorder(root,temp);
-for(int i=1;i<temp.size();i++){
-if(temp.get(i-1)>=temp.get(i)) return false;
-}
-return true;
-
+      if(root==null)  return true;
+      return isValid(root,Long.MIN_VALUE,Long.MAX_VALUE);
     }
 }
