2c2dcdc52c49a87318c935b95af4329f68479236
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-02 17:39:04 +0530

    Time: 1 ms (99.89%), Space: 44.8 MB (32.11%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,22 +1,22 @@
 class Solution {
- public void CombinationSum1(int[] arr,int idx,List<List<Integer>> ans,int target,List<Integer> ds) {
- //base case
-  if(idx==arr.length) {
-   if(target==0) {
-  ans.add(new ArrayList<Integer>(ds));}
-      return;}
-  //if we choose given idx
-  if(arr[idx]<=target) {
-   ds.add(arr[idx]);
-  CombinationSum1(arr,idx,ans,target-arr[idx],ds);
-      ds.remove(ds.size()-1);}
- //if we dont choose current idx
-  CombinationSum1(arr,idx+1,ans,target,ds);
-     }
-    
+    public void solve(int idx,List<List<Integer>> res,List<Integer> ds,int target,int[] nums) {
+int n=nums.length;
+if(idx==n) {
+if(target==0) {
+ res.add(new ArrayList<>(ds));}
+return ; 
+ }
+//pick case we can pick unlimited number of times
+if(nums[idx]<=target) {
+ds.add(nums[idx]);
+//we can pick unlimited number of times
+solve(idx,res,ds,target-nums[idx],nums);
+//not pick case
+ds.remove(ds.size()-1);}
+solve(idx+1,res,ds,target,nums);}
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-  List<List<Integer>> ans=new ArrayList<>();
- CombinationSum1(candidates,0,ans,target,new ArrayList<>());
-        return ans;
+       List<List<Integer>> ans=new ArrayList<>();
+       solve(0,ans,new ArrayList<>(),target,candidates);
+       return ans;
     }
 }
