2f1d2b61e078a003c295bbd4cf8b8d72ef5e16f6
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-03 01:48:30 +0530

    Time: 3 ms (33.3%), Space: 45.5 MB (48.21%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -19,12 +19,12 @@ pair(int row,int col){
     this.col=col;
 }
 }
-/*public void bfs(int i,int j,int[][] vis,char[][] board,int[] delrow,int[] delcol) {
+public void bfs(int r,int c,int[][] vis,char[][] board,int[] delrow,int[] delcol) {
 Queue<pair> q=new LinkedList<>();
 int n=vis.length;
 int m=vis[0].length;
-q.add(new pair(i,j));
-vis[i][j]=1;
+q.add(new pair(r,c));
+vis[r][c]=1;
 while(!q.isEmpty()) {
 pair it=q.poll();
 int row=it.row;
@@ -38,7 +38,7 @@ q.add(new pair(nrow,ncol));
 }
 }
 }
-}*/
+}
     public void solve(char[][] board) {
        int n=board.length;
 int m=board[0].length;
@@ -49,7 +49,8 @@ int[] delcol={0,0,-1,1};
 for(int i=0;i<n;i++){
 for(int j=0;j<m;j++){
 if((i==0 || j==0 || i==n-1 || j==m-1) && vis[i][j]==0 && board[i][j]=='O') {
-dfs(i,j,vis,board,delrow,delcol);
+//dfs(i,j,vis,board,delrow,delcol);
+bfs(i,j,vis,board,delrow,delcol);
 }
 }
 }
