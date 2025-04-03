b08eb3c278e33d0f37e7c2a13c2ae01c97ebec79
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-04 02:11:44 +0530

    Time: 0 ms (100%), Space: 41.9 MB (81.1%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,18 +1,20 @@
 class Solution {
     public double myPow(double x, int n) {
-     double ans=1;
-long nn=n;
-if(n<0) nn*=-1;
-while(nn>0) {
-if(nn%2==1) {
- ans*=x;
- nn-=1;
+   double res=1;
+   long nn=n;
+   if(n<0) nn*=-1;
+  while(nn>0) {
+if((nn%2)==1){
+//it is a odd power
+res*=x;
+nn-=1;
 }
 else {
- x*=x;
- nn=nn/2;
+x=x*x;
+nn/=2;
 }
-}
-return n<0 ? 1/(double)ans : ans;   
+  }
+return  n<0 ? (double)1.0/res : res;
+
     }
 }
