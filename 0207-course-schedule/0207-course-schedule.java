5cb00cebbca3890a5fc5d6776ceae9521a72dad2
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-03 16:42:02 +0530

    Time: 5 ms (82.2%), Space: 45.7 MB (44.27%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,48 +1,42 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] arr) {
-       int V=numCourses;
-       ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
-       int n=arr.length;
-       for(int i=0;i<V;i++){
-    adj.add(new ArrayList<>());
-       }
-       for(int i=0;i<n;i++){
-    adj.get(arr[i][1]).add(arr[i][0]);
-       }
-    //if topological sort is valid then this is possible
-    int[] indegree=new int[V];
-    int[] vis=new int[V];
-    for(int i=0;i<V;i++){
-    for(int it:adj.get(i)){
-        indegree[it]++;
-    }
-    }
-    Queue<Integer> q=new LinkedList<>();
-    //adding in q whose indegree is zero
+    public boolean canFinish(int numCourses, int[][] nums) {
+        int n=nums.length;
+List<List<Integer>> adj=new ArrayList<>();
+int V=numCourses;
 for(int i=0;i<V;i++){
-    if(indegree[i]==0){
-        vis[i]=1;
-        q.add(i);
-    }
+adj.add(new ArrayList<>());
+}
+for(int i=0;i<n;i++){
+    adj.get(nums[i][1]).add(nums[i][0]);
+}
+int[] indegre=new int[V];
+Queue<Integer> q=new LinkedList<>();
+int[] vis=new int[V];
+for(int i=0;i<V;i++){
+for(int it:adj.get(i)) {
+indegre[it]++;
+}
+}
+for(int i=0;i<V;i++){
+if(indegre[i]==0){
+vis[i]=1;
+q.add(i);
+}
 }
 int count=0;
-while(!q.isEmpty()){
+while(!q.isEmpty()) {
 int node=q.poll();
 count++;
 for(int it:adj.get(node)){
 if(vis[it]==0){
-    indegree[it]--;
-if(indegree[it]==0){
+indegre[it]--;
+if(indegre[it]==0){
 q.add(it);
 vis[it]=1;
 }
 }
 }
 }
-return count==V ? true:false;
-
-
-
-
+return count>=V;
     }
 }
