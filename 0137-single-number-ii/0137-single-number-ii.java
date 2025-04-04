a4a24592b16382c9a81cf2cc788b2febf6c27b7c
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-04 16:45:38 +0530

    Time: 4 ms (53.3%), Space: 45.4 MB (79.79%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,11 +1,22 @@
 class Solution {
     public int singleNumber(int[] nums) {
-    HashMap<Integer,Integer> mp=new HashMap<>();
-    for(int num:nums) mp.put(num,mp.getOrDefault(num,0)+1);
-    int ans=-1;
-    for(int it:mp.keySet()) {
-    if(mp.get(it)==1) return it;
-    }
-    return ans;
+    // HashMap<Integer,Integer> mp=new HashMap<>();
+    // for(int num:nums) mp.put(num,mp.getOrDefault(num,0)+1);
+    // int ans=-1;
+    // for(int it:mp.keySet()) {
+    // if(mp.get(it)==1) return it;
+    // }
+    // return ans;
+//since all thrice numbers appear in pair 
+Arrays.sort(nums);
+int i=1;
+while(i<nums.length){
+if(nums[i]!=nums[i-1]){
+//single element comes in betwwn
+return nums[i-1];
+}
+i+=3;
+}
+return nums[nums.length-1];
     }
 }
