14bf1db2118a14d5438db266988002f08f7c79d9
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-05 00:41:20 +0530

    Time: 0 ms (100%), Space: 42.3 MB (28.76%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,33 +1,31 @@
 class Solution {
     public int search(int[] nums, int target) {
-     //in rotated soted array we know that one part of the array will be sorted aswe rotate array only at a point
-     int n=nums.length;
-     int low=0;
-     int high=n-1;
-     while(low<=high) {
-    int mid=low+(high-low)/2;
+    //in rotated sorted array we know that one half of the array will be osted if we will find the mid index we just need to find the soted half  to search the elelemrnt
+int n=nums.length;
+ int low=0;
+int high=n-1;
+while(low<=high) {
+int mid=(low+high)>>1;
 if(nums[mid]==target) return mid;
-//identify the sorted half
-if(nums[low]<=nums[mid]){
-//left  half is sorted
-//check wheater tthe target lies betwwen the sorted half
+else if(nums[low]<=nums[mid]){
+//means left half is sorted
 if(target>=nums[low] && target<=nums[mid]){
 high=mid-1;
 }
 else {
 low=mid+1;
 }
-} 
+}
 else {
-//other half is sorted
+//right half is sorted
 if(target>=nums[mid] && target<=nums[high]){
 low=mid+1;
 }
 else {
-    high=mid-1;
+high=mid-1;
+}
 }
 } 
-     } 
-     return -1;//if the  target  is not present in array 
+return  -1; 
     }
 }
