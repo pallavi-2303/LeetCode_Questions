b64d3c953a6572c7ff2147443bc2735677e2e937
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-05 00:33:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,47 +1,33 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-      /*int xor=0;
-      for(int num:nums) {
-        xor=xor^num;
-      }
+   /* int xor=0;
+      for(int num:nums){
+    xor=xor^num;
+      } 
       return xor; */
-     /* int n=nums.length;
-      if(n==1) return nums[0];
-for(int i=0;i<n;i++) {
-if(i==0 && nums[i]!=nums[i+1]){
-return nums[i];
-} 
-else if(i==n-1 && nums[i] !=nums[i-1] ){
+    int n=nums.length;
+    if(nums[0]!=nums[1]) return nums[0];
+    if(nums[n-1]!=nums[n-2]) return nums[n-1];
+   /* for(int i=1;i<n-1;i++){
+    if(nums[i]!=nums[i-1] && nums[i]!=nums[i+1]){
     return nums[i];
-}
-else if(i!=0 && i!=n-1){
-if(nums[i] !=nums[i-1] && nums[i]!=nums[i+1]){
-    return nums[i];
-}
-}
-}
-return -1;*/
-//we know that if we are on odd index if we get same elelemt in even index this means we areon right half of single element
-int n=nums.length;
-if(n==1) return nums[0];
-if(nums[0]!=nums[1]) return nums[0];
-if(nums[n-1]!=nums[n-2]) return nums[n-1];
+    }
+    }*/
 int low=1;
 int high=n-2;
 while(low<=high){
-int mid=low+(high-low)/2;
-//comparing with mid
-if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]){
+int mid=(low+high)>>1;
+if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
     return nums[mid];
 }
-//if we are on left half and single element is not right halh
-if((mid%2==1 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1]) ){
+else if((mid%2==1 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1])){
+//we are on left side of single elelemt
 low=mid+1;
 }
 else {
 high=mid-1;
 }
 }
-return -1;//if not found
+    return -1;
     }
 }
