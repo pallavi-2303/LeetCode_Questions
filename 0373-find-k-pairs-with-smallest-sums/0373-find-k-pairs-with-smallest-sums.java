20766758c85271f13a0b392e866bb9c5035786b4
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-05 03:30:41 +0530

    Time: 47 ms (39.01%), Space: 61.4 MB (39.46%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -1,44 +1,61 @@
 class Solution {
     public class tuple{
-int sum;
-int idxi;
-int idxj;
-public tuple(int sum,int idxi,int idxj) {
-this.sum=sum;
-this.idxi=idxi;
-this.idxj=idxj;
-}
-}
+    int sum;
+    int idxi;
+    int idxj;
+    public tuple(int sum,int idxi,int idxj){
+    this.sum=sum;
+    this.idxi=idxi;
+    this.idxj=idxj;
+    }
+    }
+    public class pair{
+    int idx_i;
+    int idx_j;
+    pair(int idx_i,int idx_j){
+    this.idx_i=idx_i;
+     this.idx_j=idx_j; }
+     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof pair)) return false;
+        pair other = (pair) o;
+        return this.idx_i == other.idx_i && this.idx_j == other.idx_j;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * idx_i + idx_j;
+    }
+    }
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-      //brute force trying out all the possible pairs till k and when sum is greater than k we will remove that
-PriorityQueue<tuple> pq=new PriorityQueue<>((a,b)->Integer.compare(b.sum,a.sum));
-int n=nums1.length;
-int m=nums2.length;
-for(int i=0;i<n;i++){
-for(int j=0;j<m;j++){
-int sum=nums1[i]+nums2[j];
-if(pq.size()<k) {
-pq.add(new tuple(sum,i,j));
-}
-else if(sum<pq.peek().sum) {
-pq.poll();
-pq.add(new tuple(sum,i,j));
-}
-else {
-//if pq size is greater and equal to k now next j value will be greater than this sum so just break the loop
-break;
-}
-}
-}
-List<List<Integer>> res=new ArrayList<>();
-while(!pq.isEmpty()) {
+    int n=nums1.length;
+    int m=nums2.length;
+    HashSet<pair> vis=new HashSet<>();
+PriorityQueue<tuple> pq=new  PriorityQueue<>((a,b)->(Integer.compare(a.sum,b.sum)));
+//at start
+pq.add(new tuple(nums1[0]+nums2[0],0,0));
+vis.add(new pair(0,0));
+List<List<Integer>> res=new  ArrayList<>();
+while(k>0 && !pq.isEmpty()){
+//if it has come out it has minimum sum
 tuple it=pq.poll();
-List<Integer> temp=new ArrayList<>();
-temp.add(nums1[it.idxi]);
-temp.add(nums2[it.idxj]);
+int i=it.idxi;
+int j=it.idxj;
+List<Integer> temp=Arrays.asList(nums1[i],nums2[j]);
 res.add(temp);
+//adding the valid pair  in pq
+if(j+1<m && !vis.contains(new pair(i,j+1))){
+pq.add(new tuple(nums1[i]+nums2[j+1],i,j+1));
+vis.add(new pair(i,j+1));
 }
-Collections.reverse(res);
-return res; 
+if(i+1<n && !vis.contains(new pair(i+1,j))){
+pq.add(new tuple(nums1[i+1]+nums2[j],i+1,j));
+vis.add(new pair(i+1,j));
+}
+k--;
+}
+return res;
+        
     }
 }
