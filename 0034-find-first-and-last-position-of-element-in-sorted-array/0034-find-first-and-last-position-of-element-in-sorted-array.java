4943b228c0f97ef83e4fb1b0689a2cafa0746bda
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-05 23:10:27 +0530

    Time: 1 ms (20.87%), Space: 45.9 MB (67.93%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,49 +1,16 @@
 class Solution {
-    public int findFloor(int[] nums,int target){
-    int n=nums.length;
-    int floor=-1;
-    int low=0;
-    int high=n-1;
-    while(low<=high){
-        int mid=low+(high-low)/2;
-        if(nums[mid]==target){
-            floor=mid;
-            high=mid-1;
-        }
-        else if(nums[mid]<target){
-            low=mid+1;
-        }
-        else {
-            high=mid-1;
-        }
-    }
-    return floor;
-    }
-    public int findCeil(int[] nums,int target){
-    int n=nums.length;
-    int ceil=-1;
-    int low=0;
-    int high=n-1;
-    while(low<=high){
-    int mid=low+(high-low)/2;
-    if(nums[mid]==target){
-        ceil=mid;
-        low=mid+1;
-    }
-    else if(nums[mid]<target){
-    low=mid+1;
-    }
-    else {
-        high=mid-1;
-    }
-    }
-    return ceil;
-    }
     public int[] searchRange(int[] nums, int target) {
-    int n=nums.length;
-    int floor=findFloor(nums,target);
-    int ceil=findCeil(nums,target);
-    int[] ans=new int[]{floor,ceil};
-    return ans;
+       //brute force
+int n=nums.length;
+int[] ans={-1,-1};
+for(int i=0;i<n;i++){
+if(nums[i]==target){
+ans[0]=i;
+break;}}
+for(int i=n-1;i>=0;i--){
+if(nums[i]==target){
+ans[1]=i;
+break;}}
+return ans; 
     }
 }
