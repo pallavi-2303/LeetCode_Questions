5d2e43ba9d238939ee0600cc4c4f240dfef8b64f
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-05 17:21:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,40 +1,13 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-//int n=nums.length;
-/*brute force;
-if(n==1) return 0;
-for(int i=0;i<n;i++){
-if(i==0){
-if(nums[i]>nums[i+1]) {
-  return i;
-}
-}
-else if(i==n-1) {
- if(nums[i]>nums[i-1]){
- return i;
- }
-}
-else {
-if(nums[i]>nums[i+1] && nums[i]>nums[i-1]) {
- return i;}
-}
-}
-return -1;*/
- int n=nums.length;
-int low=0;
-int high=n-1;
-while(low<=high) {
-int mid=low+(high-low)/2;
-if((mid==0 || nums[mid]>nums[mid-1]) && (mid==n-1 || nums[mid]>nums[mid+1])) {
-return mid;
-}
-else if(nums[mid]<nums[mid+1]) {
-low=mid+1;
-}
-else {
- high=mid-1;
-} }
-return -1;
-        
+      int n=nums.length;
+if(nums[0]>nums[1]) 
+return 0;
+if(nums[n-1]>nums[n-2])
+return n-1;
+for(int i=1;i<n-1;i++){
+if(nums[i]>nums[i-1] && nums[i]>nums[i+1])
+return i;}
+return -1;  
     }
 }
