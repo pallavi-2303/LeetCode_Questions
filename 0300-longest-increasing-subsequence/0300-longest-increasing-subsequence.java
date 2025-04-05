be301cbc5c867eadd89763809a8e6823320938e8
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-05 17:38:29 +0530

    Time: 396 ms (7.58%), Space: 77.6 MB (14.71%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,38 +1,21 @@
 class Solution {
-    public int maxLen(int idx,int prev,int[] arr,int[][] dp) {
-if(idx==arr.length) return 0;
-if(dp[idx][prev+1]!=-1)
-return dp[idx][prev+1];
-int nottake=0+maxLen(idx+1,prev,arr,dp);
-int take=0;
-if(prev==-1 || arr[idx]>arr[prev]){
-take=1+maxLen(idx+1,idx,arr,dp);
-}
-return dp[idx][prev+1]=Math.max(take,nottake);
-}
-    public int lengthOfLIS(int[] arr) {
-       int n=arr.length;
-int[][] dp=new int[n+1][n+1];
-// for(int[] a:dp)
-// Arrays.fill(a,-1);
-// return maxLen(0,-1,arr,dp); 
-int[] pre=new int[n+1];
-int[] curr=new int[n+1];
-for(int i=n-1;i>=0;i--){
- for(int prev=i-1;prev>=-1;prev--){
-  int nottake=0+dp[i+1][prev+1];
- // int nottake=0+pre[prev+1];
-  int take=0;
-  if(prev==-1 || arr[i]>arr[prev]) {
-  take=1+dp[i+1][i+1];
-// take=1+pre[i+1];
-  }
-  dp[i][prev+1]=Math.max(take,nottake);
- //curr[prev+1]=Math.max(take,nottake);
- }
- //pre=curr;
-}
-return dp[0][-1+1];
-//return pre[0];
+    public int LIS(int idx,int prev,int[] nums,int[][] dp){
+    //basecase
+    if(idx==nums.length) return 0;
+    if(dp[idx][prev+1]!=-1) return dp[idx][prev+1];
+    int nottake=LIS(idx+1,prev,nums,dp);
+    int take=0;
+    if(prev==-1 || nums[idx]>nums[prev]){
+    take=1+LIS(idx+1,idx,nums,dp);
+    }
+    return dp[idx][prev+1]=Math.max(take,nottake);
+    }
+    public int lengthOfLIS(int[] nums) {
+      int n=nums.length;
+      int[][] dp=new int[n+1][n+1];
+      for(int[] a:dp) {
+    Arrays.fill(a,-1);
+      }
+      return LIS(0,-1,nums,dp);
     }
 }
