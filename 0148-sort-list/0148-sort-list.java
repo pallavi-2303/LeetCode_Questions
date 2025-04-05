57b21eae272e2827b75b2b352d547c8083cb795e
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-05 15:44:20 +0530

    Time: 9 ms (94.72%), Space: 57.8 MB (5.26%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -9,10 +9,37 @@
  * }
  */
 class Solution {
-   
+   public ListNode findMiddle(ListNode head){
+    ListNode fast=head;
+    ListNode slow=head;
+   while(fast.next!=null && fast.next.next!=null) {
+    slow=slow.next;
+    fast=fast.next.next;
+   }
+   return slow;
+   }
+   public ListNode merge(ListNode left,ListNode right){
+    ListNode head=new ListNode(0);
+    ListNode temp=head;
+  while(left!=null && right!=null) {
+if(left.val<=right.val){
+temp.next=left;
+temp=left;
+left=left.next;
+}
+else{
+    temp.next=right;
+    temp=right;
+    right=right.next;
+}
+  } 
+if(left==null) temp.next=right;
+else temp.next=left;
+return head.next;
+   }
     public ListNode sortList(ListNode head) {
     //brute force putting in aaray amd sorting it and then converting in lined list
-   if(head==null || head.next==null) return head;
+  /* if(head==null || head.next==null) return head;
     List<Integer> list=new ArrayList<>() ;
     ListNode temp=head;
     while(temp!=null){
@@ -28,6 +55,17 @@ ListNode a=new ListNode(list.get(i));
 temp.next=a;
 temp=temp.next;
 }
-return head;
+return head;*/
+//uding the merge sort alogorithrm
+//base case
+if(head==null || head.next==null) return head;
+ListNode middle= findMiddle(head);
+ListNode LeftHead=head;
+ListNode RightHead=middle.next;
+middle.next=null;
+//now calling the left and right head to sort
+LeftHead=sortList(LeftHead);
+RightHead=sortList(RightHead);
+return merge(LeftHead,RightHead);
     }
 }
