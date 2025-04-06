5c1cf64b3b51724b590b510e37ea590660ada789
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-06 22:32:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.java
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -1,29 +1,35 @@
 class Solution {
     public List<Integer> largestDivisibleSubset(int[] nums) {
-       int n=nums.length;
-//here we can store the length and longest increasing subset
-Arrays.sort(nums);
-int[] dp=new int[n];
-Arrays.fill(dp,1);
-int[] hash=new int[n];
-for(int i=1;i<n;i++){
- hash[i]=i;
-for(int prev=0;prev<i;prev++){
-if(nums[i]%nums[prev]==0 && 1+dp[prev]>dp[i]) {
-dp[i]=1+dp[prev];
-hash[i]=prev;}}}
-int lastidx=-1;
-int lastele=0;
-for(int i=0;i<n;i++){
-if(dp[i]>lastele) {
- lastele=dp[i];
- lastidx=i;}}
-List<Integer> ans=new ArrayList<>();
-ans.add(nums[lastidx]);
-while(lastidx!=hash[lastidx]) {
- lastidx=hash[lastidx];
- ans.add(nums[lastidx]);
-}
-return ans; 
+     int n=nums.length;
+     List<Integer> ans=new ArrayList<>();
+     int[] dp=new int[n];
+     int[] hash=new int[n];
+     Arrays.fill(dp,1);
+     for(int i=1;i<n;i++) {
+    //initially marking for the current index
+    hash[i]=i;
+    for(int prev=0;prev<i;prev++){
+    if(nums[i]%nums[prev]==0 && 1+dp[prev]>dp[i]){
+    dp[i]=1+dp[prev];
+    //mark the index of pprevious sequence;
+    hash[i]=prev;
+    }
+    }
+     }
+    //finding out the last ellemnt and last index whose dp valus if maximum
+    int maxLen=0;
+    int lastIdx=-1;
+    for(int i=0;i<n;i++){
+    if(dp[i]>maxLen){
+    maxLen=dp[i];
+    lastIdx=i;
+    }
+    }
+  ans.add(nums[lastIdx]);
+  while(hash[lastIdx]!=lastIdx){
+lastIdx=hash[lastIdx];
+ans.add(nums[lastIdx]);
+  }
+  return ans;
     }
 }
