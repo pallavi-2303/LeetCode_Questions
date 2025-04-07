2cf27f90f3c17e2b7b3f7a4dcfdacc1ae143e787
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-08 00:36:25 +0530

    Time: 117 ms (13.7%), Space: 57.2 MB (78.02%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,38 +1,38 @@
 class Solution {
-    public void NSE(int[] arr,int[] nse){
-    Stack<Integer> st=new Stack<>() ;
-    int n=arr.length;
-    for(int i=n-1;i>=0;i--) {
-    while(!st.isEmpty() && arr[st.peek()]>=arr[i]){
-        st.pop();
-    }
-    nse[i]=st.isEmpty() ? n:st.peek();
-    st.push(i);
-    }  
-    }
-      public void PSE(int[] arr,int[] pse){
-    Stack<Integer> st=new Stack<>() ;
-    int n=arr.length;
-    for(int i=0;i<n;i++) {
-    while(!st.isEmpty() && arr[st.peek()]>=arr[i]){
-        st.pop();
-    }
-    pse[i]=st.isEmpty() ? -1:st.peek();
-    st.push(i);
-    }  
-    }
-    public int largestRectangleArea(int[] heights) {
-      int n=heights.length;
-       int[] nse=new int[n];
-       int[] pse=new int[n];
-        NSE(heights,nse);
-        PSE(heights,pse);
-     int maxArea=0;
-     //calculating  area of ech rectange
-     for(int i=0;i<n;i++) {
-    int area=heights[i]*(nse[i]-pse[i]-1);
-    maxArea=Math.max(area,maxArea);
-     }
-     return maxArea;  
+    public void findNSE(int[] nums,int[] nse){
+Stack<Integer> st=new Stack<>();
+int n=nums.length;
+for(int i=n-1;i>=0;i--){
+while(!st.isEmpty() && nums[st.peek()]>=nums[i]) {
+st.pop();
+}
+nse[i]=st.isEmpty() ? n : st.peek();
+st.push(i);
+}
+}
+public void findPSE(int[] nums,int[] pse) {
+Stack<Integer> st=new Stack<>();
+int n=nums.length;
+for(int i=0;i<n;i++){
+while(!st.isEmpty() && nums[st.peek()]>=nums[i]) {
+st.pop();
+}
+pse[i]=st.isEmpty() ? -1 : st.peek();
+st.push(i);
+}
+}
+    public int largestRectangleArea(int[] nums) {
+      int n=nums.length;
+int[] pse=new int[n];
+int[] nse=new int[n];
+findPSE(nums,pse);
+findNSE(nums,nse);  
+//since we have yo find the maximum area rectangle we will take max height and widht between nse and pse element because till that area can be calculated
+int maxi=Integer.MIN_VALUE;
+for(int i=0;i<n;i++){
+int area=nums[i]*(nse[i]-pse[i]-1);
+maxi=Math.max(maxi,area);
+}
+return maxi;
     }
 }
