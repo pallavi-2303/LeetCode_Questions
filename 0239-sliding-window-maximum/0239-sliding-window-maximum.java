daf8aeec8248ba027dc2446b05f515b29f731da8
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-08 01:38:39 +0530

    Time: 477 ms (5.02%), Space: 62.7 MB (17.21%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,24 +1,39 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-int n=nums.length;
- int[] nge=new int[n];
+    int n=nums.length;
+    int[] ans=new int[n-k+1];
+    //brute force generating all the windows and finding max from all the windows
+  /*  for(int i=0;i<=n-k;i++) {
+    int maxi=Integer.MIN_VALUE;
+    //this the loop for generating all the windows of size k
+    for(int j=i;j<k+i;j++){
+     maxi=Math.max(maxi,nums[j]);
+    }
+ans[i]=maxi;
+    }  */
+//finding out the next greater ellement of stack and using stack concept
 Stack<Integer> st=new Stack<>();
-for(int i=n-1;i>=0;i--) {
- while(!st.isEmpty() && nums[i]>nums[st.peek()]) {
-   st.pop();}
- nge[i]=st.isEmpty() ? n : st.peek();
- st.push(i);}
-int[] ans=new int[n-k+1];
- int z=0;
-  int j=0;
- for(int i=0;i<n-k+1;i++) {
- 
-  if(j>=i+k) j=i;
-  int maxi=nums[j];
- while(j<i+k) {
-  maxi=nums[j];
-  j=nge[j];}
-  ans[z++]=maxi;}
-        return ans;
+int[] nge=new int[n];
+for(int i=n-1;i>=0;i--){
+while(!st.isEmpty() && nums[i]>nums[st.peek()]){
+    st.pop();
+}
+nge[i]=st.isEmpty() ? n:st.peek();
+st.push(i);
+}
+//calculating sliding window maximum
+int j=0;
+int maxi=0;
+for(int i=0;i<=n-k;i++){
+//if j has gone to next window
+if(j>=i+k) j=i;
+maxi=nums[j];
+while(j<i+k){
+maxi=nums[j];
+j=nge[j];
+}
+ans[i]=maxi;
+}
+    return ans;
     }
 }
