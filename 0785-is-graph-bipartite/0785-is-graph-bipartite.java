9abf3a809041ecd5233eeb5adca32283fb7404c9
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-08 23:04:36 +0530

    Time: 0 ms (100%), Space: 45.1 MB (65.41%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,70 +1,42 @@
 class Solution {
-    public class pair{
-    int node;
-    int col;
-    public pair(int node,int col){
-    this.node=node;
-    this.col=col;
-    }
-    }
-    public boolean dfs(int node,int[] color,int col,int[][]
-    adj) {
-  //marking the color
-  color[node]=col;
-for(int it:adj[node]) {
-if(color[it]==-1) {
- //if it is not colored
- if(dfs(it,color,1-col,adj)==false){
-  return false;
- }
-}
-else {
-//if it is already colored
-if(color[it]==color[node]) {
-  return false;
-}
-}
-}
-return true;//if we are able to color the whole graph
-    }
-    public boolean bfs(int i,int[] color,int c,int[][]
-    adj) {
-     Queue<pair> q=new LinkedList<>();
-     q.add(new pair(i,c));
-     color[i]=c;
-     while(!q.isEmpty()) {
-     pair pt=q.poll();
-     int node=pt.node;
-     int col=pt.col;
-   for(int it:adj[node]) {
-  if(color[it]==-1) {
-  color[it]=1-col;
-  q.add(new pair(it,color[it]));
-  }
-  else {
-  if(color[it]==color[node]) {
-  return false;
-  }
-  }
-   }
-    
-     }
-     return true;
-    }
+    public class pair {
+int node;
+int color;
+pair(int node,int color){
+this.node=node;
+this.color=color;}}
+   /* public boolean bfs(int i,int[] col,int[][] adj,int color2){
+Queue<pair> q=new LinkedList<>();
+q.add(new pair(i,color2));
+while(!q.isEmpty()){
+pair it=q.poll();
+int node=it.node;
+int color=it.color;
+for(int it:adj[node]){
+if(col[it]==-1){
+col[it]=1-color;
+q.add(new pair(it,col[it]));}
+else if(col[it]==col[node]){
+return false;}}}
+return true;}*/
+    public boolean dfs(int node,int[] col,int[][] adj,int color){
+col[node]=color;
+for(int it:adj[node]){
+if(col[it]==-1){
+if(dfs(it,col,adj,1-color)==false){
+return false;}}
+//If it is already colored
+else if(col[it]==col[node]){
+return false;}}
+return true;}
     public boolean isBipartite(int[][] graph) {
-      //firstly we perform dfs
     int V=graph.length;
-    int[] color=new int[V];
-    //we will color it by 0 and 1 
-    Arrays.fill(color,-1);
-   for(int i=0;i<V;i++){
-   if(color[i]==-1) {
- if(dfs(i,color,0,graph)==false){
-// if(bfs(i,color,0,graph)==false) {
-   return false; }
-  }
-   }
-   return true;
-      
+        int[] color=new int[V];
+Arrays.fill(color,-1);
+for(int i=0;i<V;i++){
+if(color[i]==-1){
+if(dfs(i,color,graph,0)==false){
+return false;}}}
+return true;
     }
 }
