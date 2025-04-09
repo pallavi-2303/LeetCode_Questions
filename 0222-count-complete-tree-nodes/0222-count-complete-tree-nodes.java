34512e2b90effd3fcec09ec63a833f146e5de75e
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-09 23:53:39 +0530

    Time: 0 ms (100%), Space: 48 MB (27.05%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -14,41 +14,8 @@
  * }
  */
 class Solution {
- public void countNodes2(TreeNode root,int[] count){
- if(root==null) return ;
-   count[0]=count[0]+1;
-   countNodes2(root.left,count);
-    countNodes2(root.right,count);}
-public int findLeftNodes(TreeNode root){
- root=root.left;
-  int count=0;
-  while(root!=null){
-    count++;
-    root=root.left;}
-    return count;}
-public int findRightNodes(TreeNode root){
-  root=root.right;
-  int count=0;
-  while(root!=null){
-    count++;
-    root=root.right;}
- return count;}
     public int countNodes(TreeNode root) {
-  //brute force counting all nodes
-/*int[] count=new int[1];
- countNodes2(root,count);
-      return count[0];*/
-//we will use height property
-  if(root==null) return 0;
-int left=findLeftNodes(root);
-int right=findRightNodes(root);
-if(left==right){
-return ((2<<left)-1);}
- else {
- return 1+countNodes(root.left)+countNodes(root.right);}
-
-
-
-
+     if(root==null) return 0;
+     return  countNodes(root.left)+countNodes(root.right)+1;
     }
 }
