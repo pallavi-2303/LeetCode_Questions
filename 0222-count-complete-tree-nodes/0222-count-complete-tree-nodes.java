2102ca75cfbad67a337010c3a5df77a3230856b6
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-10 00:13:33 +0530

    Time: 0 ms (100%), Space: 47.7 MB (62.95%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -14,8 +14,41 @@
  * }
  */
 class Solution {
+    public int findHeight1(TreeNode root){
+    int count=0;
+    while(root!=null){
+        count++;
+        root=root.left;
+    }
+    return count;
+    }
+    public int findHeight2(TreeNode root){
+        int count=0;
+        while(root!=null){
+            count++;
+            root=root.right;
+        }
+        return count;
+    }
+    //private int count=0;
     public int countNodes(TreeNode root) {
-     if(root==null) return 0;
-     return  countNodes(root.left)+countNodes(root.right)+1;
+     //brute force
+   /*  if(root==null) return 0;
+     count++;
+     countNodes(root.left);
+     countNodes(root.right);
+     return count;*/
+    //if at any point the height of the tree are equal we can use the formula .. and to find the left and right hright the current node is also included
+ if(root==null) return 0;
+ int left=findHeight1(root) ;
+ int right=findHeight2(root) ;
+ if(left==right){
+  return ((1<<left)-1);
+ }
+
+    //calculate the left and right height at each point by going to its left and right
+ else return 1+countNodes(root.left)+countNodes(root.right);
+ 
+
     }
 }
