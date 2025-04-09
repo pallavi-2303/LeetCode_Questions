07de3f0085686b05e795c0560bf60a0c2b14370b
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-09 23:48:50 +0530

    Time: 0 ms (100%), Space: 44.6 MB (26.54%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -14,18 +14,29 @@
  * }
  */
 class Solution {
-public int pathSum(TreeNode root,int[] sum){
-if(root==null) return 0;
-  //if at any point sum become negative
-int left=Math.max(0,pathSum(root.left,sum));
-int right=Math.max(0,pathSum(root.right,sum));
-//sum can pass through root or it can be left or right we have to take max of all
-sum[0]=Math.max(sum[0],left+right+root.val);
-//as there is negative value also 
-return root.val+Math.max(left,right);}
+    public int Height(TreeNode root){
+if(root==null){
+return 0;}
+int left=Height(root.left);
+int right=Height(root.right);
+return Math.max(left,right)+root.val;}
+    public int sum(TreeNode root){
+if(root==null){
+return 0;}
+int currSum=Height(root.left)+Height(root.right)+root.val;
+int left=sum(root.left);
+int right=sum(root.right);
+return Math.max(currSum,Math.max(left,right));}
+public int sum2(TreeNode root,int[] max){
+if(root==null){
+return 0;}
+int left=Math.max(0,sum2(root.left,max));
+int right=Math.max(0,sum2(root.right,max));
+max[0]=Math.max(max[0],left+right+root.val);
+return Math.max(left,right)+root.val;}
     public int maxPathSum(TreeNode root) {
-int[] sum={Integer.MIN_VALUE};
-  pathSum(root,sum);
-      return sum[0];
+     int[] maxi={Integer.MIN_VALUE};
+ sum2(root,maxi);
+return maxi[0];   
     }
 }
