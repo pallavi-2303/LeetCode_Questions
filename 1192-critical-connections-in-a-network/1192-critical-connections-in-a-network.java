d95897925e660bef8947d7e126fc927eed5b6997
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-11 00:28:33 +0530

    Time: 87 ms (68.24%), Space: 113 MB (83%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
@@ -1,44 +1,48 @@
 class Solution {
 private int timer=1;
- public void dfs(int node,int parent,int[] vis,int[] time,int[] low,List<List<Integer>> adj, List<List<Integer>> ans) {
-  vis[node]=1;
-  time[node]=low[node]=timer++;
-  //visiting the neighbour 
-  for(int it:adj.get(node)) {
-  if(it==parent) continue;
-  if(vis[it]==0){
-  dfs(it,node,vis, time,low,adj,ans);
-  //after performing dfs before backtracking update low of node
- low[node]=Math.min(low[node],low[it]);
- //checking bridge condition node->it 
- if(low[it]>time[node]) {
- // means node can't be reached anyhow by adjacent node so it is bridge
- ans.add(Arrays.asList(node,it));
- }}
- else {
- //it is alteady visit update low.
- low[node]=Math.min(low[node],low[it]);
- }}
+    public void dfs(int node,int parent,List<List<Integer>> adj,int[] vis,List<List<Integer>> ans,int[] time,int[] low ){
+vis[node]=1;
+low[node]=time[node]=timer++;
+//Now visting all the  nodes
+for(int it:adj.get(node)){
+if(it==parent) continue;
+if(vis[it]==0){
+//perform the dfs
+dfs(it,node,adj,vis,ans,time,low);
+//after dfs of current node it done
+low[node]=Math.min(low[node],low[it]);
+//checking the conditio for bridge
+if(low[it]>time[node]){
+//this adjacentt node cant reach the current nnode via any of  its neghbour means it is not connected
+//node--it
+ans.add(Arrays.asList(node,it));
 }
+}
+else {
+//if adjacent node it alreaddy visited
+low[node]=Math.min(low[node],low[it]);
+}}
+    }
     public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
-        List<List<Integer>> adj=new ArrayList<>();
+     //first we will make adjacency list of graph
+      //Making of adjacency list 
+List<List<Integer>> adj=new ArrayList<>();
 for(int i=0;i<n;i++){
  adj.add(new ArrayList<>());
 }
-int m=connections.size();
-//making adjacency list
-for(int i=0;i<m;i++){
-int u=connections.get(i).get(0);
-int v=connections.get(i).get(1);
+for(List<Integer> it:connections){
+int u=it.get(0);
+int v=it.get(1);
 adj.get(u).add(v);
 adj.get(v).add(u);
 }
-//performing the dfs
-int[] vis=new int[n];
+List<List<Integer>> ans=new ArrayList<>(); 
 int[] time=new int[n];
 int[] low=new int[n];
-List<List<Integer>> ans=new ArrayList<>();
-dfs(0,-1,vis,time,low,adj,ans);
+int[] vis=new int[n];
+//performing the dfsto find the bridges in graph
+dfs(0,-1,adj,vis,ans,time,low);
 return ans;
+
     }
 }
