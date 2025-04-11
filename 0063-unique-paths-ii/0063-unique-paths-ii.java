fc6b4f3d79e8aad8bdac16a72a9121db24288f94
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-11 17:57:43 +0530

    Time: 0 ms (100%), Space: 41.3 MB (99.21%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,36 +1,22 @@
 class Solution {
-    public int findPath(int i,int j,int[][] mat,int[][] dp) {
-//base cases
-if(i<0 || j<0) return 0;
-if(mat[i][j]==1) return 0;
-if(i==0 && j==0) return 1;
+    public int findPath(int i,int j,int[][] grid,int[][] dp){
+if(j<0 || i<0 ) return 0;
+if(grid[i][j]==1) return 0;
+if(i==0 && j==0){
+if(grid[i][j]==0) return 1;
+else return 0;}
 if(dp[i][j]!=-1) return dp[i][j];
-int up=findPath(i-1,j,mat,dp);
-int left=findPath(i,j-1,mat,dp);
+int up=findPath(i-1,j,grid,dp);
+int left=findPath(i,j-1,grid,dp);
 return dp[i][j]=up+left;
-}
-    public int uniquePathsWithObstacles(int[][] mat) {
-    int n=mat.length;
-    int m=mat[0].length;
+    }
+    public int uniquePathsWithObstacles(int[][] grid) {
+     int n=grid.length;
+     int m=grid[0].length;
+     if(grid[n-1][m-1]==1) return 0;
      int[][] dp=new int[n][m];
-// for(int[] a:dp)
-// Arrays.fill(a,-1);
-// return findPath(n-1,m-1,mat,dp);   
-for(int i=0;i<n;i++){
-for(int j=0;j<m;j++){
-if(mat[i][j]==1) dp[i][j]=0;
-else if(i==0 && j==0){
-dp[i][j]=1;
-}
-else {
- int up=0;
- int left=0;
- if(i>0) up=dp[i-1][j];
- if(j>0) left=dp[i][j-1];
- dp[i][j]=up+left;
-}
-}
-}
-return dp[n-1][m-1];
+     for(int[] a:dp) Arrays.fill(a,-1);
+
+     return findPath(n-1,m-1,grid,dp);
     }
 }
