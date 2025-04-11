05b742db009893ef8ed8a47f607f04be7fa87ea2
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-11 18:12:36 +0530

    Time: 0 ms (100%), Space: 41.8 MB (27.83%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -14,57 +14,22 @@
  * }
  */
 class Solution {
-      public void inOrder(TreeNode root,List<Integer> res) {
-if(root==null) return;
-inOrder(root.left,res);
-res.add(root.val);
-inOrder(root.right,res);
-}
     public List<Integer> inorderTraversal(TreeNode root) {
-List<Integer> res=new ArrayList<>();
-if(root==null) return res;
-// inOrder(root,res);
-//Interative aproach
-// Stack<TreeNode> st=new Stack<>();
-// while(true){
-// if(root!=null) {
-// st.push(root);
-// root=root.left;
-// }  
-// else {
-// if(st.isEmpty()) break;
-// TreeNode curr=st.pop();
-// res.add(curr.val);
-// root=curr.right;
-// } 
-// }
-//Morris Traversal so there are two case if root->left is null or if it not null 
-while(root!=null){
-//left root and right
-if(root.left==null){
-res.add(root.val);
-//move to right side
-root=root.right;
-}
-else {
-//go to right most guy of left part and coonect the theread  it also has two caes
-TreeNode prev=root.left;
-while(prev.right!=null && prev.right!=root){
-prev=prev.right;
-}
-//case 1 if prev right is null
-if(prev.right==null){
-prev.right=root;
+     Stack<TreeNode> st=new Stack<>();
+List<Integer> ans=new ArrayList<>();
+if(root==null) return ans;
+while(true) {
+if(root!=null) {
+st.push(root);
 root=root.left;
 }
 else {
-//it is the place we have travsered the left aprt we have come to root node via right pointer 
-prev.right=null;
-res.add(root.val);
-root=root.right;
+if(st.isEmpty()) break;
+TreeNode curr=st.pop();
+ans.add(curr.val);
+root=curr.right;
 }
 }
-}
-return res;
+return ans;   
     }
 }
