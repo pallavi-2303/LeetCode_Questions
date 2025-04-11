5325cee3780a89c07232d690689d71de084fc5d6
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-12 01:03:55 +0530

    Time: 4 ms (46.49%), Space: 44.1 MB (77.8%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -10,8 +10,30 @@ return dp[i][j]=Math.min(take,nottake);
     public int minimumTotal(List<List<Integer>> arr) {
        int n=arr.size();
 int[][] dp=new int[n][n];
-for(int[] a:dp)
-Arrays.fill(a,-1);
-return findSum(0,0,arr,dp); 
+// for(int[] a:dp)
+// Arrays.fill(a,-1);
+// return findSum(0,0,arr,dp); 
+
+
+//space optimization
+int[] prev=new int[n];
+int[] curr=new int[n];
+for(int j=0;j<n;j++){
+ dp[n-1][j]=arr.get(n-1).get(j);
+ prev[j]=arr.get(n-1).get(j);
+}
+for(int i=n-2;i>=0;i--){
+for(int j=i;j>=0;j--){
+//int take=arr.get(i).get(j)+dp[i+1][j];
+int take=arr.get(i).get(j)+prev[j];
+//int notake=arr.get(i).get(j)+dp[i+1][j+1];
+int nottake=arr.get(i).get(j)+prev[j+1];
+//dp[i][j]=Math.min(take,nottake);
+curr[j]=Math.min(take,nottake);
+}
+prev=curr.clone();
+}
+//return dp[0][0];
+return prev[0];
     }
 }
