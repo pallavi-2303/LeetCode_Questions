0d3e2724f4ce1ccfb7fa14d674577628a8272408
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-11 17:29:33 +0530

    Time: 0 ms (100%), Space: 42.3 MB (50.57%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -17,12 +17,26 @@ class Solution {
     TreeNode prev=null;
     public void flatten(TreeNode root) {
      //recurrsive code by post order traversal
-if(root==null) return ;
-flatten(root.right);
-flatten(root.left);
-//do operation on node
-root.right=prev;
-root.left=null;
-prev=root;
+// if(root==null) return ;
+// flatten(root.right);
+// flatten(root.left);
+// //do operation on node
+// root.right=prev;
+// root.left=null;
+// prev=root;
+//recurrsive traversal of morris traversal
+while(root!=null){
+if(root.left!=null){
+TreeNode prev=root.left;
+//go to rightmost guy
+ while(prev.right!=null)  {
+prev=prev.right;
+ }
+ prev.right=root.right;
+ root.right=root.left;
+ root.left=null;
+}
+//move previous to left 
+root=root.right;}
     }
 }
