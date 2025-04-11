5e5c7fe29654f3a6d4104be3b0137e3b7671926b
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-11 18:00:33 +0530

    Time: 1 ms (19.51%), Space: 41.9 MB (50.66%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -15,8 +15,26 @@ return dp[i][j]=up+left;
      int m=grid[0].length;
      if(grid[n-1][m-1]==1) return 0;
      int[][] dp=new int[n][m];
-     for(int[] a:dp) Arrays.fill(a,-1);
+    //  for(int[] a:dp) Arrays.fill(a,-1);
 
-     return findPath(n-1,m-1,grid,dp);
+    //  return findPath(n-1,m-1,grid,dp);
+    for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+    if(i==0 && j==0){
+    if(grid[i][j]==0){
+    dp[i][j]=1;
+    }
+    else dp[i][j]=0;
+    }
+else {
+int up=0;
+int left=0;
+if(i>0 && grid[i][j]==0) up=dp[i-1][j];
+if(j>0 && grid[i][j]==0) left=dp[i][j-1];
+dp[i][j]=left+up;
+}
+    }
+    }
+return dp[n-1][m-1];
     }
 }
