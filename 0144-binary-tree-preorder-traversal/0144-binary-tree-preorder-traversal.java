2a8134c43a9a34429777a2b129e01e4aa48fd272
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-11 18:20:31 +0530

    Time: 0 ms (100%), Space: 41.4 MB (92.51%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -14,53 +14,30 @@
  * }
  */
 class Solution {
-public void preOrder(TreeNode root,List<Integer> res) {
-if(root==null)
-return;
-res.add(root.val);
-preOrder(root.left,res);
-preOrder(root.right,res);
-}
     public List<Integer> preorderTraversal(TreeNode root) {
-List<Integer> res=new ArrayList<>();
-// preOrder(root,res);
-if(root==null) return res;
-// Stack<TreeNode> st=new Stack<>();
-// st.push(root);
-// while(!st.isEmpty()){
-//  root=st.pop();
-//  res.add(root.val);
-//  if(root.right!=null)  st.push(root.right);
-//  if(root.left!=null) st.push(root.left);
-// }
-// return res;
-//morris traversal
-//root left right
-while(root!=null){
-if(root.left==null){
-res.add(root.val);
+      List<Integer> ans=new ArrayList<>();
+if(root==null) return ans;
+while(root!=null) {
+if(root.left==null) {
+ans.add(root.val);
 root=root.right;
 }
 else {
-//if root left is not null go to right mos node and connect pointer to root so that it can return back
 TreeNode prev=root.left;
-while(prev.right!=null && prev.right!=root){
+while(prev.right!=null && prev.right!=root) {
 prev=prev.right;
 }
-if(prev.right==null){
-//since here we are standing at the root 
-res.add(root.val);
+if(prev.right==null) {
 prev.right=root;
+ans.add(root.val);
 root=root.left;
 }
 else {
-//if it is already coonected
 prev.right=null;
 root=root.right;
 }
 }
 }
-return res;
-        
+return ans;  
     }
 }
