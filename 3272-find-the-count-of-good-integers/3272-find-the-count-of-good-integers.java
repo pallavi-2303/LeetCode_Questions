0225daae91c463edb25153d49e5006d0d4df62ea
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-13 00:06:19 +0530

    Time: 391 ms (44%), Space: 48 MB (32%) - LeetHub

--- 3272-find-the-count-of-good-integers/3272-find-the-count-of-good-integers.java
+++ 3272-find-the-count-of-good-integers/3272-find-the-count-of-good-integers.java
@@ -1,57 +1,50 @@
 class Solution {
     public long countGoodIntegers(int n, int k) {
-     Set<String> st = new HashSet<>();
-
-        int d = (n + 1) / 2;
-        int start = (int) Math.pow(10, d - 1);
-        int end = (int) Math.pow(10, d) - 1;
-
-        for (int num = start; num <= end; num++) {
-            String leftHalf = Integer.toString(num);
-            String full;
-
-            if (n % 2 == 0) {
-                StringBuilder rightHalf = new StringBuilder(leftHalf).reverse();
-                full = leftHalf + rightHalf.toString();
-            } else {
-                StringBuilder rightHalf = new StringBuilder(leftHalf.substring(0, d - 1)).reverse();
-                full = leftHalf + rightHalf.toString();
-            }
-
-            long number = Long.parseLong(full);
-            if (number % k != 0)
-                continue;
-
-            char[] chars = full.toCharArray();
-            Arrays.sort(chars);
-            st.add(new String(chars));
-        }
-
-        long[] factorial = new long[11];
-        factorial[0] = 1;
-        for (int i = 1; i < 11; i++) {
-            factorial[i] = factorial[i - 1] * i;
-        }
-
-        long result = 0;
-        for (String s : st) {
-            int[] count = new int[10];
-            for (char ch : s.toCharArray()) {
-                count[ch - '0']++;
-            }
-
-            int totalDigits = s.length();
-            int zeroDigits = count[0];
-            int nonZeroDigits = totalDigits - zeroDigits;
-
-            long perm = nonZeroDigits * factorial[totalDigits - 1];
-            for (int i = 0; i < 10; i++) {
-                perm /= factorial[count[i]];
-            }
-
-            result += perm;
-        }
-
-        return result;   
+     //since we have to count the combination of every string we will try to findout  a string and count all permutaions by taking left half and making it pallindrome
+int d=(n+1)/2;
+int start=(int) Math.pow(10,d-1) ;
+int end=(int)Math.pow(10,d)-1;
+HashSet<String> st=new HashSet<>();
+for(int num=start;num<=end;num++){
+String leftSide=Integer.toString(num);
+String full;
+if(n%2==0){
+StringBuilder right=new StringBuilder(leftSide).reverse();
+full=leftSide+right.toString();
+}
+else {
+StringBuilder right=new StringBuilder(leftSide.substring(0,d-1)).reverse();
+full=leftSide+right.toString();
+}
+long number=Long.parseLong(full);
+if(number%k!=0) continue;
+//we have to store the elelemts by sorting it
+char[] charArray=full.toCharArray();
+Arrays.sort(charArray);
+st.add(new String(charArray));
+}
+//let us precalculate the factoral of all the numbers from 0 to 10
+long[] factorial=new long[11];
+factorial[0]=1;
+for(int i=1;i<11;i++){
+ factorial[i]=factorial[i-1]*i;}
+ //iterating all the characetrs of set and find out all the coninations and also devide by permuation of reprating numbers
+ long result=0;
+ for(String str:st) {
+int[] count=new int[10];
+for(char ch:str.toCharArray()){
+count[ch-'0']++;
+}
+int totlength=str.length();
+int zero=count[0];
+int nonzero=totlength-zero;
+long permutation=nonzero*factorial[totlength-1];
+//devide by reprating chracters
+for(int i=0;i<10;i++){
+permutation/=factorial[count[i]];
+}
+result+=permutation;
+ } 
+ return result;
     }
 }
