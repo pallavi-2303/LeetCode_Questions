ca355b345eaa15e3cc3bb8f908c11289b576f6d1
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-13 17:02:16 +0530

    Time: 1 ms (47.32%), Space: 45.1 MB (32.49%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,59 +1,26 @@
 class Solution {
-    public class pair{
-        int row;
-        int col;
-        public pair(int row,int col){
-            this.row=row;
-            this.col=col;
-        }
-    }
-    public void bfs(int i,int j,int[][] image,int[][] vis,int color,int inicolor,int[] delrow,int[] delcol){
-          int n=image.length;
-    int m=image[0].length;
-    Queue<pair> q=new LinkedList<>();
-    q.add(new pair(i,j));
-    vis[i][j]=color;
-    while(!q.isEmpty()){
-    pair it=q.poll();
-    int row=it.row;
-    int col=it.col;
-    //traversing the 4 neighbour
-    for(int idx=0;idx<4;idx++) {
-    int nrow=row+delrow[idx];
-    int ncol=col+delcol[idx];
-    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]!=color && image[nrow][ncol]==inicolor){
-    vis[nrow][ncol]=color;
-    q.add(new pair(nrow,ncol));
-    }
-    }  
-    }
-    }
-    public void dfs(int row,int col,int[][] image,int[][] vis,int color,int inicolor,int[] delrow,int[] delcol){
-        int n=image.length;
-    int m=image[0].length;
-        vis[row][col]=color;//coloring it with given color
-//moving in four direction
+    public void dfs(int sr,int sc,int color,int inicolor,int[][] vis,int[][] image,int[] dr,int[] dc) {
+int n=image.length;
+int m=image[0].length;
+vis[sr][sc]=color;
+//try in 4 direction
 for(int i=0;i<4;i++){
-int nrow=row+delrow[i];
-int ncol=col+delcol[i];
-//checking all the condition
-if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]!=color && image[nrow][ncol]==inicolor){
-    dfs(nrow,ncol,image,vis,color,inicolor,delrow,delcol);
+int nrow=sr+dr[i];
+int ncol=sc+dc[i];
+if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && vis[nrow][ncol]!=color && image[nrow][ncol]==inicolor) {
+dfs(nrow,ncol,color,inicolor,vis,image,dr,dc);
 }
-} 
-    }
+}
+}
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-    //in thhis we can use bfs and dfs both to color the image
-    int n=image.length;
-    int m=image[0].length;
-    int[][] vis=new int[n][m];
-    vis=image;
-    int inicolor=image[sr][sc];
-    int[] delrow={-1,1,0,0} ;
-    int[] delcol={0,0,-1,1};
-   // dfs(sr,sc,image,vis,color,inicolor,delrow,delcol);
-   //now the bfs approach
-   bfs(sr,sc,image,vis,color,inicolor,delrow,delcol);
-    return vis;
+       int n=image.length;
+int m=image[0].length;
+int[][] vis=new int[n][m];
+vis=image;
+int[] delrow={-1,1,0,0};
+int[] delcol={0,0,-1,1};
+int initialcolor=image[sr][sc];
+dfs(sr,sc,color, initialcolor,vis,image,delrow,delcol);
+return vis; 
     }
 }
