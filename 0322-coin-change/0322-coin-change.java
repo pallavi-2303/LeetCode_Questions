0606e03aa7d1f0d3e51134f6fe3bda99b8d57541
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-16 00:51:16 +0530

    Time: 27 ms (29.29%), Space: 45.1 MB (32.68%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,68 +1,23 @@
 class Solution {
-    public int memo(int idx,int[] nums,
-int[][] dp,int target) {
+    public int coinChange2(int idx,int target,int[] nums,int[][] dp){
 if(target==0) return 0;
-if(idx==0) {
-if(target%nums[0]==0) {
-return target/nums[0];
-}
-else {
-return (int)1e9;
-}
-}
-if(dp[idx][target]!=-1) return dp[idx][target];
-int take=(int)1e9;
-if(nums[idx]<=target) 
-take=1+memo(idx,nums,dp,
-target-nums[idx]);
-int nottake=0+memo(idx-1,nums,dp,
-target);
-return dp[idx][target]=
-Math.min(take,nottake);
-}
-public int tabulation(int[] nums,int target){
-int n=nums.length;
-//int[][] dp=new int[n][target+1];
-int[] prev=new int[target+1];
-int[] curr=new int[target+1];
-for(int tar=0;tar<=target;tar++){
-if(tar%nums[0]==0){
-//dp[0][tar]=tar/nums[0];
-prev[tar]=tar/nums[0];
-}
-else {
-//dp[0][tar]=(int)1e9;
-prev[tar]=(int)1e9;
-}
-}
-for(int i=0;i<n;i++){
-//dp[i][0]=0;
-prev[0]=0;
-}
-for(int i=1;i<n;i++){
-for(int tar=0;tar<=target;tar++){
-int take=(int)1e9;
-if(nums[i]<=tar){
-//take=1+dp[i][tar-nums[i]];
-take=1+curr[tar-nums[i]];
-}
-//int nottake=0+dp[i-1][tar];
-int nottake=0+prev[tar];
-//dp[i][tar]=Math.min(take,nottake);
-curr[tar]=Math.min(take,nottake);
-}
-prev=curr.clone();
-}
-//return dp[n-1][target];
-return prev[target];
-}
-    public int coinChange(int[] coins, int amount) {
+if(idx==0){
+if(target%nums[0]==0){
+return target/nums[0];}
+else return (int)1e9;}
+if(dp[idx][target]!=-1)
+return dp[idx][target];
+int take=(int) 1e9;
+if(nums[idx]<=target)
+take=1+coinChange2(idx,target-nums[idx],nums,dp);
+int nottake=0+coinChange2(idx-1,target,nums,dp);
+return dp[idx][target]=Math.min(take,nottake);}
+    public int coinChange(int[] coins, int target) {
        int n=coins.length;
-       int[][] dp=new int[n][amount+1];
-       for(int[] a:dp) Arrays.fill(a,-1);
-//int ans= memo(n-1,coins,dp,amount);
-int ans=tabulation(coins,amount);
-return ans==(int)1e9 ? -1 :ans;
-
+int[][] dp=new int[n][target+1];
+for(int[] a:dp){
+Arrays.fill(a,-1);}
+ int ans=coinChange2(n-1,target,coins,dp);
+return ans==(int)1e9 ? -1 : ans;
     }
 }
