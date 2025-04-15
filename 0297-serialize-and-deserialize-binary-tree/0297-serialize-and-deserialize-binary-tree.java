6b8efd1a626b8cef0bf4409846b8a3ff454a2cc9
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-15 23:27:20 +0530

    Time: 14 ms (67.52%), Space: 45.6 MB (81.53%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -11,43 +11,46 @@ public class Codec {
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-     if(root==null) return "";
+        if(root==null) return " ";
+StringBuilder res=new StringBuilder();
 Queue<TreeNode> q=new LinkedList<>();
 q.add(root);
-StringBuilder res=new StringBuilder();
 while(!q.isEmpty()) {
 TreeNode curr=q.poll();
 if(curr==null) {
 res.append("n ");
 }
 else {
-res.append(curr.val+" ");
+res.append(curr.val).append(" ");
 q.add(curr.left);
 q.add(curr.right);
 }
 }
-return res.toString().trim() ;  
+return res.toString().trim();
     }
+
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-    if(data.length()==0) return null;
+
+ if(data.length()==0) return null;
      String[] values=data.split(" ");
-     TreeNode root=new TreeNode(Integer.parseInt(values[0])) ;
-     Queue<TreeNode> q=new LinkedList<>();
-     q.offer(root);
-     int n=values.length;
-     for(int i=1;i<n-1;i++){
-    TreeNode curr=q.poll();
-    if(!values[i].equals("n")){
-    curr.left=new TreeNode(Integer.parseInt(values[i]));
-    q.add(curr.left);
-    }
-    if( ++i < n && !values[i].equals("n")){
-        curr.right=new TreeNode(Integer.parseInt(values[i]));
-        q.add(curr.right);
-    }
-     }
-     return root;
+int n=values.length;
+if(n==0) return null;
+TreeNode root=new TreeNode(Integer.parseInt(values[0])) ;
+Queue<TreeNode> q=new LinkedList<>();
+q.add(root);
+for(int i=1;i<n;i++){
+TreeNode curr=q.poll();
+if(!values[i].equals("n")) {
+curr.left=new TreeNode(Integer.parseInt(values[i]));
+q.add(curr.left);
+}
+if(++i<n && !values[i].equals("n")) {
+curr.right=new TreeNode(Integer.parseInt(values[i]));
+q.add(curr.right);
+}
+}
+return root;
     }
 }
 
