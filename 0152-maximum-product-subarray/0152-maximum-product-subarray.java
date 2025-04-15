680398b97534f3369051c4df71c34f4fbceefcc1
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-16 00:33:58 +0530

    Time: 1 ms (89.94%), Space: 45.3 MB (13.75%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,15 +1,18 @@
 class Solution {
     public int maxProduct(int[] nums) {
       //brute force generating all the subarray product
-      int max=Integer.MIN_VALUE;
-      int n=nums.length;
-      for(int i=0;i<n;i++) {
-    int ans=1;
-    for(int j=i;j<n;j++){
-    ans*=nums[j];
-    max=Math.max(ans,max);
-    }
-      } 
-      return max;
+//since we have to find the maximum pproduct we can find out leftmax product and rightmax
+int n=nums.length;
+int leftProduct=1;
+int rightProduct=1;
+int maxProduct=Integer.MIN_VALUE;
+for(int i=0;i<n;i++){
+leftProduct=leftProduct==0 ? 1 :leftProduct;
+rightProduct=rightProduct==0 ? 1: rightProduct;
+leftProduct*=nums[i];
+rightProduct*=nums[n-1-i];
+maxProduct=Math.max(maxProduct,Math.max(rightProduct,leftProduct));
+}
+return maxProduct;
     }
 }
