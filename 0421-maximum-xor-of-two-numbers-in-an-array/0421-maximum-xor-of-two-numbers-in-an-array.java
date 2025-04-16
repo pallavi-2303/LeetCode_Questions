2aae6a25a20baf70c8520ac9bd045e279a7dfc91
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-16 17:53:33 +0530

    Time: 870 ms (32.49%), Space: 180.5 MB (53.93%) - LeetHub

--- 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.java
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.java
@@ -1,53 +1,71 @@
 class Solution {
-    class Node {
-Node[] links=new Node[2];
-public boolean containsKey(int idx) {
- return links[idx]!=null;
+//first we will initilasise the trie data structure
+class Node{
+    Node[] links;
+    boolean end;
+    public Node(){
+        links=new Node[2];
+        end=false;
+    }
+    public boolean contains(int idx){
+   return links[idx]!=null;
+    }
+    public Node get(int idx){
+        return links[idx];
+    }
+    public void put(int idx,Node node){
+    links[idx]=node;
+    }
+    public void setEnd(){
+        end=true;
+    }
+
 }
-public void put(int idx,Node node){
-links[idx]=node;
+class Trie{
+private Node root;
+public Trie(){
+    root=new Node();
 }
-public Node get(int idx) {
-return links[idx];
-}
-}
-    public class Trie{
- Node root;
- Trie() {
- root=new Node();
- }
- public void insert(int num) {
- Node node=root;
- for(int i=31;i>=0;i--) {
-//ith bit of number
-int bit=(num>>i)&1;
-if(!node.containsKey(bit)) {
- node.put(bit,new Node());
-}
-node=node.get(bit);}}
-public int findmax(int num) {
+public void insert(int num){
 Node node=root;
-int maxi=0;
-for(int i=31;i>=0;i--) {
-int bit=(num>>i)&1;
-if(node.containsKey(1-bit)) {
-//set maxi to 1
-maxi=(maxi|(1<<i));
-node=node.get(1-bit);}
+//we will start to insert from the 31st bit to increase the maximum as left to right
+for(int i=31;i>=0;i--){
+//finding the bit index if it is set it will go to 1 else 0
+int bit=(num>>i) & 1;
+if(!node.contains(bit)){
+node.put(bit,new Node());
+}
+//get the reference node
+node=node.get(bit);
+}
+}
+public int maxValue(int num){
+//we have to fing the maximum value we get from array with this number
+int max=0;
+Node node=root;
+for(int i=31;i>=0;i--){
+int bit=(num>>i) & 1;
+//if the opposite bit is present we can do xor with it to make it omne
+if(node.contains(1-bit)){
+//make maxi 1
+max=max | (1<<i);
+node=node.get(1-bit);
+}
 else {
- node=node.get(bit);} }
- return maxi;} 
-  }
+    //if no number exist with opposite digit
+node=node.get(bit);
+}
+}
+return max;
+}
+}
     public int findMaximumXOR(int[] nums) {
-
-Trie trie=new Trie();
-for(int it:nums) {
- trie.insert(it);}
-int maxi=0;
- for(int it:nums) {
- maxi=Math.max(maxi,trie.findmax(it));}
-    return maxi;
-
-        
+     Trie trie=new Trie();
+     for(int it:nums)  trie.insert(it);
+     int max=-1;
+     for(int it:nums) {
+        max=Math.max(max,trie.maxValue(it));
+     }
+     return max;
     }
 }
