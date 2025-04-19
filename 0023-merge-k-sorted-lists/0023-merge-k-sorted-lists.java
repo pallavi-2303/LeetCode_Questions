7427143a723c12555c76667f3094bfde4cbedf06
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-19 14:40:34 +0530

    Time: 4 ms (72.13%), Space: 44.3 MB (79.1%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,31 +9,24 @@
  * }
  */
 class Solution {
-    public ListNode merge(ListNode left,ListNode right){
-    ListNode dummy=new ListNode(0);
-    ListNode  temp=dummy;
-    while(left!=null && right!=null){
-    if(left.val<=right.val){
-    temp.next=left;
-    left=left.next;
-    }
-    else {
-    temp.next=right;
-    right=right.next;
-    }
-    temp=temp.next;
-    }
-if(left==null) temp.next=right;
-else temp.next=left;
-return dummy.next;
-    }
     public ListNode mergeKLists(ListNode[] lists) {
-     if(lists.length==0) return null;
-    ListNode head=lists[0];
-    int n=lists.length;
-    for(int i=1;i<n;i++) {
-    head=merge(head,lists[i]);
-    } 
-    return head; 
+     //using priority  queue as it will give the smallest  element on top
+   if(lists==null || lists.length==0) return null;
+   PriorityQueue<ListNode> pq=new PriorityQueue<>((a,b)->(Integer.compare(a.val,b.val)));
+ for(ListNode node:lists) {
+if(node!=null){
+pq.offer(node);
+ }}
+ ListNode dummy=new ListNode(0);
+ ListNode temp=dummy;
+ while(!pq.isEmpty()) {
+ListNode curr=pq.poll();
+if(curr.next!=null){
+pq.offer(curr.next);
+}
+temp.next=curr;
+temp=temp.next;
+ }
+return dummy.next;  
     }
 }
