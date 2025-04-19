b79d938bf548e2db7806bbdf490ed09630a3b6e6
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-19 14:13:47 +0530

    Time: 25 ms (66.33%), Space: 45.4 MB (67.59%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -14,9 +14,22 @@ return dp[i][j]=Math.max(findInsertions(i-1,j,s,t,dp),findInsertions(i,j-1,s,t,d
      //since we have to find minimum number of insertions we need to find out the longest pallindromic pallindrme so than we need to do minmum insertion
 int n=s.length();
 int[][] dp=new int[n+1][n+1];
-for(int[] a:dp) Arrays.fill(a,-1);
+//for(int[] a:dp) Arrays.fill(a,-1);
 StringBuilder str=new StringBuilder(s);
-return n-findInsertions(n,n,s,str.reverse().toString(),dp);
+//return n-findInsertions(n,n,s,str.reverse().toString(),dp);
+String t=str.reverse().toString();
+//if any of the index go out of bound
+for(int i=1;i<=n;i++){
+for(int j=1;j<=n;j++){
+if(s.charAt(i-1)==t.charAt(j-1)){
+dp[i][j]=1+dp[i-1][j-1];
+}
+else {
+dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
+}
+}
+}
+return n-dp[n][n];
    
     }
 }
