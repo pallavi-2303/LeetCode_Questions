85e8ed5b23605483f631faffbf223ae5a6a717d1
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-19 14:11:26 +0530

    Time: 34 ms (18.22%), Space: 45.8 MB (26.07%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,40 +1,22 @@
 class Solution {
-    public int findSub(int idx1,int idx2,String s,String t,int[][] dp) {
- if(idx1==0 || idx2==0)
- return 0;
- if(dp[idx1][idx2]!=-1) return dp[idx1][idx2];
- //if character matches
- if(s.charAt(idx1-1)==t.charAt(idx2-1))
- return 1+findSub(idx1-1,idx2-1,s,t,dp);
- else return Math.max(findSub(idx1-1,idx2,s,t,dp),findSub(idx1,idx2-1,s,t,dp));}
+public int findInsertions(int i,int j,String s,String t,int[][] dp){
+if(i==0 || j==0) return 0;
+if(dp[i][j]!=-1) return dp[i][j];
+//if the string matches
+if(s.charAt(i-1)==t.charAt(j-1)){
+return dp[i][j]=1+findInsertions(i-1,j-1,s,t,dp);
+}
+else {
+return dp[i][j]=Math.max(findInsertions(i-1,j,s,t,dp),findInsertions(i,j-1,s,t,dp));
+}
+}
     public int minInsertions(String s) {
-       int n=s.length();
-String t=new StringBuilder(s).reverse().toString();
+     //since we have to find minimum number of insertions we need to find out the longest pallindromic pallindrme so than we need to do minmum insertion
+int n=s.length();
 int[][] dp=new int[n+1][n+1];
-//we will keep the longest palindromic portion enact 
-//for(int[] a:dp)
-// Arrays.fill(a,-1);
-// return n-findSub(n,n,s,t,dp); 
-int[] curr=new int[n+1];
-int[] prev=new int[n+1];
-for(int i=0;i<=n;i++){
-dp[0][i]=0;
-dp[i][0]=0;
-prev[i]=0;
-prev[0]=0;
-}
-for(int i=1;i<=n;i++){
-for(int j=1;j<=n;j++){
-if(s.charAt(i-1)==t.charAt(j-1))
-dp[i][j]=1+dp[i-1][j-1];
-//curr[j]=1+prev[j-1];
-else
-dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
-//curr[j]=Math.max(curr[j-1],prev[j]);
-}
-//prev=curr.clone();
-}
-return n-dp[n][n];
-//return n-prev[n];
+for(int[] a:dp) Arrays.fill(a,-1);
+StringBuilder str=new StringBuilder(s);
+return n-findInsertions(n,n,s,str.reverse().toString(),dp);
+   
     }
 }
