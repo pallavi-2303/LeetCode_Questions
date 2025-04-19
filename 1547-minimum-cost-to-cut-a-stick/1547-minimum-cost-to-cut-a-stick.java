cf8e2d74439943752ee7c9612952530f2c067258
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-19 15:59:17 +0530

    Time: 38 ms (12.8%), Space: 42.5 MB (59.74%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -1,27 +1,32 @@
 class Solution {
-    public int findCost(int i,int j,List<Integer> arr,int[][] dp) {
- //base case
- if(i>j) return 0;
- if(dp[i][j]!=-1) return dp[i][j];
- int mini=(int)(1e9);
- //try to cut at index starting from i to j
- for(int idx=i;idx<=j;idx++){
- int totalCost=arr.get(j+1)-arr.get(i-1)+findCost(i,idx-1,arr,dp)+findCost(idx+1,j,arr,dp);
- mini=Math.min(mini,totalCost);
- }
- return dp[i][j]=mini;
+    public int findMinimum(int i,int j,List<Integer> cost,int[][] dp){
+//if there is nothing to solve 
+if(i>j) return 0;
+if(dp[i][j]!=-1) {
+return dp[i][j];
+}
+//we can cut at any index from i to j
+int mini=(int)1e9;
+for(int idx=i;idx<=j;idx++){
+//we will remember where the last cut was made as this will give the length of stick
+int cost2=cost.get(j+1)-cost.get(i-1)+findMinimum(i,idx-1,cost,dp)+findMinimum(idx+1,j,cost,dp);
+mini=Math.min(cost2, mini);
+}
+return dp[i][j]=mini;
 }
     public int minCost(int n, int[] cuts) {
-        List<Integer> temp=new ArrayList<>();
+      List<Integer> arr=new ArrayList<>();
+arr.add(0);
 int c=cuts.length;
-temp.add(0);
-for(int i=0;i<c;i++){
-temp.add(cuts[i]);
+for(int i=0;i<cuts.length;i++){
+arr.add(cuts[i]);
 }
-temp.add(n);
-Collections.sort(temp);
+arr.add(n);
+Collections.sort(arr);
 int[][] dp=new int[c+1][c+1];
-for(int[] a:dp) Arrays.fill(a,-1);
-return findCost(1,c,temp,dp);
+for(int[] a:dp) {
+Arrays.fill(a,-1);
+}
+return findMinimum(1,c,arr,dp);  
     }
 }
