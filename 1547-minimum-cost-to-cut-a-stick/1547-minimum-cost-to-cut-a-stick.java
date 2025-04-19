dc655bd8bd64a242b5be0af3e1d4916e1e7bce74
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-19 16:01:43 +0530

    Time: 19 ms (74.34%), Space: 42.4 MB (84.96%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -1,32 +1,40 @@
 class Solution {
-    public int findMinimum(int i,int j,List<Integer> cost,int[][] dp){
-//if there is nothing to solve 
-if(i>j) return 0;
-if(dp[i][j]!=-1) {
-return dp[i][j];
-}
-//we can cut at any index from i to j
-int mini=(int)1e9;
-for(int idx=i;idx<=j;idx++){
-//we will remember where the last cut was made as this will give the length of stick
-int cost2=cost.get(j+1)-cost.get(i-1)+findMinimum(i,idx-1,cost,dp)+findMinimum(idx+1,j,cost,dp);
-mini=Math.min(cost2, mini);
-}
-return dp[i][j]=mini;
+    public int findCost(int i,int j,List<Integer> arr,int[][] dp) {
+ //base case
+ if(i>j) return 0;
+ if(dp[i][j]!=-1) return dp[i][j];
+ int mini=(int)(1e9);
+ //try to cut at index starting from i to j
+ for(int idx=i;idx<=j;idx++){
+ int totalCost=arr.get(j+1)-arr.get(i-1)+findCost(i,idx-1,arr,dp)+findCost(idx+1,j,arr,dp);
+ mini=Math.min(mini,totalCost);
+ }
+ return dp[i][j]=mini;
 }
     public int minCost(int n, int[] cuts) {
-      List<Integer> arr=new ArrayList<>();
-arr.add(0);
+        List<Integer> arr=new ArrayList<>();
 int c=cuts.length;
-for(int i=0;i<cuts.length;i++){
+arr.add(0);
+for(int i=0;i<c;i++){
 arr.add(cuts[i]);
 }
 arr.add(n);
 Collections.sort(arr);
-int[][] dp=new int[c+1][c+1];
-for(int[] a:dp) {
-Arrays.fill(a,-1);
+// int[][] dp=new int[c+1][c+1];
+// for(int[] a:dp) Arrays.fill(a,-1);
+// return findCost(1,c,temp,dp);
+int[][] dp=new int[c+2][c+2];
+for(int i=c;i>=1;i--){
+for(int j=1;j<=c;j++){
+    if(i>j) continue;
+int mini=(int)(1e9);
+for(int idx=i;idx<=j;idx++){
+int cost=arr.get(j+1)-arr.get(i-1)+dp[i][idx-1]+dp[idx+1][j];
+mini=Math.min(mini,cost);
 }
-return findMinimum(1,c,arr,dp);  
+dp[i][j]=mini;
+}
+}
+return dp[1][c];
     }
 }
