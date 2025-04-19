aa5fd554c027ed1b3379a820892346640cbcf59b
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-19 14:07:51 +0530

    Time: 26 ms (58.06%), Space: 45.1 MB (90.36%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -18,23 +18,23 @@ int[][] dp=new int[n+1][n+1];
 int[] curr=new int[n+1];
 int[] prev=new int[n+1];
 for(int i=0;i<=n;i++){
-//dp[0][i]=0;
-//dp[i][0]=0;
+dp[0][i]=0;
+dp[i][0]=0;
 prev[i]=0;
 prev[0]=0;
 }
 for(int i=1;i<=n;i++){
 for(int j=1;j<=n;j++){
 if(s.charAt(i-1)==t.charAt(j-1))
-//dp[i][j]=1+dp[i-1][j-1];
-curr[j]=1+prev[j-1];
+dp[i][j]=1+dp[i-1][j-1];
+//curr[j]=1+prev[j-1];
 else
-//dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
-curr[j]=Math.max(curr[j-1],prev[j]);
+dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+//curr[j]=Math.max(curr[j-1],prev[j]);
 }
-prev=curr.clone();
+//prev=curr.clone();
 }
-//return dp[n][n];
-return n-prev[n];
+return n-dp[n][n];
+//return n-prev[n];
     }
 }
