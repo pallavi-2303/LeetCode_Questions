a540fda3af14749983eb3e7e59e0be1460bff195
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-20 20:16:30 +0530

    Time: 6 ms (86.13%), Space: 44.9 MB (23.54%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -3,23 +3,24 @@ class Solution {
     int n=s.length();
     String ans="";
     int i=n-1;
-    while(i>=0) {
-    //for the white spaces
-    while(i>=0 && s.charAt(i)==' '){
-    i--;
-    }
-    if(i<0) break;
-    int j=i;
-    while(i>=0 && s.charAt(i)!=' '){
-        i--;
-    }
-    if(ans.length()==0){
-        ans=ans.concat(s.substring(i+1,j+1));
-    }
-    else {
-    ans=ans.concat(' '+s.substring(i+1,j+1));
-    }
-    } 
-    return ans;
+    int j=-1;
+  while(i>=0) {
+while(i>=0 && s.charAt(i)==' '){
+i--;
+}
+if(i<0) break;
+if(i>=0) j=i;
+while(i>=0 && s.charAt(i)!=' '){
+i--;
+}
+//i will to empty space and then  it stops
+if(ans.length()==0){
+ans=ans.concat(s.substring(i+1,j+1));
+}
+else {
+ans=ans.concat(" "+s.substring(i+1,j+1));
+}
+  } 
+return ans;
     }
 }
