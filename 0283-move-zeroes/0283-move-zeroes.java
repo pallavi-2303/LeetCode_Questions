196f1f11a93da681bf77e2a086f6a77b228100da
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-21 18:33:28 +0530

    Time: 2 ms (84.54%), Space: 46.3 MB (19.74%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,53 +1,19 @@
 class Solution {
-    public void swap(int[] nums,int i,int j){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
-    }
     public void moveZeroes(int[] nums) {
-     int n=nums.length;
-     //brute force putting non zero number 
-//      int idx=0;
-//     int[] ans=new int[n];
-//      for(int i=0;i<n;i++)  {
-//     if(nums[i]!=0){
-//     ans[idx++]=nums[i];
-//     }
-//      }
-// for(int i=0;i<n;i++){
-// nums[i]=ans[i];
-// }
-// while(idx<n){
-//     nums[idx++]=0;
-// }
-//boptimal aproch of finding the first index where zero occur
-int j=-1;
-for(int i=0;i<n;i++){
-    if(nums[i]==0){
-        j=i;
-        break;
-    }
-}
-if(j==-1) return;
-for(int i=j+1;i<n;i++){
+       int n=nums.length;
+int[] ans=new int[n];
+int idx=0;
+for(int i=0;i<n;i++) {
 if(nums[i]!=0){
-    swap(nums,i,j);
-    j++;
+ans[idx]=nums[i];
+idx++;
 }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+while(idx<n){
+ans[idx++]=0;
+}
+for(int i=0;i<n;i++){
+nums[i]=ans[i];
+} 
     }
 }
