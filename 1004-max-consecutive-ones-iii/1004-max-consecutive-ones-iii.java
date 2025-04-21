cfbfee6eb2ad6b690f4240058fb33edd35e3990f
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-21 23:01:26 +0530

    Time: 3 ms (91.04%), Space: 46.2 MB (96.67%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,38 +1,19 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-      int n=nums.length;
-      int maxlen=0;
-      //brute force genearting all the subarray
-   /* for(int i=0;i<n;i++) {
-        int zeroes=0;
-        for(int j=i;j<n;j++){
-        if(nums[j]==0) zeroes++;
-        //finding the length if zeroes is less than or equal to k
-        if(zeroes<=k){
-        maxlen=Math.max(maxlen,j-i+1);
-        }
-        else {
-            break;
-        }
-        }
-    } */
-//optimal aproch of using a sliding window maximum
+       int n=nums.length;
+int maxi=0;
 int l=0;
 int r=0;
 int zeroes=0;
 while(r<n){
-if(nums[r]==0) zeroes++;
+if(nums[r]==0){
+zeroes++;}
 while(zeroes>k){
 if(nums[l]==0){
-    zeroes--;
-}
-//increase l pointer in both casees
-l++;
-}
-maxlen=Math.max(maxlen,r-l+1);
-r++;
-}
-    return maxlen;
+zeroes--;}
+l++;}
+maxi=Math.max(maxi,r-l+1);
+r++;}
+return maxi; 
     }
-
 }
