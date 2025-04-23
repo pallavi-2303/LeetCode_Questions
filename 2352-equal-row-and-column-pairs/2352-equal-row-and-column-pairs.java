7002b08d1af7f627297f9153055ed54b6edcb40c
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-23 20:49:49 +0530

    Time: 89 ms (14.18%), Space: 55.7 MB (5.33%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -1,21 +1,25 @@
 class Solution {
     public int equalPairs(int[][] grid) {
-       //brute force
-int n=grid.length;
-//let us check for each row and each column which are similar 
-int count=0;
+     //oprimal storing all rows in hashmap and then iterating through all columns and finding how many similar rows are present this will give equivalent pair count 
+     int n=grid.length;
+HashMap<String,Integer> mp=new HashMap<>();
 for(int i=0;i<n;i++){
+String temp="";
 for(int j=0;j<n;j++){
-boolean flag=true;
-for(int k=0;k<n;k++){
-if(grid[i][k]!=grid[k][j]) {
-flag=false;
-break;
+temp+=grid[i][j]+"+";
+}
+mp.put(temp,mp.getOrDefault(temp,0)+1);
+}
+int count=0;
+for(int j=0;j<n;j++){
+String temp="";
+for(int i=0;i<n;i++){
+ temp+=grid[i][j]+"+";
+}
+if(mp.containsKey(temp)) {
+count+=mp.get(temp);
 }
 }
-if(flag) count++;
-}
-}
-return count; 
+return count;   
     }
 }
