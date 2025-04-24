724b3b398ec883445602d5ada7b73c2c67dc2a67
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-24 23:08:32 +0530

    Time: 2 ms (68.78%), Space: 42.4 MB (75.72%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,59 +1,52 @@
 class Solution {
     public class tuple{
-        int row;
-        int col;
-        int tm;
-        public tuple(int row,int col,int tm){
-            this.row=row;
-            this.col=col;
-            this.tm=tm;
-        }
-    }
+int row;
+int col;
+int time;
+public tuple(int row,int col,int time){
+this.row=row;
+this.col=col;
+this.time=time;
+}
+}
     public int orangesRotting(int[][] grid) {
-    //since we have to move four dirctionaly we will use bfs as it one unit of time it will rot its adjacent neighbour 
-  int n=grid.length;
-  int m=grid[0].length;  
-  int[][] vis=new int[n][m] ;
-  Queue<tuple> q=new LinkedList<>(); 
-  //insert the values that are rotten 
-  int countfresh=0;
-  for(int i=0;i<n;i++){
+        int n=grid.length;
+int m=grid[0].length;
+int[][] vis=new int[n][m];
+Queue<tuple> q=new LinkedList<>();
+int countfresh=0;
+for(int i=0;i<n;i++){
 for(int j=0;j<m;j++){
 if(grid[i][j]==2){
-//means it is rotten 
-vis[i][j]=1;
 q.add(new tuple(i,j,0));
+vis[i][j]=1;
 }
-else {
-vis[i][j]=0;
+else if(grid[i][j]==1) {
+countfresh++;
 }
-if(grid[i][j]==1) countfresh++;
 }
-  }
-  //performing the bfs traversal and finding out the time to rot all the oranges
-  int time=0;
-  int cnt=0;
-  int[] delrow={-1,1,0,0};
-  int[] delcol={0,0,-1,1};
-  while(!q.isEmpty()){
+}
+int[] delrow={-1,1,0,0};
+int[] delcol={0,0,-1,1};
+int maxtime=0;
+int count=0;
+while(!q.isEmpty()){
 tuple it=q.poll();
 int row=it.row;
 int col=it.col;
-int tm=it.tm;
-time=Math.max(time,tm);
-//traversing in all the four direction
+int t=it.time;
+maxtime=Math.max(maxtime,t);
+//visiting all the nodes that are fresh
 for(int i=0;i<4;i++){
 int nrow=row+delrow[i];
 int ncol=col+delcol[i];
-//conditional check
-if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && vis[nrow][ncol]==0){
-//it is fresh orange and it can be rotten
-cnt++;
+if( nrow>=0 && ncol>=0 && nrow<n && ncol<m && grid[nrow][ncol]==1 && vis[nrow][ncol]==0){
+count++;
 vis[nrow][ncol]=1;
-q.add(new tuple(nrow,ncol,tm+1));
+q.add(new tuple(nrow,ncol,t+1));
 }
 }
-  }
-  return cnt>= countfresh ?  time : -1;
+}
+return count>=countfresh ? maxtime : -1;
     }
 }
