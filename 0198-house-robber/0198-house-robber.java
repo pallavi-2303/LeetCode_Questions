70b0689676de022d3691ca4d11560b524a4dc640
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-27 20:35:25 +0530

    Time: 0 ms (100%), Space: 40.8 MB (88.51%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,16 +1,25 @@
 class Solution {
-    public int findProfit(int idx,int[] nums,int[] dp){
-    if(idx<0) return 0;
-    if(idx==0) return nums[0];
-    if(dp[idx]!=-1) return dp[idx];
-int take=nums[idx]+findProfit(idx-2,nums,dp);
-int nottake=0+findProfit(idx-1,nums,dp);
-return dp[idx]=Math.max(take,nottake);
-    }
+//     public int findProfit(int idx,int[] nums,int[] dp){
+//     if(idx<0) return 0;
+//     if(idx==0) return nums[0];
+//     if(dp[idx]!=-1) return dp[idx];
+// int take=nums[idx]+findProfit(idx-2,nums,dp);
+// int nottake=0+findProfit(idx-1,nums,dp);
+// return dp[idx]=Math.max(take,nottake);
+//     }
     public int rob(int[] nums) {
     int n=nums.length;
     int[] dp=new int[n];
-    Arrays.fill(dp,-1);
-    return findProfit(n-1,nums,dp) ;  
+    // Arrays.fill(dp,-1);
+    // return findProfit(n-1,nums,dp) ;  
+    dp[0]=nums[0];
+    for(int i=1;i<n;i++){
+     int take=nums[i];
+    if(i-2>=0)
+  take+=dp[i-2];
+    int nottake=dp[i-1];
+    dp[i]=Math.max(take,nottake);
+    }
+    return dp[n-1];
     }
 }
