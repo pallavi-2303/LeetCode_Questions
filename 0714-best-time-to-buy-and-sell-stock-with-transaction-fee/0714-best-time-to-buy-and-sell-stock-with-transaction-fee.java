a4a01e1b147f73339751558fa6e81a2f40a7da54
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-27 17:39:26 +0530

    Time: 29 ms (30.33%), Space: 62.7 MB (21.7%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,39 +1,22 @@
 class Solution {
-    public int findMax(int idx,int buy,int[] arr,int fee,int[][] dp){
-    if(idx==arr.length) return 0;
+    public int findProfit(int idx,int[] prices,int buy,int[][] dp,int fee){
+    if(idx==prices.length) return 0;
     if(dp[idx][buy]!=-1) return dp[idx][buy];
     int profit=0;
+    //we have two options we can buy or not buy
     if(buy==1){
-        profit=Math.max(-arr[idx]+findMax(idx+1,0,arr,fee,dp),findMax(idx+1,1,arr,fee,dp));
+    profit=Math.max(-prices[idx]+findProfit(idx+1,prices,0,dp,fee),findProfit(idx+1,prices,1,dp,fee));
     }
     else {
-         profit=Math.max(arr[idx]-fee+findMax(idx+1,1,arr,fee,dp),findMax(idx+1,0,arr,fee,dp));
+    profit=Math.max(prices[idx]-fee+findProfit(idx+1,prices,1,dp,fee),findProfit(idx+1,prices,0,dp,fee));
     }
     return dp[idx][buy]=profit;
     }
     public int maxProfit(int[] prices, int fee) {
-     int n=prices.length;
-     int[][] dp=new int[n+1][2];
-    //  for(int[] a:dp) Arrays.fill(a,-1) ;
-    //  return findMax(0,1,prices,fee,dp);
-    int[] prev=new int[2];
-    int[] curr=new int[2];
-    for(int i=n-1;i>=0;i--){
-        for(int buy=0;buy<=1;buy++){
-          int profit=0;
-    if(buy==1){
-       // profit=Math.max(-prices[i]+dp[i+1][0],dp[i+1][1]);
-       profit=Math.max(-prices[i]+prev[0],prev[1]);
-    }
-    else {
-       //  profit=Math.max(prices[i]-fee+dp[i+1][1],dp[i+1][0]);
-        profit=Math.max(prices[i]-fee+prev[1],prev[0]);
-    } 
-    curr[buy]=profit;  
-        }
-        prev=curr.clone();
-    }
-   // return dp[0][1];
-   return prev[1];
+    //since we have to find out the transaction 
+    int n=prices.length;
+    int[][] dp=new int[n][2];
+    for(int[] a:dp)  Arrays.fill(a,-1);
+    return findProfit(0,prices,1,dp,fee);
     }
 }
