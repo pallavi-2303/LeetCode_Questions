8d646c1d00bc03230789f07cb30c35937edc75f4
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-04-27 19:11:51 +0530

    Time: 3 ms (95.2%), Space: 45.3 MB (19.14%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,61 +1,48 @@
 class Solution {
-public int findMin(int idx1,int idx2,String s1,String s2,int[][] dp){
-//base if any one the character exhaust
-if(idx1==0) 
-//we can insert all characters of j into i 
-return idx2;
-if(idx2==0) //delete operation
-return idx1;
-if(dp[idx1][idx2]!=-1) return dp[idx1][idx2];
-//if string matches
-if(s1.charAt(idx1-1)==s2.charAt(idx2-1)){
-// no need to perform any operation just move to next string
-return 0+findMin(idx1-1,idx2-1,s1,s2,dp);
-}
-//if not match we have to perform the 3 operations
-//1 for the operation
-//if insert is perform->i,j-1
-//delete i-1,j
-//replace i-1,j-1
-
-return 1+Math.min(
-findMin(idx1,idx2-1,s1,s2,dp),Math.min(findMin(idx1-1,idx2,s1,s2,dp),
-findMin(idx1-1,idx2-1,s1,s2,dp)));
-}
-    public int minDistance(String s1, String s2) {
-      int n1=s1.length();
-int n2=s2.length();
-int[][] dp=new int[n1+1][n2+1];
-//for(int[] a:dp)
-// Arrays.fill(a,-1);
-// return findMin(n1,n2,s1,s2,dp); 
-int[] prev=new int[n2+1];
-int[] curr=new int[n2+1];
-
+    public int findLen(int idx1,int idx2,String s,String t,int[][] dp){
 //base case
-for(int j=0;j<=n2;j++){
-//dp[0][j]=j;//deletion
-prev[j]=j;
+if(idx1==0){
+//out of biubd case
+return idx2;//we have to perform insert opeartion idx2 number of times 
 }
-//for(int i=0;i<=n1;i++){
-//dp[i][0]=i;//insertion
-//prev[0]=i;
-//}
-for(int i=1;i<=n1;i++){
-curr[0]=i;
- for(int j=1;j<=n2;j++){
- if(s1.charAt(i-1)==s2.charAt(j-1)){
- //dp[i][j]=0+dp[i-1][j-1];
- curr[j]=0+prev[j-1];
- }
- else {
- //dp[i][j]=1+Math.min(dp[i][j-1],Math.min(dp[i-1][j],dp[i-1][j-1]));
-curr[j]=1+Math.min(curr[j-1],Math.min(prev[j],prev[j-1]));
- }
- }
- prev=curr.clone();
+if(idx2==0){
+return idx1;//delete all the characters 
 }
-//return dp[n1][n2];
-return prev[n2]; 
+if(dp[idx1][idx2]!=-1){
+return dp[idx1][idx2];
+}
+if(s.charAt(idx1-1)==t.charAt(idx2-1)){
+return dp[idx1][idx2]=0+findLen(idx1-1,idx2-1,s,t,dp);
+}
+else {
+//replace i-1 j-1 delete i-1 j insert i j-1 
+return dp[idx1][idx2]=1+Math.min(findLen(idx1-1,idx2-1,s,t,dp),Math.min(findLen(idx1-1,idx2,s,t,dp),findLen(idx1,idx2-1,s,t,dp)));
+}
+}
+    public int minDistance(String s, String t) {
+        int n=s.length();
+int m=t.length();
+int[][] dp=new int[n+1][m+1];
+for(int[] a:dp) 
+Arrays.fill(a,-1);
+return findLen(n,m,s,t,dp);
+//Tabulation base case
+// for(int i=0;i<=n;i++){
+// dp[i][0]=i;//insert
+// }
+// for(int j=0;j<=m;j++){
+// dp[0][j]=j;//delete 
+// }
+// for(int i=1;i<=n;i++){
+// for(int j=1;j<=m;j++){
+// if(s.charAt(i-1)==t.charAt(j-1)){
+// dp[i][j]=1+dp[i-1][j-1];
+// }
+// else {
+// dp[i][j]=1+Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]));
+// }
+// }
+// }
+// return dp[n][m];
     }
 }
