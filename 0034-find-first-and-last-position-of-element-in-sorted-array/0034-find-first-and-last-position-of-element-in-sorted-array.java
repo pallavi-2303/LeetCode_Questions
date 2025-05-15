325cdbb8aa651d52e410e6ac5abe02f6fbd03b86
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-05-15 16:03:12 +0530

    Time: 0 ms (100%), Space: 45.9 MB (83.56%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,16 +1,37 @@
 class Solution {
+    public int findLowerBound(int[] nums,int target){
+int low=0;
+int high=nums.length-1;
+int fo=-1;
+while(low<=high){
+int mid=(low+high)>>1;
+if(nums[mid]==target){
+fo=mid;
+high=mid-1;}
+else if(target>nums[mid]) {
+low=mid+1;}
+else{
+  high=mid-1;}  }
+return fo;}
+public int findUpperBound(int[] nums,int target){
+int low=0;
+int high=nums.length-1;
+int lo=-1;
+while(low<=high){
+int mid=(low+high)>>1;
+if(nums[mid]==target){
+lo=mid;
+low=mid+1;}
+else if(target<nums[mid]){
+high=mid-1;}
+else {
+low=mid+1;}}
+return lo;}
     public int[] searchRange(int[] nums, int target) {
-       //brute force
-int n=nums.length;
-int[] ans={-1,-1};
-for(int i=0;i<n;i++){
-if(nums[i]==target){
-ans[0]=i;
-break;}}
-for(int i=n-1;i>=0;i--){
-if(nums[i]==target){
-ans[1]=i;
-break;}}
-return ans; 
+      int n=nums.length;
+int fo=findLowerBound(nums,target);
+int lo=findUpperBound(nums,target);
+int[] ans={fo,lo};
+return ans;  
     }
 }
