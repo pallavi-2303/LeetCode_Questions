3665168ae99d1b22590c1eed5bb0ed6a74fca87a
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-05-17 23:37:16 +0530

    Time: 1 ms (98.4%), Space: 41.5 MB (75.58%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,27 +1,29 @@
 class Solution {
-    public boolean backTrack(int n,StringBuilder curr,int k,StringBuilder res,int[] count){
-  if(curr.length()==n){
+    public boolean backTrack(int n,StringBuilder res,StringBuilder curr,int[] count,int k) {
+//base case
+if(curr.length()==n) {
 count[0]++;
-if(count[0]==k){
+if(count[0]==k) {
 res.append(curr.toString());
-return true;//ths is the answer
-}
-return false;
-  }
-//since we want lexicographtical string
+return true;}
+else return false;
+}//we haven't found the answer
 for(char ch='a';ch<='c';ch++){
- int len=curr.length() ;
-if(len>0 && curr.charAt(len-1)==ch) continue;
+int len=curr.length();
+if(len>0 && curr.charAt(len-1)==ch){
+ continue;   
+}
+//append the current character 
 curr.append(ch);
-if(backTrack(n,curr,k,res,count)) return true;
+if(backTrack(n,res,curr,count,k)) 
+return true;
 curr.deleteCharAt(curr.length()-1);
 }
 return false;
-    }
+}
     public String getHappyString(int n, int k) {
-     StringBuilder res=new StringBuilder();
-     int[] count={0};
-     backTrack(n,new StringBuilder(),k,res,count) ;
-     return res.toString(); 
+        StringBuilder res=new StringBuilder();
+backTrack(n,res,new StringBuilder(),new int[]{0},k);
+return res.toString();
     }
 }
