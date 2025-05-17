0e51894c05e5c4f3d9642205a26c91a88470f2f0
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-05-17 23:03:31 +0530

    Time: 0 ms (100%), Space: 42.1 MB (30.03%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,33 +1,16 @@
 class Solution {
-    public void  swap(int[] nums,int i,int j){
-    int temp=nums[i];
-   nums[i]=nums[j];
-   nums[j]=temp;
+    public void swap(int[] nums,int x,int y){
+    int temp=nums[x];
+    nums[x]=nums[y];
+    nums[y]=temp;
     }
     public void sortColors(int[] nums) {
-    /*int count0=0;
-    int count1=0;
-    int count2=0;
- for(int num:nums) {
-if(num==0) count0++;
-else if(num==1) count1++;
-else count2++;
- }   
-for(int i=0;i<count0;i++){
-nums[i]=0;
-}
-for(int i=count0;i<count0+count1;i++){
-nums[i]=1;
-}
-for(int i=count0+count1;i<count0+count1+count2;i++){
-nums[i]=2;
-}*/
+     //since there are only 3 integers we can use 3 var to store count
+//in dutch algorihtm we know that 0 to low-1 ->0 low to mid-1-> 1 mid to high->unsoted element and high+1 to n-1 2 
 int n=nums.length;
-//by dutch  algorithm we know that 
-//0 to  low-1->0 low to mid-1->1 and mid to high -> unsorted element and high+1 to n-1->2
 int low=0;
-int mid=0;
 int high=n-1;
+int mid=0;
 while(mid<=high){
 if(nums[mid]==0){
 swap(nums,low,mid);
@@ -38,11 +21,10 @@ else if(nums[mid]==1){
 mid++;
 }
 else {
-//it is two
+//it is 2
 swap(nums,mid,high);
 high--;
 }
-}
-
+} 
     }
 }
