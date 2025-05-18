f49e5f333da93ea0297a7c9521054fb3efabe8c6
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-05-18 23:22:02 +0530

    Time: 6 ms (61.16%), Space: 44.5 MB (53.13%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,26 +1,29 @@
 class Solution {
-    public int findWord(int idx,String s,int[] dp, HashSet<String> st) {
-if(idx==s.length()) {
-//the string is exhausted and it is found
-return 1;
-}
-if(st.contains(s)) return 1;
-if(dp[idx]!=-1) return dp[idx];
-for(int len=1;len+idx<=s.length();len++){
-if(st.contains(s.substring(idx,idx+len)) && (findWord(idx+len,s,dp,st))==1) {
-return dp[idx]=1;
-}
-}
-return dp[idx]=0;//if the loop break and answer not found 
-}
+        int[] dp=new int[301];
+    public int find(int idx,String s,HashSet<String> st) {
+    int n=s.length();
+    if(idx>=n) return 1;
+    if(dp[idx]!=-1) return dp[idx];
+    if(st.contains(s.substring(idx,n))) {
+    return 1;
+    }
+    //at perticular index we can try to split it in diffrentvsubstring and find wheather it us present in that or not
+    for(int j=idx;j<n;j++){
+    String str=s.substring(idx,j+1);
+    //check whether it is possible to break or not
+    if(st.contains(str) && (find(j+1,s,st))==1){
+    return dp[idx]=1;
+    }
+    }
+
+    return dp[idx]=0;
+    }
     public boolean wordBreak(String s, List<String> wordDict) {
-    
-HashSet<String> st=new HashSet<>();
-for(String str:wordDict){
-st.add(str);
-}
-int[] dp=new int[s.length()];
-Arrays.fill(dp,-1);
-return findWord(0,s,dp,st)==1 ? true : false;   
+
+        HashSet<String> st = new HashSet<>(wordDict);
+        //adding all wordlist to st
+        Arrays.fill(dp, -1);
+        //return find(0, s, st);
+        return find(0,s,st)==1 ? true : false;
     }
 }
