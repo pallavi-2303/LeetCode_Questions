592557b4ecc6218a53495c6e4c37467fc1caed2a
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-05-19 22:48:50 +0530

    Time: 17 ms (86.18%), Space: 94.6 MB (58.05%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,20 +1,23 @@
 class Solution {
-    public void combination(int start,int n,int k,List<Integer> temp,List<List<Integer>> res){
-    if(temp.size()==k){
-    res.add(new ArrayList<>(temp));
-    return;
-    }
-if(start> n) return ;
-//choose the current element
-temp.add(start);
-combination(start+1,n,k,temp,res);
+    //leetcode 77
+public void find(int idx,List<Integer> temp,int n,int k,List<List<Integer>> res){
+if(temp.size()==k){
+res.add(new ArrayList<>(temp));
+return;
+}
+//TC-> 2^n
+//Sc->O(n) 
+for(int i=idx;i<=n;i++){
+//Take case
+temp.add(i);
+find(i+1,temp,n,k,res);
+//Backtrack
 temp.remove(temp.size()-1);
-combination(start+1,n,k,temp,res);
-    }
+}
+}
     public List<List<Integer>> combine(int n, int k) {
-List<List<Integer>> res=new ArrayList<>();
-combination(1,n,k,new ArrayList<>(),res);
-return res;
-        
+       List<List<Integer>> res=new ArrayList<>();
+find(1,new ArrayList<>(),n,k,res);
+return res; 
     }
 }
