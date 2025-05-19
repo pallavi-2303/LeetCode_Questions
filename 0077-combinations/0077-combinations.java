c61e50a2ce6cc9d12befba7da2653a7868c18e89
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-05-19 22:51:31 +0530

    Time: 18 ms (78.07%), Space: 94.6 MB (58.05%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,23 +1,34 @@
 class Solution {
-    //leetcode 77
-public void find(int idx,List<Integer> temp,int n,int k,List<List<Integer>> res){
+//     public void combination(int start,int n,int k,List<Integer> temp,List<List<Integer>> res){
+//     if(temp.size()==k){
+//     res.add(new ArrayList<>(temp));
+//     return;
+//     }
+// if(start> n) return ;
+// //choose the current element
+// temp.add(start);
+// combination(start+1,n,k,temp,res);
+// temp.remove(temp.size()-1);
+// combination(start+1,n,k,temp,res);
+//     }
+ public void combination2(int start,int n,int k,List<Integer> temp,List<List<Integer>> res){
+//base case
 if(temp.size()==k){
 res.add(new ArrayList<>(temp));
 return;
 }
-//TC-> 2^n
-//Sc->O(n) 
-for(int i=idx;i<=n;i++){
-//Take case
+if(start> n) return ;
+for(int i=start;i<=n;i++){
 temp.add(i);
-find(i+1,temp,n,k,res);
-//Backtrack
+combination2(i+1,n,k,temp,res);
 temp.remove(temp.size()-1);
 }
-}
+ }
     public List<List<Integer>> combine(int n, int k) {
-       List<List<Integer>> res=new ArrayList<>();
-find(1,new ArrayList<>(),n,k,res);
-return res; 
+List<List<Integer>> res=new ArrayList<>();
+//combination(1,n,k,new ArrayList<>(),res);
+combination2(1,n,k,new ArrayList<>(),res);
+return res;
+        
     }
 }
