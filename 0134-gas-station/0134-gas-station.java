41c15c244d4f4de8b3c07ac57406451e0f87ef19
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-06-12 17:41:33 +0530

    Time: 2 ms (97.33%), Space: 56.6 MB (22.25%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,19 +1,25 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-     //if gas-cost>0 then only we can travel
-     int n=gas.length;
-     int diff=0;
-     int res=0;
-     int ans=0;
-     for(int i=0;i<n;i++){
-    diff+=(gas[i]-cost[i]);
-    res+=(gas[i]-cost[i]);
-    if(res<0){
-    res=0;
-    ans=i+1;
-    }
-     }
-if(diff<0 || ans>=n) return -1;
+         int n=gas.length;
+         int totalCost=0;
+         int totalGas=0;
+         for(int i=0;i<n;i++){
+        totalCost+=cost[i];
+        totalGas+=gas[i];
+         }
+
+//not possible case
+if(totalGas<totalCost) return -1;
+int total=0;
+int ans=0;
+for(int i=0;i<n;i++){
+total+=gas[i]-cost[i];
+if(total<0){
+total=0;
+ans=i+1;//next index woulbe answer and it is guranned that one naswer exist and the nymber that come first whose total is grater than 0 will go to end
+}
+}
 return ans;
+
     }
 }
