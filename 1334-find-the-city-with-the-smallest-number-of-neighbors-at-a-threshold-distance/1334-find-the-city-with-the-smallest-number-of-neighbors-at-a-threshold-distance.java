6fa43d2ef07f7f1eb7bcb5bc9349a8955311e3de
pallavi-2303 <168811556+pallavi-2303@users.noreply.github.com>
2025-06-28 18:23:36 +0530

    Time: 106 ms (6.72%), Space: 44.5 MB (61.12%) - LeetHub

---
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -0,0 +1,69 @@
+class Solution {
+
+    // Bellman-Ford algorithm to find shortest paths from a source city
+    void bellmanFord(int n, int[][] edges, int[] shortestPathDistances, int source) {
+        Arrays.fill(shortestPathDistances, Integer.MAX_VALUE);
+        shortestPathDistances[source] = 0;
+
+        for (int i = 1; i < n; i++) {
+            for (int[] edge : edges) {
+                int u = edge[0];
+                int v = edge[1];
+                int wt = edge[2];
+                if (shortestPathDistances[u] != Integer.MAX_VALUE && 
+                    shortestPathDistances[u] + wt < shortestPathDistances[v]) {
+                    shortestPathDistances[v] = shortestPathDistances[u] + wt;
+                }
+                // Bi-directional edge
+                if (shortestPathDistances[v] != Integer.MAX_VALUE && 
+                    shortestPathDistances[v] + wt < shortestPathDistances[u]) {
+                    shortestPathDistances[u] = shortestPathDistances[v] + wt;
+                }
+            }
+        }
+    }
+
+    int getCityWithFewestReachable(int n, int[][] shortestPathMatrix, int distanceThreshold) {
+        int cityWithFewestReachable = -1;
+        int fewestReachableCount = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            int reachableCount = 0;
+            for (int j = 0; j < n; j++) {
+                if (i != j && shortestPathMatrix[i][j] <= distanceThreshold) {
+                    reachableCount++;
+                }
+            }
+
+            if (reachableCount <= fewestReachableCount) {
+                fewestReachableCount = reachableCount;
+                cityWithFewestReachable = i;
+            }
+        }
+        return cityWithFewestReachable;
+    }
+
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        int[][] shortestPathMatrix = new int[n][n];
+
+      /*  for (int i = 0; i < n; i++) {
+            Arrays.fill(shortestPathMatrix[i], Integer.MAX_VALUE);
+            shortestPathMatrix[i][i] = 0;  // Distance to itself is zero
+        }
+
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
+            shortestPathMatrix[u][v] = wt;
+            shortestPathMatrix[v][u] = wt;
+        }*/
+
+        // Compute shortest paths from each city using Bellman-Ford algorithm
+        for (int i = 0; i < n; i++) {
+            bellmanFord(n, edges, shortestPathMatrix[i], i);
+        }
+
+        return getCityWithFewestReachable(n, shortestPathMatrix, distanceThreshold);
+    }
+}
