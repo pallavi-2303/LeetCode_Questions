6f26b7be5cce64b108a821a79811014a44f2bf82
Pallavi Singh <168811556+pallavi-2303@users.noreply.github.com>
2025-08-21 04:05:29 +0530

    Time: 80 ms (59.4%), Space: 97.4 MB (13.01%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
@@ -1,30 +1,54 @@
 class Solution {
-    public void dfs(int ancestor,int node,List<List<Integer>> adj,List<List<Integer>> res){
-    for(int neigh:adj.get(node)){
-  if(res.get(neigh).isEmpty() || res.get(neigh).get(res.get(neigh).size()-1)!=ancestor) {
-res.get(neigh).add(ancestor);
-dfs(ancestor,neigh,adj,res);
-  }
-    }
-    }
     public List<List<Integer>> getAncestors(int n, int[][] edges) {
-     //the nodes to which it is going then particualr node will become ancestor of that node
-     List<List<Integer>> adj=new ArrayList<>();
-     for(int i=0;i<n;i++) {
-        adj.add(new ArrayList<>());
-     }  
- List<List<Integer>> res=new ArrayList<>();
-  for(int i=0;i<n;i++) {
-        res.add(new ArrayList<>());
-     }
-for(int[] a:edges){
-adj.get(a[0]).add(a[1]);
+        //we can only calulate a node ancestor if it previous ancestor are calculated so we can use a topo sort order of nodes
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        int[] indegre = new int[n];
+        for (int[] a : edges) {
+            int u = a[0];
+            int v = a[1];
+            adj.get(u).add(v);
+            indegre[v]++;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (indegre[i] == 0) {
+                q.add(i);
+            }
+        }
+        List<Integer> topo = new ArrayList<>();
+        while (!q.isEmpty()) {
+            int node = q.poll();
+          topo.add(node);
+            for (int it : adj.get(node)) {
+                indegre[it]--;
+                if (indegre[it] == 0) {
+                    q.add(it);
+                }
+            }
+        }
+        //since topo sort contains elements in sorted array so if  ancestor of node is required it will be filled previously
+List<Set<Integer>> res=new ArrayList<>();
+for(int i=0;i<n;i++) 
+res.add(new HashSet<>());
+for(int node:topo){
+for(int v:adj.get(node)){
+res.get(v).add(node);//adding the current ancestor 
+//adding all ancestor of node also as node ancestor will also ve ancestor of v
+res.get(v).addAll(res.get(node));//all ancesttor of node
 }
-//checking out nodes for which the particuar node can be ancestor
-for(int i=0;i<n;i++){
-dfs(i,i,adj,res);
 }
-return res;
+ // Step 3: convert to sorted lists
+        List<List<Integer>> ans = new ArrayList<>();
+         for (int i = 0; i < n; i++) {
+            List<Integer> temp=new ArrayList<>(res.get(i));
+            Collections.sort(temp);
+            ans.add(temp);
+        }
+
+    return ans;
 
     }
 }
