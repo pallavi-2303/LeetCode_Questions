56a93277a521860d95eecf83b7105dfb87b2411f
Pallavi Singh <168811556+pallavi-2303@users.noreply.github.com>
2025-08-24 01:10:39 +0530

    Time: 5 ms (14.64%), Space: 45.8 MB (5.79%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -9,6 +9,22 @@ class Solution {
     }
     return false;//not found
     }
+    public boolean bfs(int src,int des,int[] vis,List<List<Integer>> adj){
+    Queue<Integer> q=new LinkedList<>();
+    q.add(src);
+    vis[src]=1;
+    while(!q.isEmpty()){
+    int node=q.poll();
+    if(node==des) return true;
+    for(int it:adj.get(node)){
+    if(vis[it]==0){
+    vis[it]=1;
+    q.add(it);
+    }
+    }
+    }
+return false;
+    }
     public int[] findRedundantConnection(int[][] edges) {
     //in this we have to remove the last node that is already connected
     int n=edges.length; 
@@ -17,11 +33,12 @@ for(int i=0;i<=n;i++){
 adj.add(new ArrayList<>());
 }
 //treverse on all edges
+//Tc(V+E)
 for(int[] a:edges){
 int u=a[0];
 int v=a[1];
 int[] vis=new int[n+1];
-if(!adj.get(u).isEmpty() && !adj.get(v).isEmpty() && dfs(u,v,vis,adj)){
+if(!adj.get(u).isEmpty() && !adj.get(v).isEmpty() && bfs(u,v,vis,adj)){
 //if u and v are already conncted then this is last edge that can lead to cycle
 return a;
 }
