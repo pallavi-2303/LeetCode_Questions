f51410a7d90a6644a3732bd8e8d356bb8ae044cd
Pallavi Singh <168811556+pallavi-2303@users.noreply.github.com>
2025-08-24 01:00:09 +0530

    Time: 5 ms (14.55%), Space: 45.6 MB (5.89%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean dfs(int src,int des,int[] vis,List<List<Integer>> adj){
+    vis[src]=1;
+    if(src==des) return true;
+    for(int it:adj.get(src)){
+    if(vis[it]==0){
+    if(dfs(it,des,vis,adj)) return true;
+    }
+    }
+    return false;//not found
+    }
+    public int[] findRedundantConnection(int[][] edges) {
+    //in this we have to remove the last node that is already connected
+    int n=edges.length; 
+List<List<Integer>> adj=new ArrayList<>();
+for(int i=0;i<=n;i++){
+adj.add(new ArrayList<>());
+}
+//treverse on all edges
+for(int[] a:edges){
+int u=a[0];
+int v=a[1];
+int[] vis=new int[n+1];
+if(!adj.get(u).isEmpty() && !adj.get(v).isEmpty() && dfs(u,v,vis,adj)){
+//if u and v are already conncted then this is last edge that can lead to cycle
+return a;
+}
+adj.get(u).add(v);
+adj.get(v).add(u);
+}
+return new int[]{};
+
+    }
+}
