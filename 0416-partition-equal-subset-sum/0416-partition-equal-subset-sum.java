ed460b4f4a413f7f926e21be0da06853619a3e00
Pallavi Singh <168811556+pallavi-2303@users.noreply.github.com>
2025-09-13 00:05:21 +0530

    Time: 45 ms (59.43%), Space: 56.9 MB (37.91%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -30,8 +30,24 @@ take=solve(idx+1,nums,target-nums[idx],dp);
     if(totalSum%2==1) return false;
     int target=totalSum/2;//means is a subset can contain this sum
     int[][] dp=new int[n+1][target+1];
-    for(int[] a:dp) Arrays.fill(a,-1);
-    return solve(0,nums,target,dp);
-
+   // for(int[] a:dp) Arrays.fill(a,-1);
+   // return solve(0,nums,target,dp);
+  for(int tar=0;tar<=target;tar++){
+dp[n][tar]=tar==0 ? 1 :0;
+  }
+   for(int i=0;i<n;i++){
+dp[i][0]=1;
+   }
+for(int i=n-1;i>=0;i--){
+for(int tar=1;tar<=target;tar++){
+boolean take=false;
+if(nums[i]<=tar){
+take=dp[i+1][tar-nums[i]]==1 ? true : false;
+}
+boolean nottake=dp[i+1][tar]==1 ? true : false;
+dp[i][tar]=(take|nottake) ? 1 :0;
+}
+}
+return dp[0][target]==1 ? true : false;
     }
 }
