0736bdc4daba5635dbf7a18b6fdd905fae721bbd
Pallavi Singh <168811556+pallavi-2303@users.noreply.github.com>
2025-09-12 23:41:32 +0530

    Time: 116 ms (8.43%), Space: 56.3 MB (40.14%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,28 +1,37 @@
 class Solution {
-    public boolean findSubset(int idx,int target,int[] nums,int[][] dp){
-    //base case
-if(target==0) return true;
-if(idx==0)
-return nums[0]==target;
-if(dp[idx][target]!=-1) return dp[idx][target]==1 ? true :false;
-boolean take=false;
-if(nums[idx]<=target){
-take=findSubset(idx-1,target-nums[idx],nums,dp);
-}
-//nottake case
-boolean nottake=findSubset(idx-1,target,nums,dp);
- dp[idx][target]=(take||nottake) ? 1:0;
- return take||nottake;
+    int n;
+    public boolean solve(int idx,int[] nums,int target,int[][] dp){
+    if(target==0){
+    return true;
+    }
+    if(idx>=n){
+    return target==0;
+    }
+  //we have two cases wheather to take this index for subset or not take it
+
+  if(dp[idx][target]!=-1){
+return dp[idx][target]==1 ? true :false;
+  }
+ boolean take=false;
+  if(nums[idx]<=target) {
+take=solve(idx+1,nums,target-nums[idx],dp);
+  } 
+  boolean nottake=solve(idx+1,nums,target,dp);
+ dp[idx][target]=(take | nottake)==true ? 1 :0;
+  return take | nottake;
     }
     public boolean canPartition(int[] nums) {
-   //we can devide an array in subset if and only if sum if even if sum is odd we will not be able to devide in 2 subset
-    int sum=0;
-    int n=nums.length;
-    for(int num:nums) sum+=num;
-    if(sum%2==1)   return false;
-    int target=sum/2;
-int[][] dp=new int[n][target+1];
-for(int[] a:dp) Arrays.fill(a,-1);
-return findSubset(n-1,target,nums,dp);
+      //if the total sum of all the array is odd then it wont be possible to devide the subset in two parts as it will neverr give a equal sum and now we if are given a even aray sum array it is possible to devide it intwo parts
+     n=nums.length;
+      int totalSum=0;
+      for(int num:nums){
+      totalSum+=num;  
+      }  
+    if(totalSum%2==1) return false;
+    int target=totalSum/2;//means is a subset can contain this sum
+    int[][] dp=new int[n+1][target+1];
+    for(int[] a:dp) Arrays.fill(a,-1);
+    return solve(0,nums,target,dp);
+
     }
 }
