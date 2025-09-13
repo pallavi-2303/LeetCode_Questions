620c83b8897a22dcc4f8979eb34f60d6ac7320a3
Pallavi Singh <168811556+pallavi-2303@users.noreply.github.com>
2025-09-14 00:46:32 +0530

    Time: 1 ms (99.85%), Space: 41.4 MB (92.91%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,31 +1,27 @@
 class Solution {
-    int n;
-    public int findProfit(int idx,int[] prices,int buy,int k,int[][][] dp){
-    //base case if number of transction is finded means buy and cell is finished one trasaction completed
-    if(k==0 ||idx>=n) return 0;
-    if(dp[idx][k][buy]!=-1){
-    return dp[idx][k][buy];
-    }
+    public int maxProfit(int k, int[] prices) {
+    int n=prices.length;
+    //int[][][] dp=new int[n+1][2][k+1];
+     int[][] curr=new int[2][k+1];
+      int[][] ahead=new int[2][k+1];
+    for(int i=n-1;i>=0;i--){
+    for(int buy=0;buy<=1;buy++){
+    for(int cap=1;cap<=k;cap++){
     int profit=0;
     if(buy==1){
-        //we have two options wheather we can buy or not buy
-    profit=Math.max(-prices[idx]+findProfit(idx+1,prices,0,k,dp),0+findProfit(idx+1,prices,1,k,dp));
+    // profit=Math.max(-prices[i]+dp[i+1][0][cap],dp[i+1][1][cap]);
+    profit=Math.max(-prices[i]+ahead[0][cap],ahead[1][cap]);
     }
+
     else {
-  //we can sell and not cell at this index
-  profit=Math.max(prices[idx]+findProfit(idx+1,prices,1,k-1,dp),findProfit(idx+1,prices,0,k,dp));
+    profit=Math.max(prices[i]+ahead[1][cap-1],ahead[0][cap]);
     }
-    return dp[idx][k][buy]= profit;
+   // dp[i][buy][cap]=profit;
+   curr[buy][cap]=profit;
     }
-    public int maxProfit(int k, int[] prices) {
-    //here at particular index we have two options wheather we will buy at that index if we are ahaving buy or not buy at that index or sell at particlar index or not cell
- n=prices.length;
-int[][][] dp=new int[n+1][k+1][2];
-for(int[][] a:dp){
-for(int[] b:a){
-Arrays.fill(b,-1);
-}
-}
-return findProfit(0,prices,1,k,dp);
+    }
+ahead=curr;
+    } 
+    return ahead[1][k];  
     }
 }
