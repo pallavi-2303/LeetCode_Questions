d5667533882ddc9d39fac79165cc05f90d1d138b
Pallavi Singh <168811556+pallavi-2303@users.noreply.github.com>
2025-12-19 01:20:19 +0530

    Time: 839 ms (10.95%), Space: 62.9 MB (34.01%) - LeetHub

--- 2040-kth-smallest-product-of-two-sorted-arrays/2040-kth-smallest-product-of-two-sorted-arrays.java
+++ 2040-kth-smallest-product-of-two-sorted-arrays/2040-kth-smallest-product-of-two-sorted-arrays.java
@@ -1,63 +1,63 @@
 class Solution {
-    public long findCount(int[] nums1, int[] nums2, long midProduct) {
-        long countProduct = 0;
-        int n = nums2.length;
-        //we will search for nums1 how many productvwe can form with nums2 2 if we have product less than equal to given product 
-        for (int i = 0; i < nums1.length; i++) {
-            if (nums1[i] >= 0) {
-                int l = 0;
-                int r = n - 1;
-                int m = -1;
-                while (l <= r) {
-                    int mid = l + (r - l) / 2;
-                    long product = (long)nums2[mid] * nums1[i];
-                    if (product <= midProduct) {
-                        m = mid;
-                        l = mid + 1;
-                    } else {
-                        r = mid - 1;
-                    }
-                }
-                countProduct += (m + 1);
-            } else {
-                //if nums1 is negative 
-                int l = 0;
-                int r = n - 1;
-                int m = n;
-                while (l <= r) {
-                    int mid = l + (r - l) / 2;
-                    long product = (long)nums1[i] * nums2[mid];
-                    if (product <= midProduct) {
-                        m = mid;
-                        r = mid - 1;
-                        ;
-                    } else {
-                        l = mid + 1;
-                    }
-                }
-                countProduct += (n - m);
-            }
-        }
-        return countProduct;
+    public long findCount(int[] nums1,int[] nums2,long k,long product){
+    int n1=nums1.length;
+    int n2=nums2.length;
+    //we have to find the count of elelemts that has product less than equal to give n product we will fix element of nums1 and check for nums2
+long count=0;
+for(int i=0;i<n1;i++){
+//we are fixing nums1
+if(nums1[i]>0){
+long m=-1;//invalid index to left
+int l=0;
+int h=n2-1;
+while(l<=h){
+int mid=l+(h-l)/2;
+long currProduct=(long)nums1[i]*nums2[mid];
+if(currProduct<=product){
+m=mid;
+l=mid+1;
+}
+else {
+h=mid-1;
+}
+}
+count=count+(m+1);//m+1 possible products are available for given value of i and j
+}
+else {
+long m=n2;//invalid index to right
+int l=0;
+int h=n2-1;
+while(l<=h){
+int mid=l+(h-l)/2;
+long currProduct=(long)nums1[i]*nums2[mid];
+if(currProduct<=product){
+m=mid;
+h=mid-1;//increase the product
+}
+else {
+l=mid+1;
+}
+}
+count=count+(n2-m);//this number of vald count
+}
+}
+return count;
     }
-
     public long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
-        // we need kth smallest to find kth smallest we will try for every possible value of K
-        long left = -1_000_000_0000L; // -1e10
-        long right = 1_000_000_0000L;//1e10
-        long result = 0;
-        //apply binary search 
-        while (left <= right) {
-            long mid = left + ((right - left) >> 1);
-            long countEquals = findCount(nums1, nums2, mid);
-            if (countEquals >= k) {
-                result = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return result;
-
+    //since both arrays are sorted we can we can apply binary search on answer and choose a peoduct and find out how many elemnts give prdoduct less than equal to this product to keep track the kth smalllest product 
+  long low=-(long)1e10 ;
+  long high=(long) 1e10;
+  long res=0;
+  while(low<=high) {
+long midProduct=low+(high-low)/2;
+if(findCount(nums1,nums2,k,midProduct)<k){
+low=midProduct+1;
+}
+else {
+    res=midProduct;
+    high=midProduct-1;
+}
+  }
+  return res;  
     }
 }
