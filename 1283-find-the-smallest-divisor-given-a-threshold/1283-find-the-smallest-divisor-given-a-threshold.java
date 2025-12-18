36c6424ba0e6e25c64397f631b3fe19b9113ba03
Pallavi Singh <168811556+pallavi-2303@users.noreply.github.com>
2025-12-19 00:49:02 +0530

    Time: 33 ms (8.86%), Space: 51.2 MB (5.71%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
@@ -1,40 +1,28 @@
 class Solution {
-public boolean findDivisionResult(int[] nums,int divisor,int threshold) {
- int n=nums.length;
- int sum=0;
- for(int i=0;i<n;i++){
- sum+=Math.ceil((double)nums[i]/(double)divisor);
- }
- return sum<=threshold;
-}
-  public int findMax(int[] arr) {
- int maxi=Integer.MIN_VALUE;
- for(int num:arr) {
- maxi=Math.max(maxi,num);
- }
- return maxi;}
+    public boolean isPossible(int[] nums,int mid,int threshold){
+    int n=nums.length;
+    int d=0;
+    for(int i=0;i<n;i++){
+    d=d+(int)Math.ceil(nums[i]/(double)mid);
+    }
+    return d<=threshold;
+    }
     public int smallestDivisor(int[] nums, int threshold) {
-       int low=1;
-int high=findMax(nums);
-// for(int i=low;i<=high;i++){
-// if(findDivisionResult(nums,i, threshold)){
-//  return i;
-// }
-// else continue;
-// }
-// return -1;
-//binary Search
-while(low<=high){
-int mid=low+(high-low)/2;
-if(findDivisionResult(nums,mid, threshold)) {
-// ans=mid;
-high=mid-1;
-}
-else {
- low=mid+1;
-}
-}
-//return ans;
-return low; 
+     //check all the divisor from 1 to largest elelemnt as it is tesmallest divisor affaterdivising it can give 0.. whihc round off to nreats value that is 1
+     int n=nums.length;
+     int low=1;
+     int high=Arrays.stream(nums).max().getAsInt();
+     int res=-1;
+     while(low<=high) {
+    int mid=low+(high-low)/2;
+    if(isPossible(nums,mid,threshold)){
+    res=mid;
+    high=mid-1;
+    }
+    else {
+    low=mid+1;
+    }
+     }
+     return res;
     }
 }
