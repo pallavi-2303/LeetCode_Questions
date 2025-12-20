1537dcb9cecc03b2aa05aa81e2b2f45618593f70
Pallavi Singh <168811556+pallavi-2303@users.noreply.github.com>
2025-12-21 04:02:03 +0530

    Time: 1 ms (99.78%), Space: 48.9 MB (22.93%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,61 +1,34 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        //since the array is sorted we can use merge sort aproch to find the median elements
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-        int n = n1 + n2;
-        int ele1 = -1;
-        int ele2 = -1;
-        int idx1 = n / 2;
-        int idx2 = n / 2 - 1;
-        int idx = 0;
-        int i = 0;
-        int j = 0;
-        while (i < n1 && j < n2) {
-            if (nums1[i] <= nums2[j]) {
-                if (idx == idx1) {
-                    ele1 = nums1[i];
-                }
-                if (idx == idx2) {
-                    ele2 = nums1[i];
-                }
-                i++;
-                idx++;
-            } else {
-                if (idx == idx1) {
-                    ele1 = nums2[j];
-                }
-                if (idx == idx2) {
-                    ele2 = nums2[j];
-                }
-                j++;
-                idx++;
-            }
-        }
-        while (i < n1) {
-            if (ele1 != -1 && ele2 != -1)
-                break;
-            if (idx == idx1) {
-                ele1 = nums1[i];
-            }
-            if (idx == idx2) {
-                ele2 = nums1[i];
-            }
-            idx++;
-            i++;
-        }
-        while (j < n2) {
-            if (ele1 != -1 && ele2 != -1)
-                break;
-            if (idx == idx1) {
-                ele1 = nums2[j];
-            }
-            if (idx == idx2) {
-                ele2 = nums2[j];
-            }
-            idx++;
-            j++;
-        }
-        return n%2==1 ? ele1 :(ele1+ele2)/2.0;
+      int n1=nums1.length;
+      int n2=nums2.length;
+      //we will apply binary search on sorter array 
+      //take 0 elelemnts from array 1 and n ellements from array 1 1 3 // 2
+      if(n2<n1) return findMedianSortedArrays(nums2, nums1);
+      int n=n1+n2;//elements in left 
+      int required=(n+1)/2;
+     int low=0;//take elements from array1 in left
+     int high=n1;//take elements from array1 in left
+     while(low<=high) {
+    //elements from array 1
+    int mid1=low+(high-low)/2;
+    int mid2=required-mid1;//number of elelemnts from array2 ij left
+    int l1=mid1-1>=0 ? nums1[mid1-1] :Integer.MIN_VALUE;
+    int l2=mid2-1>=0 ? nums2[mid2-1] :Integer.MIN_VALUE;
+    int r1=mid1<n1 ? nums1[mid1] :Integer.MAX_VALUE;
+    int r2=mid2<n2? nums2[mid2] :Integer.MAX_VALUE;
+    //l1 <=r1 and l2<=r2
+    if(l1<=r2 && l2<=r1){
+    //we found correct sorted half
+    return n%2==1 ? (double)Math.max(l1,l2) : (Math.max(l1,l2)+Math.min(r1,r2))/2.0;
+    }
+    else if(l1>r2){
+    high=mid1-1;
+    }
+    else {
+    low=mid1+1;
+    }
+     }
+     return -1;
     }
 }
