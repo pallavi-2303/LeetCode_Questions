f53ce88d84c4c2a0886d4cde021dcf006b6b41be
Pallavi Singh <168811556+pallavi-2303@users.noreply.github.com>
2025-12-21 03:59:58 +0530

    Time: 2 ms (46.31%), Space: 49.2 MB (9.58%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,13 +1,61 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-     //brute force put all ellements in array sort and find mediam of the array 
-     int n1=nums1.length;
-     int n2=nums2.length;
-     List<Integer> arr=new ArrayList<>();
-     for(int num:nums1) arr.add(num);
-     for(int num:nums2) arr.add(num) ;
-     Collections.sort(arr);
-   int n=  n1+n2;
-     return n%2==1 ? arr.get(n/2)*1.0 :(arr.get(n/2)+arr.get((n/2)-1))/2.0;
+        //since the array is sorted we can use merge sort aproch to find the median elements
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int n = n1 + n2;
+        int ele1 = -1;
+        int ele2 = -1;
+        int idx1 = n / 2;
+        int idx2 = n / 2 - 1;
+        int idx = 0;
+        int i = 0;
+        int j = 0;
+        while (i < n1 && j < n2) {
+            if (nums1[i] <= nums2[j]) {
+                if (idx == idx1) {
+                    ele1 = nums1[i];
+                }
+                if (idx == idx2) {
+                    ele2 = nums1[i];
+                }
+                i++;
+                idx++;
+            } else {
+                if (idx == idx1) {
+                    ele1 = nums2[j];
+                }
+                if (idx == idx2) {
+                    ele2 = nums2[j];
+                }
+                j++;
+                idx++;
+            }
+        }
+        while (i < n1) {
+            if (ele1 != -1 && ele2 != -1)
+                break;
+            if (idx == idx1) {
+                ele1 = nums1[i];
+            }
+            if (idx == idx2) {
+                ele2 = nums1[i];
+            }
+            idx++;
+            i++;
+        }
+        while (j < n2) {
+            if (ele1 != -1 && ele2 != -1)
+                break;
+            if (idx == idx1) {
+                ele1 = nums2[j];
+            }
+            if (idx == idx2) {
+                ele2 = nums2[j];
+            }
+            idx++;
+            j++;
+        }
+        return n%2==1 ? ele1 :(ele1+ele2)/2.0;
     }
 }
