21720bf22197708e9b78d8fa633e9c45b3f99dea
Pallavi Singh <168811556+pallavi-2303@users.noreply.github.com>
2025-12-21 03:15:28 +0530

    Time: 6 ms (24.4%), Space: 49.2 MB (9.58%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,75 +1,13 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+     //brute force put all ellements in array sort and find mediam of the array 
      int n1=nums1.length;
      int n2=nums2.length;
-     int n=n1+n2;
-    /* List<Integer> arr=new ArrayList<>();
-     for(int num:nums1)  arr.add(num);
-     for(int num:nums2)  arr.add(num);
+     List<Integer> arr=new ArrayList<>();
+     for(int num:nums1) arr.add(num);
+     for(int num:nums2) arr.add(num) ;
      Collections.sort(arr);
-     
-     if(n%2==1) return arr.get(n/2);
-     else return (arr.get(n/2)+arr.get(n/2-1))/2.0;*/
-//we want n/2 and n/2-1 ellement index  so we can track it as both aeeat is soted we dont need extra space and extra complexity of sorting
-int idx1=n/2-1;
-int idx2=n/2;
-int ele1=-1;
-int ele2=-1;
-int idx=0;
-int i=0;
-int j=0;
-while(i<n1 && j<n2){
-if(nums1[i]<=nums2[j]){
-if(idx==idx1){
-//we found the first element
-ele1=nums1[i];
-}
-if(idx==idx2){
-//we found the second element
-ele2=nums1[i];
-}
-idx++;
-i++;
-}
-else {
-if(idx==idx1){
-//we found the first element
-ele1=nums2[j];
-}
-if(idx==idx2){
-//we found the second element
-ele2=nums2[j];
-}
-j++;
-idx++;
-}
-}
-while(i<n1){
-if(ele1!=-1 && ele2!=-1) break;
-if(idx==idx1){
-//we found the first element
-ele1=nums1[i];
-}
-if(idx==idx2){
-//we found the second element
-ele2=nums1[i];
-}
-i++;
-idx++;
-}
-while(j<n2){
-if(idx==idx1){
-//we found the first element
-ele1=nums2[j];
-}
-if(idx==idx2){
-//we found the second element
-ele2=nums2[j];
-}
-j++;
-idx++;
-}
-return n%2==1 ? ele2 :(ele1+ele2)/2.0;
-
+   int n=  n1+n2;
+     return n%2==1 ? arr.get(n/2)*1.0 :(arr.get(n/2)+arr.get((n/2)-1))/2.0;
     }
 }
