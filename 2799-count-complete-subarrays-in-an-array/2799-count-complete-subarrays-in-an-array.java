0adafd8204a1c7f65b9f2fda6a5391a7e1788fa7
Pallavi Singh <168811556+pallavi-2303@users.noreply.github.com>
2025-12-22 03:40:27 +0530

    Time: 10 ms (54.08%), Space: 47 MB (54.27%) - LeetHub

--- 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.java
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.java
@@ -1,21 +1,30 @@
 class Solution {
-    public int countCompleteSubarrays(int[] nums) {
-     int n=nums.length;
-     HashMap<Integer,Integer> mp=new HashMap<>();
-     for(int num:nums) {
-    mp.put(num,mp.getOrDefault(num,0)+1);
-     } 
-    int size=mp.size();
+    public int findSubarray(int[] nums,int goal){
+    if(goal<0) return 0;
+    HashMap<Integer,Integer> mp=new HashMap<>();
     int count=0;
-    for(int i=0;i<n;i++) {
-    HashMap<Integer,Integer> temp=new HashMap<>();
-    for(int j=i;j<n;j++){
-    temp.put(nums[j],temp.getOrDefault(nums[j],0)+1);
-    if(temp.size()==size){
-    count++;
+    int n=nums.length;
+    int i=0;
+    int j=0;
+    while(j<n){
+    mp.put(nums[j],mp.getOrDefault(nums[j],0)+1);
+    while(mp.size()>goal){
+    mp.put(nums[i],mp.get(nums[i])-1);
+    if(mp.get(nums[i])==0){
+    mp.remove(nums[i]);
     }
+    i++;
     }
+    count+=(j-i+1);
+    j++;
     }
-    return count++;
+    return count;
+    }
+    public int countCompleteSubarrays(int[] nums) {
+    int n=nums.length;
+    //number of subarray with numbers equal to nums lesss than equal to distcnt-less than equal to distnct-1
+    HashMap<Integer,Integer> mp=new HashMap<>();
+    for(int num:nums) mp.put(num,mp.getOrDefault(num,0)+1);
+    return findSubarray(nums,mp.size())-findSubarray(nums,mp.size()-1);
     }
 }
