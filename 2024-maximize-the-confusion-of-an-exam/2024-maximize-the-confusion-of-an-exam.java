1d69d033f182448c7058e51df8aec5abf978d603
Pallavi Singh <168811556+pallavi-2303@users.noreply.github.com>
2025-12-25 02:21:14 +0530

    Time: 62 ms (5.1%), Space: 47.3 MB (7.21%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -21,8 +21,30 @@ r++;
 }
 return maxLen;
     }
+     public int solve2(String answerKey,int k){
+    int maxLen=0;
+int l=0;
+int r=0;
+ int n=answerKey.length();
+HashMap<Character,Integer> mp=new HashMap<>();
+while(r<n){
+char ch=answerKey.charAt(r);
+mp.put(ch,mp.getOrDefault(ch,0)+1);
+//if invald window 
+while(Math.min(mp.getOrDefault('T',0),mp.getOrDefault('F',0))>k){
+//shrink from left
+char tch=answerKey.charAt(l);
+mp.put(tch,mp.get(tch)-1);
+l++;
+}
+maxLen=Math.max(maxLen,r-l+1);
+r++;
+} 
+return maxLen;  
+     }
     public int maxConsecutiveAnswers(String answerKey, int k) {
     //max streak of given character with atmost particular charcter
-    return Math.max(solve(answerKey,'T',k),solve(answerKey,'F',k)) ;   
+   // return Math.max(solve(answerKey,'T',k),solve(answerKey,'F',k)) ;  
+   return solve2(answerKey,k) ;
     }
 }
