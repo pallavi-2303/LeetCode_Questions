663ab2a9c4ef3a5e5a81cbac142b1038858c8b88
Pallavi Singh <168811556+pallavi-2303@users.noreply.github.com>
2025-12-25 02:12:23 +0530

    Time: 14 ms (48.12%), Space: 46.7 MB (12.64%) - LeetHub

---
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solve(String answerKey,char ch,int k){
+    int n=answerKey.length();
+    //given charcter ka max k ocoorence hone chahiye in given subarray
+int maxLen=0;
+int l=0;
+int r=0;
+int count=0;
+while(r<n){
+if(answerKey.charAt(r)==ch){
+count++;
+}
+while(count>k){
+if(answerKey.charAt(l)==ch){
+count--;
+}
+l++;
+}
+maxLen=Math.max(maxLen,r-l+1);
+r++;
+}
+return maxLen;
+    }
+    public int maxConsecutiveAnswers(String answerKey, int k) {
+    //max streak of given character with atmost particular charcter
+    return Math.max(solve(answerKey,'T',k),solve(answerKey,'F',k)) ;   
+    }
+}
