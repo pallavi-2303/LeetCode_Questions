0043e02e2d472cf98a358f2aef98d19c52ff83f0
Pallavi Singh <168811556+pallavi-2303@users.noreply.github.com>
2026-06-13 16:28:37 +0530

    Time: 66 ms (42.67%), Space: 43.9 MB (82.59%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
@@ -1,30 +1,27 @@
 class Solution {
-  public int Max(int[] freq) {
-   int max=Integer.MIN_VALUE;
-   for(int i=0;i<26;i++) {
-   if(freq[i]!=0) 
-   max=Math.max(max,freq[i]);}
-   return max;}
-  public int Min(int[] freq) {
- int min=Integer.MAX_VALUE;
- for(int i=0;i<26;i++) {
-  if(freq[i]!=0) 
-  min=Math.min(min,freq[i]);
-   }
-      return min;}
-    
+    public int find(int[] freq) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int f : freq) {
+            if (f != 0) {
+                min = Math.min(min, f);
+                max = Math.max(max, f);
+            }
+        }
+        return max - min;
+    }
+
     public int beautySum(String s) {
-    int n=s.length();
-  int ans=0;
-  for(int i=0;i<n;i++) {
- int[] freq=new int[26];
-  for(int j=i;j<n;j++) {
- freq[s.charAt(j)-'a']++;
- int max=Max(freq);
- int min=Min(freq);
- int beauty=max-min;
-  ans+=beauty;}}
-        return ans;
-   
+        int n = s.length();
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            int[] freq = new int[26];
+            for (int j = i; j < n; j++) {
+                char ch = s.charAt(j);
+                freq[ch - 'a']++;
+                result += find(freq);
+            }
+        }
+        return result;
     }
 }
