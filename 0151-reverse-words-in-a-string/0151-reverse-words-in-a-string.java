187dd6747ed4e0c11e78e523d07deff44e1facc9
Pallavi Singh <168811556+pallavi-2303@users.noreply.github.com>
2026-06-13 15:30:31 +0530

    Time: 3 ms (98.83%), Space: 44.4 MB (48.22%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,26 +1,28 @@
 class Solution {
     public String reverseWords(String s) {
-    int n=s.length();
-    String ans="";
-    int i=n-1;
-    int j=-1;
-  while(i>=0) {
+   
+int n=s.length();
+StringBuilder sb=new StringBuilder();
+
+int i=n-1;
+while(i>=0){
 while(i>=0 && s.charAt(i)==' '){
 i--;
 }
 if(i<0) break;
-if(i>=0) j=i;
+int j=i;
 while(i>=0 && s.charAt(i)!=' '){
 i--;
 }
-//i will to empty space and then  it stops
-if(ans.length()==0){
-ans=ans.concat(s.substring(i+1,j+1));
+if(sb.isEmpty()){
+sb.append(s.substring(i+1,j+1));
 }
 else {
-ans=ans.concat(" "+s.substring(i+1,j+1));
+sb.append(" ").append(s.substring(i+1,j+1));
 }
-  } 
-return ans;
+}
+
+        return sb.toString();
     }
-}
+}    
+  
