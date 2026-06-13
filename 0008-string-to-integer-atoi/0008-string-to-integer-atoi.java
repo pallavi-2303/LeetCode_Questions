309e9f2e2bae907e36dde34e2c68522c5b82a8fd
Pallavi Singh <168811556+pallavi-2303@users.noreply.github.com>
2026-06-13 15:44:24 +0530

    Time: 1 ms (100%), Space: 43.6 MB (74.86%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,24 +1,25 @@
 class Solution {
     public int myAtoi(String s) {
-        //To reduce leading white spaces trim the string
-if(s==null) return 0;
-s=s.trim();
-if(s.length()==0) return 0;
-int min=Integer.MIN_VALUE;
-int max=Integer.MAX_VALUE;
-int sign=1;
-long ans=0;
-if(s.charAt(0)=='-'){
-sign=-1;}
-int i=s.charAt(0)=='-' || s.charAt(0)=='+' ? 1:0;
-while(i<s.length()){
-char ch=s.charAt(i);
-if(!Character.isDigit(ch)){
-break;}
-ans=ans*10+ch-'0';
-if(sign==-1 && ans*sign<min) return min;
-if(sign==1 && sign*ans>max) return max;
-i++;}
-return (int) ans*sign;
+    if(s==null) return 0;
+    s=s.trim();
+    int n=s.length();
+    if(n==0) return 0;
+    int min=Integer.MIN_VALUE;
+    int max=Integer.MAX_VALUE;
+    long ans=0;
+    int sign=s.charAt(0)=='-' ? -1 :1;
+    int i=(s.charAt(0)=='-' ||s.charAt(0)=='+') ? 1:0;
+    while(i<n){
+    char ch=s.charAt(i);
+    if(!Character.isDigit(ch)){
+    break;
+    }
+    ans=ans*10+(ch-'0');
+    if(sign==1 && ans*sign>max) return max;
+     if(sign==-1 && ans*sign<min) return min;
+    i++;
+    }
+    return (int) (ans*sign);
+      
     }
 }
