91d923e8d96db084dca426f6af4523852a2b474c
Pallavi Singh <168811556+pallavi-2303@users.noreply.github.com>
2026-06-13 16:06:05 +0530

    Time: 264 ms (17.51%), Space: 105.4 MB (5.21%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,34 +1,31 @@
 class Solution {
-    public boolean isPallindrome(int i,int j,String s,int[][] dp){
-if(i>=j) return true;
-if(dp[i][j]!=-1) return dp[i][j]==1 ? true : false;
-boolean result=false;
-if(s.charAt(i)==s.charAt(j)) {
-result=isPallindrome(i+1,j-1,s,dp);
-dp[i][j]=result ? 1 :0;
-return result;
-}
-else {
-dp[i][j]=0;
-return false;}
-}
+    public int solve(int i,int j,String s,int[][] dp){
+    if(i>=j) return 1;
+    if(dp[i][j]!=-1) return dp[i][j];
+    if(s.charAt(i)==s.charAt(j)){
+        return dp[i][j]=solve(i+1,j-1,s,dp);
+    }
+    else {
+        return dp[i][j]=0;
+    }
+    }
     public String longestPalindrome(String s) {
-        int n=s.length();
-int[][] dp=new int[n][n];
-for(int[] a:dp) 
-Arrays.fill(a,-1);
-int maxi=0;
-int st=-1;
-for(int i=0;i<n;i++){
-for(int j=i;j<n;j++){
-if(isPallindrome(i,j,s,dp)) {
-if(j+1-i>maxi){
-maxi=j+1-i;
-st=i;
-}
-}
-}
-}
-return s.substring(st,st+maxi);
+     //brute force;
+     int n=s.length();
+     int max=0;
+     int stIdx=-1;
+     int[][] dp=new int[n][n];
+     for(int[] a:dp) Arrays.fill(a,-1);
+     for(int i=0;i<n;i++){
+    for(int j=i;j<n;j++){
+    if(solve(i,j,s,dp)==1){
+    if(j-i+1>max){
+    max=j-i+1;
+    stIdx=i;
+    }
+    }
+    }
+     } 
+     return s.substring(stIdx,stIdx+max);
     }
 }
