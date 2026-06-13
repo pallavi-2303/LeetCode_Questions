d03b451898138d3468f800c7d76eefe447c7d345
Pallavi Singh <168811556+pallavi-2303@users.noreply.github.com>
2026-06-13 16:21:50 +0530

    Time: 168 ms (20.94%), Space: 48.6 MB (5.21%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,31 +1,28 @@
 class Solution {
-    public int solve(int i,int j,String s,int[][] dp){
-    if(i>=j) return 1;
-    if(dp[i][j]!=-1) return dp[i][j];
-    if(s.charAt(i)==s.charAt(j)){
-        return dp[i][j]=solve(i+1,j-1,s,dp);
-    }
-    else {
-        return dp[i][j]=0;
-    }
-    }
     public String longestPalindrome(String s) {
-     //brute force;
      int n=s.length();
-     int max=0;
+     boolean[][] dp=new boolean[n+1][n+1];
+     int maxLen=0;
      int stIdx=-1;
-     int[][] dp=new int[n][n];
-     for(int[] a:dp) Arrays.fill(a,-1);
-     for(int i=0;i<n;i++){
-    for(int j=i;j<n;j++){
-    if(solve(i,j,s,dp)==1){
-    if(j-i+1>max){
-    max=j-i+1;
-    stIdx=i;
+    for(int len=1;len<=n;len++){
+    for(int i=0;i+len-1<n;i++){
+    int j=i+len-1;//we have to at l len substring from i to j
+if(i==j){
+dp[i][j]=true;//1 len sbustring
+}
+else if(i+1==j){
+dp[i][j]=s.charAt(i)==s.charAt(j);
+}
+else {
+//more than 2;
+dp[i][j]=(s.charAt(i)==s.charAt(j) && dp[i+1][j-1]);
+}
+if(dp[i][j] && len>maxLen){
+maxLen=len;
+stIdx=i;
+}
     }
     }
-    }
-     } 
-     return s.substring(stIdx,stIdx+max);
+    return s.substring(stIdx,stIdx+maxLen);
     }
 }
