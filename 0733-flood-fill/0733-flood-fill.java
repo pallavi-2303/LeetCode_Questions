e8f2404027fd90ae61e9a7b292ffbee771a36a2b
Pallavi Singh <168811556+pallavi-2303@users.noreply.github.com>
2026-06-25 18:04:16 +0530

    Time: 2 ms (11.73%), Space: 47.1 MB (10.88%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -14,12 +14,33 @@ class Solution {
     }
     }
     }
+     public void bfs(int r,int c,int[][] image,int[][] vis,int color){
+Queue<int[]> q=new LinkedList<>();
+q.offer(new int[]{r,c});
+vis[r][c]=1;
+image[r][c]=color;
+while(!q.isEmpty()){
+int[] curr=q.poll();
+int row=curr[0];
+int col=curr[1];
+for(int[] d:dir){
+int nr=row+d[0];
+int nc=col+d[1];
+if(nr>=0 && nc>=0 &&  nr<n && nc<m && vis[nr][nc]==0 && image[nr][nc]==iniCol){
+q.offer(new int[]{nr,nc});
+vis[nr][nc]=1;
+image[nr][nc]=color;
+}
+}
+}
+
+     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
      n=image.length;
      m=image[0].length;
      iniCol=image[sr][sc];
      int[][] vis=new int[n][m];
-     dfs(sr,sc,image,vis,color);
+     bfs(sr,sc,image,vis,color);
      return image;
 
 
