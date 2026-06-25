ed5c7626a531ed40b627120de5019591f64e62e8
Pallavi Singh <168811556+pallavi-2303@users.noreply.github.com>
2026-06-25 17:39:30 +0530

    Time: 2 ms (90.7%), Space: 44.3 MB (39.98%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,52 +1,44 @@
 class Solution {
-    public class tuple{
-int row;
-int col;
-int time;
-public tuple(int row,int col,int time){
-this.row=row;
-this.col=col;
-this.time=time;
-}
-}
     public int orangesRotting(int[][] grid) {
-        int n=grid.length;
-int m=grid[0].length;
-int[][] vis=new int[n][m];
-Queue<tuple> q=new LinkedList<>();
-int countfresh=0;
-for(int i=0;i<n;i++){
-for(int j=0;j<m;j++){
-if(grid[i][j]==2){
-q.add(new tuple(i,j,0));
-vis[i][j]=1;
-}
-else if(grid[i][j]==1) {
-countfresh++;
-}
-}
-}
-int[] delrow={-1,1,0,0};
-int[] delcol={0,0,-1,1};
-int maxtime=0;
-int count=0;
-while(!q.isEmpty()){
-tuple it=q.poll();
-int row=it.row;
-int col=it.col;
-int t=it.time;
-maxtime=Math.max(maxtime,t);
-//visiting all the nodes that are fresh
-for(int i=0;i<4;i++){
-int nrow=row+delrow[i];
-int ncol=col+delcol[i];
-if( nrow>=0 && ncol>=0 && nrow<n && ncol<m && grid[nrow][ncol]==1 && vis[nrow][ncol]==0){
-count++;
-vis[nrow][ncol]=1;
-q.add(new tuple(nrow,ncol,t+1));
-}
-}
-}
-return count>=countfresh ? maxtime : -1;
+     //since we have to find out the minimum time so we need to do bfs
+     int n=grid.length;
+     int m=grid[0].length;
+     int[][] dir={{-1,0},{0,-1},{1,0},{0,1}};
+     int freshCount=0;
+     int[][] vis=new int[n][m];
+     Queue<int[]> q=new LinkedList<>();
+     for(int i=0;i<n;i++) {
+    for(int j=0;j<m;j++){
+    if(grid[i][j]==0){
+    continue;
+    }
+    else if(grid[i][j]==1){
+    freshCount++;
+    }
+    else {
+    q.offer(new int[]{i,j,0});
+    vis[i][j]=1;
+    }
+    }
+     }
+     int count=0;
+     int time=0;
+     while(!q.isEmpty()){
+    int[] curr=q.poll();
+    int r=curr[0];
+    int c=curr[1];
+    int t=curr[2];
+    time=Math.max(time,t);
+    for(int[] d:dir){
+    int nr=r+d[0];
+    int nc=c+d[1];
+    if(nr>=0 && nc>=0 && nr<n && nc<m && vis[nr][nc]==0 && grid[nr][nc]==1){
+    q.offer(new int[]{nr,nc,t+1});
+    vis[nr][nc]=1;
+    count++;
+    }
+    }
+     }
+     return count>=freshCount ? time :-1;
     }
 }
